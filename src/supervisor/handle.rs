9470123c555f3ec8ee012350e08d55fdc6e4c5d8
David Sánchez <davidsanchez@newrelic.com>
2023-06-01 10:45:41 +0100

    feat: NR-121867 Supervisor traits, infra supervisor

---
+++ src/supervisor/handle.rs
@@ -0,0 +1,14 @@
+use crate::command::{processrunner::Started, ProcessRunner};
+
+use super::{runner::SupervisorRunner, Handle};
+pub(crate) struct SupervisorHandle {
+    pub(crate) p_handle: ProcessRunner<Started>,
+}
+impl Handle for SupervisorHandle {
+    type E = std::io::Error;
+    type R = SupervisorRunner;
+
+    fn stop(self) -> Result</* Self::R */ (), Self::E> {
+        Ok(())
+    }
+}
