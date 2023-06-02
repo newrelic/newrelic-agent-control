85d6eaec95ded6a66f8af80d6b1f3eed8e5514e8
Grigorii Merkushev <brushknight@gmail.com>
2023-06-02 17:13:33 +0200

    add arch

--- replace_go_with_rust.sh
+++ replace_go_with_rust.sh
@@ -9,21 +9,27 @@ rm -rf ./target/**
 
 # compile production version of rust agent
 
-if [ "$ARCH" == "arm64" ];then
+if [ "$ARCH" = "arm64" ];then
 
   docker build -t rust-cross-aarch64 -f ./rust-aarch64.Dockerfile .
   docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/app rust-cross-aarch64
+  # move rust compiled files into goreleaser generated locations
+  cp ./target/aarch64-unknown-linux-gnu/release/main "${BINARY_PATH}"
 
 fi
 
-if [ "$ARCH" == "amd64" ];then
+if [ "$ARCH" = "amd64" ];then
 
   docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/app -w /usr/src/app rust:${RUST_VERSION} cargo build --release
-
+  # move rust compiled files into goreleaser generated locations
+  cp ./target/release/main "${BINARY_PATH}"
 fi
 
 # move rust compiled files into goreleaser generated locations
 cp ./target/release/main "${BINARY_PATH}"
 
 # download assets
-docker run --rm -e DOWNLOAD_ARCH="$ARCH" -v "$PWD":/usr/src/app -w /usr/src/app/build/embedded golang:1.20 make run
+docker run --rm --user "$(id -u)":"$(id -g)" -e DOWNLOAD_ARCH="$ARCH" -v "$PWD":/usr/src/app -w /usr/src/app/build/embedded golang:1.20 make run
+
+# validate
+docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app ubuntu /bin/bash -c "apt-get update && apt-get install tree -y && tree ./"
