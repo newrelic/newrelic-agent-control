04328c98e4bbffe5b9e80df4d6a98ce2cc20836f
David Sánchez <davidsanchez@newrelic.com>
2023-06-08 16:06:24 +0100

    feat: supervisor group

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,11 +1,33 @@
-use meta_agent::cli;
+use meta_agent::{cli, supervisor::supervisor_group::SupervisorGroup};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let _config = cli::init_meta_agent()?;
+    // Ok, what would be the lifecycle of the meta agent?
+    // I expect it to be something like:
+    // There is a setup phase, in which the meta agent config is retrieved from the CLI or default,
+    // then, for each agent in the config, a supervisor is created, and the agent is started.
+    // The supervisor will be in charge of restarting the agent if it fails.
+    // The meta agent will be in charge of stopping the supervisor if it receives a signal.
+    // If there is a signal to finish the meta agent, the meta agent will stop all the supervisors
+
+    // Initial setup phase
+    let meta_agent_configs = cli::init_meta_agent()?;
 
     println!("Hello, world!");
-    println!("config: {:?}", _config);
-    println!("I should be overseeing {} agents", _config.agents.len());
+    println!("config: {:?}", meta_agent_configs);
+    println!(
+        "I should be overseeing {} agents",
+        meta_agent_configs.agents.len()
+    );
+
+    // Create the supervisor group
+    let _supervisor_group = SupervisorGroup::from(&meta_agent_configs);
+
+    println!("Hello, world!");
+    println!("config: {:?}", meta_agent_configs);
+    println!(
+        "I should be overseeing {} agents",
+        meta_agent_configs.agents.len()
+    );
 
     Ok(())
 }
