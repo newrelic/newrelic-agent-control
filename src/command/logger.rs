a6a82af5fe64431f620dbfb61c085492c35763df
Roger Coll <rogercoll@protonmail.com>
2023-06-08 13:26:38 +0200

    feat: add tests for stdeventreceiver

--- src/command/logger.rs
+++ src/command/logger.rs
@@ -29,3 +29,77 @@ impl EventLogger for StdEventReceiver {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::sync::{mpsc::channel, Once};
+
+    use log::Log;
+
+    use crate::command::{
+        stream::{Event, Metadata, OutputEvent},
+        EventLogger,
+    };
+
+    use super::StdEventReceiver;
+
+    // mocked implementation of logger to assert key/values and messages
+    #[derive(Clone, Debug)]
+    struct MockedLogger {
+        expected_command_value: String,
+        expected_msg: String,
+    }
+
+    impl Log for MockedLogger {
+        fn enabled(&self, _metadata: &log::Metadata) -> bool {
+            true
+        }
+        fn log(&self, record: &log::Record) {
+            assert_eq!(
+                self.expected_command_value,
+                record
+                    .key_values()
+                    .get("command".into())
+                    .unwrap()
+                    .to_string()
+            );
+            assert_eq!(self.expected_msg, record.args().to_string());
+        }
+
+        fn flush(&self) {}
+    }
+
+    static INIT_LOGGER: Once = Once::new();
+    pub fn init_logger(mocked_logger: Box<dyn Log>) {
+        INIT_LOGGER.call_once(|| {
+            log::set_boxed_logger(mocked_logger).unwrap();
+            log::set_max_level(log::LevelFilter::Debug);
+        });
+    }
+
+    #[test]
+    fn std_logged_data_and_finish() {
+        let metadata = "testbin";
+        let send_message = "this is a test";
+        let mocked_logger = Box::new(MockedLogger {
+            expected_command_value: metadata.to_owned(),
+            expected_msg: send_message.to_string(),
+        });
+        init_logger(mocked_logger);
+
+        let logger = StdEventReceiver::default();
+
+        let (tx, rx) = channel();
+
+        let logger_handle = logger.log(rx);
+
+        tx.send(Event::new(
+            OutputEvent::Stderr(send_message.to_owned()),
+            Metadata::new(metadata),
+        ))
+        .unwrap();
+
+        drop(tx);
+        assert!(logger_handle.join().is_ok());
+    }
+}
