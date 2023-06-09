3a39dbbe8476d3620a9ec9b795f646025fb2863c
Roger Coll <rogercoll@protonmail.com>
2023-06-09 12:25:29 +0200

    fix: conflicts

--- src/agent/mod.rs
+++ src/agent/mod.rs
@@ -1,45 +1,13 @@
-pub(crate) mod config;
+use std::fmt::{Display, Formatter, Result};
+
+use self::error::AgentError;
+
 mod error;
 
 pub mod logging;
 
-use log::info;
-use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
-use std::marker::PhantomData;
-
-use crate::agent::config::Getter;
-
-use self::error::AgentError;
-
 impl Display for AgentError {
-    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+    fn fmt(&self, f: &mut Formatter) -> Result {
         write!(f, "invalid first item to double")
-    }
-}
-
-/// The Agent Struct that injects a config getter that implements
-/// the config::Getter trait and uses the V value serializer
-pub struct Agent<G: Getter<V>, V: Debug> {
-    conf_getter: G,
-    phantom: PhantomData<V>,
-}
-
-impl<G: Getter<V>, V: Debug> Agent<G, V> {
-    pub fn new(getter: G) -> Self {
-        Self {
-            conf_getter: getter,
-            phantom: PhantomData,
-        }
-    }
-
-    /// The start function calls the config getter to print the configuration.
-    pub fn start(&self) -> Result<(), AgentError> {
-        let parsed_config = self.conf_getter.get();
-
-        // init default log configuration
-        logging::init()?;
-
-        info!("{:?}", parsed_config);
-        Ok(())
     }
 }
