05be3bd4a849a28285b7330d30cec8aab073a50f
Roger Coll <rogercoll@protonmail.com>
2023-06-09 16:32:06 +0200

    feat: add source trait to Resolver constructor

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -9,14 +9,14 @@ use crate::config::{
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)] // Read from `Cargo.toml`
 struct MetaAgentCli {
-    #[arg(short, long)]
-    config: Option<PathBuf>,
+    #[arg(short, long, default_value_t = String::from("/tmp/static.yml"))]
+    config: String,
 }
 
 /// Parses command line arguments and retrieves the passed configuration
 pub fn init_meta_agent() -> Result<MetaAgentConfig, MetaAgentConfigError> {
     let cli = init_meta_agent_cli();
-    Resolver::retrieve_config(cli.config.as_deref())
+    Resolver::retrieve_config(&PathBuf::from(cli.config))
 }
 
 fn init_meta_agent_cli() -> MetaAgentCli {
