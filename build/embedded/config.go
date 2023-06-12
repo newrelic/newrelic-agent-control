89efbfe2c3902abbbf2dcd9af40397ef42f85f94
rubenruizdegauna <rubenruizdegauna@gmail.com>
2023-06-12 11:24:11 +0200

    NR-122274 feat: downloader for embedded binaries (#28)

---
+++ build/embedded/config.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"gopkg.in/yaml.v3"
+	"os"
+	"strings"
+	"text/template"
+)
+
+var errLoadingConfig = errors.New("error loading config")
+var errRequiredValue = errors.New("required value missing")
+
+type Config struct {
+	Artifacts []Artifact `yaml:"artifacts"`
+	defaults  Defaults   `yaml:"-"`
+}
+
+type Defaults struct {
+	Destination string `yaml:"destination"`
+	URL         string `yaml:"url"`
+	StagingURL  string `yaml:"staging_url"`
+}
+
+type Artifact struct {
+	Name    string `yaml:"name"`
+	URL     string `yaml:"url"`
+	Version string `yaml:"version"`
+	Files   []File `yaml:"files"`
+	Arch    string `yaml:"-"`
+}
+
+func (a Artifact) renderedUrl() (string, error) {
+	tpl, err := newTemplate("url").Parse(a.URL)
+	if err != nil {
+		return "", errors.Join(errParsingUrlTemplate, err)
+	}
+
+	return renderTemplate(tpl, a)
+}
+
+type File struct {
+	Name string `yaml:"name"`
+	Src  string `yaml:"src"`
+	Dest string `yaml:"dest"`
+}
+
+func (f File) parseDest(artifact Artifact) (string, error) {
+	tpl, err := newTemplate("dest").Parse(f.Dest)
+	if err != nil {
+		return "", err
+	}
+
+	return renderTemplate(tpl, artifact)
+}
+
+func (f File) parseSrc(artifact Artifact) (string, error) {
+	tpl, err := newTemplate("src").Parse(f.Src)
+	if err != nil {
+		return "", err
+	}
+	return renderTemplate(tpl, artifact)
+}
+
+func configFromFile(staging bool, arch string) (Config, error) {
+	// Read the YAML file into a byte slice
+	yamlFile, err := os.ReadFile("embedded.yaml")
+	if err != nil {
+		return Config{}, errors.Join(errLoadingConfig, err)
+	}
+	return config(staging, arch, yamlFile)
+}
+
+func config(staging bool, arch string, content []byte) (Config, error) {
+	// Unmarshal YAML into Config struct
+	var cnf Config
+	err := yaml.Unmarshal(content, &cnf)
+	if err != nil {
+		return Config{}, errors.Join(errLoadingConfig, err)
+	}
+
+	// Unmarshal YAML into Config Defaults struct
+	err = yaml.Unmarshal(content, &cnf.defaults)
+	if err != nil {
+		return Config{}, errors.Join(errLoadingConfig, err)
+	}
+
+	// validate required
+	if cnf.defaults.URL == "" {
+		return Config{}, errors.Join(errRequiredValue, errors.New("cnf.defaults.URL is missing"))
+	}
+	if cnf.defaults.Destination == "" {
+		return Config{}, errors.Join(errRequiredValue, errors.New("cnf.defaults.Destination is missing"))
+	}
+	if cnf.defaults.StagingURL == "" {
+		return Config{}, errors.Join(errRequiredValue, errors.New("cnf.defaults.StagingURL is missing"))
+	}
+
+	expandDefaults(staging, arch, &cnf)
+
+	return cnf, nil
+}
+
+func expandDefaults(staging bool, arch string, cnf *Config) {
+	// fill the non specified values with defaults
+	defaultUrl := cnf.defaults.URL
+	if staging {
+		defaultUrl = cnf.defaults.StagingURL
+	}
+
+	for i := range cnf.Artifacts {
+		if cnf.Artifacts[i].URL == "" {
+			cnf.Artifacts[i].URL = defaultUrl
+		}
+		if cnf.Artifacts[i].Arch == "" {
+			cnf.Artifacts[i].Arch = arch
+		}
+		for j := range cnf.Artifacts[i].Files {
+			if cnf.Artifacts[i].Files[j].Dest == "" {
+				cnf.Artifacts[i].Files[j].Dest = cnf.defaults.Destination
+			}
+		}
+	}
+}
+
+// newTemplate creates a new template and adds the helper trimv function
+func newTemplate(name string) *template.Template {
+	return template.New(name).Funcs(
+		template.FuncMap{
+			// trimv is a helper template function that removes leading v from the input string, typically a version
+			"trimv": func(str string) string {
+				return strings.TrimPrefix(str, "v")
+			},
+		},
+	)
+}
+
+func renderTemplate(tpl *template.Template, artifact Artifact) (string, error) {
+	urlbuf := &bytes.Buffer{}
+	err := tpl.Execute(urlbuf, artifact)
+	if err != nil {
+		return "", err
+	}
+	return urlbuf.String(), nil
+}
---
+++ build/embedded/config_test.go
@@ -0,0 +1,269 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_ConfigExpand(t *testing.T) {
+	testCases := []struct {
+		name         string
+		content      string
+		staging      bool
+		arch         string
+		expectedConf Config
+		expectedErr  error
+	}{
+		{
+			name:    "fallback to default",
+			staging: false,
+			arch:    "amd64",
+			content: `
+# defaults
+url: "https://download.newrelic.com/infrastructure_agent/binaries/linux/{{.Arch}}/{{.Name}}_linux_{{.Version | trimv}}_{{.Arch}}.tar.gz"
+staging_url: "https://nr-downloads-ohai-staging.s3-website-us-east-1.amazonaws.com/infrastructure_agent/binaries/linux/{{.Arch}}/{{.Name}}_linux_{{.Version | trimv}}_{{.Arch}}.tar.gz"
+destination: "./artifacts/{{.Arch}}"
+
+# artifacts
+artifacts:
+  - name: newrelic-infra
+    version: 1.42.2
+    files:
+      - name: newrelic-infra binary
+        src: newrelic-infra/usr/bin/newrelic-infra
+
+  - name: nr-otel-collector
+    version: 0.1.0
+    files:
+      - name: nr-otel-collector-binary
+        src: nr-otel-collector/usr/bin/nr-otel-collector
+      - name: another file
+        src: nr-otel-collector/usr/bin/another-file
+`,
+			expectedConf: Config{
+				Artifacts: []Artifact{
+					{
+						Name:    "newrelic-infra",
+						URL:     "https://download.newrelic.com/infrastructure_agent/binaries/linux/{{.Arch}}/{{.Name}}_linux_{{.Version | trimv}}_{{.Arch}}.tar.gz",
+						Version: "1.42.2",
+						Files: []File{
+							{
+								Name: "newrelic-infra binary",
+								Src:  "newrelic-infra/usr/bin/newrelic-infra",
+								Dest: "./artifacts/{{.Arch}}",
+							},
+						},
+						Arch: "amd64",
+					},
+					{
+						Name:    "nr-otel-collector",
+						URL:     "https://download.newrelic.com/infrastructure_agent/binaries/linux/{{.Arch}}/{{.Name}}_linux_{{.Version | trimv}}_{{.Arch}}.tar.gz",
+						Version: "0.1.0",
+						Files: []File{
+							{
+								Name: "nr-otel-collector-binary",
+								Src:  "nr-otel-collector/usr/bin/nr-otel-collector",
+								Dest: "./artifacts/{{.Arch}}",
+							},
+							{
+								Name: "another file",
+								Src:  "nr-otel-collector/usr/bin/another-file",
+								Dest: "./artifacts/{{.Arch}}",
+							},
+						},
+						Arch: "amd64",
+					},
+				},
+				defaults: Defaults{
+					URL:         "https://download.newrelic.com/infrastructure_agent/binaries/linux/{{.Arch}}/{{.Name}}_linux_{{.Version | trimv}}_{{.Arch}}.tar.gz",
+					StagingURL:  "https://nr-downloads-ohai-staging.s3-website-us-east-1.amazonaws.com/infrastructure_agent/binaries/linux/{{.Arch}}/{{.Name}}_linux_{{.Version | trimv}}_{{.Arch}}.tar.gz",
+					Destination: "./artifacts/{{.Arch}}",
+				},
+			},
+		},
+		{
+			name:    "fallback to default staging url",
+			staging: true,
+			arch:    "amd64",
+			content: `
+# defaults
+url: "https://download.newrelic.com/infrastructure_agent/binaries/linux/{{.Arch}}/{{.Name}}_linux_{{.Version | trimv}}_{{.Arch}}.tar.gz"
+staging_url: "https://nr-downloads-ohai-staging.s3-website-us-east-1.amazonaws.com/infrastructure_agent/binaries/linux/{{.Arch}}/{{.Name}}_linux_{{.Version | trimv}}_{{.Arch}}.tar.gz"
+destination: "./artifacts/{{.Arch}}"
+
+# artifacts
+artifacts:
+  - name: newrelic-infra
+    version: 1.42.2
+    files:
+      - name: newrelic-infra binary
+        src: newrelic-infra/usr/bin/newrelic-infra
+
+  - name: nr-otel-collector
+    version: 0.1.0
+    files:
+      - name: nr-otel-collector-binary
+        src: nr-otel-collector/usr/bin/nr-otel-collector
+      - name: another file
+        src: nr-otel-collector/usr/bin/another-file
+`,
+			expectedConf: Config{
+				Artifacts: []Artifact{
+					{
+						Name:    "newrelic-infra",
+						URL:     "https://nr-downloads-ohai-staging.s3-website-us-east-1.amazonaws.com/infrastructure_agent/binaries/linux/{{.Arch}}/{{.Name}}_linux_{{.Version | trimv}}_{{.Arch}}.tar.gz",
+						Version: "1.42.2",
+						Files: []File{
+							{
+								Name: "newrelic-infra binary",
+								Src:  "newrelic-infra/usr/bin/newrelic-infra",
+								Dest: "./artifacts/{{.Arch}}",
+							},
+						},
+						Arch: "amd64",
+					},
+					{
+						Name:    "nr-otel-collector",
+						URL:     "https://nr-downloads-ohai-staging.s3-website-us-east-1.amazonaws.com/infrastructure_agent/binaries/linux/{{.Arch}}/{{.Name}}_linux_{{.Version | trimv}}_{{.Arch}}.tar.gz",
+						Version: "0.1.0",
+						Files: []File{
+							{
+								Name: "nr-otel-collector-binary",
+								Src:  "nr-otel-collector/usr/bin/nr-otel-collector",
+								Dest: "./artifacts/{{.Arch}}",
+							},
+							{
+								Name: "another file",
+								Src:  "nr-otel-collector/usr/bin/another-file",
+								Dest: "./artifacts/{{.Arch}}",
+							},
+						},
+						Arch: "amd64",
+					},
+				},
+				defaults: Defaults{
+					URL:         "https://download.newrelic.com/infrastructure_agent/binaries/linux/{{.Arch}}/{{.Name}}_linux_{{.Version | trimv}}_{{.Arch}}.tar.gz",
+					StagingURL:  "https://nr-downloads-ohai-staging.s3-website-us-east-1.amazonaws.com/infrastructure_agent/binaries/linux/{{.Arch}}/{{.Name}}_linux_{{.Version | trimv}}_{{.Arch}}.tar.gz",
+					Destination: "./artifacts/{{.Arch}}",
+				},
+			},
+		},
+		{
+			name:    "fallback to default + hardcoded ones mixed",
+			staging: false,
+			arch:    "amd64",
+			content: `
+# defaults
+url: "https://download.newrelic.com/infrastructure_agent/binaries/linux/{{.Arch}}/{{.Name}}_linux_{{.Version | trimv}}_{{.Arch}}.tar.gz"
+staging_url: "https://nr-downloads-ohai-staging.s3-website-us-east-1.amazonaws.com/infrastructure_agent/binaries/linux/{{.Arch}}/{{.Name}}_linux_{{.Version | trimv}}_{{.Arch}}.tar.gz"
+destination: "./artifacts/{{.Arch}}"
+
+# artifacts
+artifacts:
+  - name: newrelic-infra
+    version: 1.42.2
+    files:
+      - name: newrelic-infra binary
+        src: newrelic-infra/usr/bin/newrelic-infra
+
+  - name: nr-otel-collector
+    version: 0.1.0
+    url: "http://www.some.url/and/path"
+    files:
+      - name: nr-otel-collector-binary
+        src: nr-otel-collector/usr/bin/nr-otel-collector
+        dest: "./hardcoded/dest"
+      - name: another file
+        src: nr-otel-collector/usr/bin/another-file
+`,
+			expectedConf: Config{
+				Artifacts: []Artifact{
+					{
+						Name:    "newrelic-infra",
+						URL:     "https://download.newrelic.com/infrastructure_agent/binaries/linux/{{.Arch}}/{{.Name}}_linux_{{.Version | trimv}}_{{.Arch}}.tar.gz",
+						Version: "1.42.2",
+						Files: []File{
+							{
+								Name: "newrelic-infra binary",
+								Src:  "newrelic-infra/usr/bin/newrelic-infra",
+								Dest: "./artifacts/{{.Arch}}",
+							},
+						},
+						Arch: "amd64",
+					},
+					{
+						Name:    "nr-otel-collector",
+						URL:     "http://www.some.url/and/path",
+						Version: "0.1.0",
+						Files: []File{
+							{
+								Name: "nr-otel-collector-binary",
+								Src:  "nr-otel-collector/usr/bin/nr-otel-collector",
+								Dest: "./hardcoded/dest",
+							},
+							{
+								Name: "another file",
+								Src:  "nr-otel-collector/usr/bin/another-file",
+								Dest: "./artifacts/{{.Arch}}",
+							},
+						},
+						Arch: "amd64",
+					},
+				},
+				defaults: Defaults{
+					URL:         "https://download.newrelic.com/infrastructure_agent/binaries/linux/{{.Arch}}/{{.Name}}_linux_{{.Version | trimv}}_{{.Arch}}.tar.gz",
+					StagingURL:  "https://nr-downloads-ohai-staging.s3-website-us-east-1.amazonaws.com/infrastructure_agent/binaries/linux/{{.Arch}}/{{.Name}}_linux_{{.Version | trimv}}_{{.Arch}}.tar.gz",
+					Destination: "./artifacts/{{.Arch}}",
+				},
+			},
+		},
+		{
+			name:    "validate required url",
+			staging: false,
+			arch:    "amd64",
+			content: `
+# defaults
+url: ""
+staging_url: "https://nr-downloads-ohai-staging.s3-website-us-east-1.amazonaws.com/infrastructure_agent/binaries/linux/{{.Arch}}/{{.Name}}_linux_{{.Version | trimv}}_{{.Arch}}.tar.gz"
+destination: "./artifacts/{{.Arch}}"
+`,
+			expectedErr: errRequiredValue,
+		},
+		{
+			name:    "validate required staging url",
+			staging: false,
+			arch:    "amd64",
+			content: `
+# defaults
+url: "https://download.newrelic.com/infrastructure_agent/binaries/linux/{{.Arch}}/{{.Name}}_linux_{{.Version | trimv}}_{{.Arch}}.tar.gz"
+staging_url: ""
+destination: "./artifacts/{{.Arch}}"
+`,
+			expectedErr: errRequiredValue,
+		},
+		{
+			name:    "validate required destination",
+			staging: false,
+			arch:    "amd64",
+			content: `
+# defaults
+url: "https://download.newrelic.com/infrastructure_agent/binaries/linux/{{.Arch}}/{{.Name}}_linux_{{.Version | trimv}}_{{.Arch}}.tar.gz"
+staging_url: "some url"
+destination: ""
+`,
+			expectedErr: errRequiredValue,
+		},
+	}
+	for i := range testCases {
+		testCase := testCases[i]
+		t.Run(testCase.name, func(t *testing.T) {
+			conf, err := config(testCase.staging, testCase.arch, []byte(testCase.content))
+			if testCase.expectedErr == nil {
+				assert.NoError(t, err)
+				assert.Equal(t, testCase.expectedConf, conf)
+			} else {
+				assert.ErrorIs(t, err, testCase.expectedErr)
+			}
+		})
+	}
+}
