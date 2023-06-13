9338749713ec4e0e72e7fb70640413100b95c4e9
David Sánchez <davidsanchez@newrelic.com>
2023-06-13 10:03:10 +0100

    refactor: use Agent entrypoint for work

--- src/agent/mod.rs
+++ src/agent/mod.rs
@@ -12,59 +12,63 @@ pub mod lifecycle;
 pub mod logging;
 pub mod supervisor_group;
 
-pub fn work() -> Result<(), Box<dyn std::error::Error>> {
-    // Initial setup phase
-    let mut init = Lifecycle::init()?;
+pub struct Agent;
 
-    // FIXME: Placeholder for NR-124576
-    let signal_manager = thread::spawn({
-        let ctx = init.get_context();
-        move || {
-            info!("Starting the signal manager");
-            thread::sleep(std::time::Duration::from_secs(120));
-            ctx.cancel_all().unwrap();
+impl Agent {
+    pub fn work() -> Result<(), Box<dyn std::error::Error>> {
+        // Initial setup phase
+        let mut init = Lifecycle::init()?;
+
+        // FIXME: Placeholder for NR-124576
+        let signal_manager = thread::spawn({
+            let ctx = init.get_context();
+            move || {
+                info!("Starting the signal manager");
+                thread::sleep(std::time::Duration::from_secs(120));
+                ctx.cancel_all().unwrap();
+            }
+        });
+
+        let (tx, rx) = init.extract_channel()?;
+
+        let output_manager = StdEventReceiver::default().log(rx);
+
+        let supervisor_group = SupervisorGroup::new(init.get_context(), tx, init.get_configs());
+        {
+            /*
+                TODO: We should first compare the current config with the one in the meta agent config.
+                In a future situation, it might have changed due to updates from OpAMP, etc.
+                Then, this would require selecting the agents whose config has changed,
+                and restarting them.
+
+                FIXME: Given the above comment, this should be converted to a loop in which we modify
+                the supervisor group behavior on config changes and selectively restart them as needed.
+                For checking the supervisors in a non-blocking way, we can use Handle::is_finished().
+
+                Suppose there's a config change. Situations:
+                - Current agents stay as is, new agents are added: start these new agents, merge them with the current group.
+                - Current agents stay as is, some agents are removed: get list of these agents (by key), stop and remove them from the current group.
+                - Updated config for a certain agent(s) (type, name). Get (by key), stop, remove from the current group, start again with the new config and merge with the running group.
+
+                The "merge" operation can only be done if the agents are of the same type! Supervisor<Running>. If they are not started we won't be able to merge them to the running group, as they are different types.
+            */
+
+            info!("Starting the supervisor group.");
+            // Run all the agents in the supervisor group
+            let running_supervisors = supervisor_group.run();
+
+            // For the time being, we only need to wait for the supervisor group to finish
+            let _ = running_supervisors.wait();
+            info!("Supervisor group has finished. Exiting the meta agent");
         }
-    });
 
-    let (tx, rx) = init.extract_channel()?;
+        // Ending the program
+        info!("Waiting for the signal manager to finish");
+        signal_manager.join().unwrap();
+        info!("Waiting for the output manager to finish");
+        output_manager.join().unwrap();
 
-    let output_manager = StdEventReceiver::default().log(rx);
-
-    let supervisor_group = SupervisorGroup::new(init.get_context(), tx, init.get_configs());
-    {
-        /*
-            TODO: We should first compare the current config with the one in the meta agent config.
-            In a future situation, it might have changed due to updates from OpAMP, etc.
-            Then, this would require selecting the agents whose config has changed,
-            and restarting them.
-
-            FIXME: Given the above comment, this should be converted to a loop in which we modify
-            the supervisor group behavior on config changes and selectively restart them as needed.
-            For checking the supervisors in a non-blocking way, we can use Handle::is_finished().
-
-            Suppose there's a config change. Situations:
-            - Current agents stay as is, new agents are added: start these new agents, merge them with the current group.
-            - Current agents stay as is, some agents are removed: get list of these agents (by key), stop and remove them from the current group.
-            - Updated config for a certain agent(s) (type, name). Get (by key), stop, remove from the current group, start again with the new config and merge with the running group.
-
-            The "merge" operation can only be done if the agents are of the same type! Supervisor<Running>. If they are not started we won't be able to merge them to the running group, as they are different types.
-        */
-
-        info!("Starting the supervisor group.");
-        // Run all the agents in the supervisor group
-        let running_supervisors = supervisor_group.run();
-
-        // For the time being, we only need to wait for the supervisor group to finish
-        let _ = running_supervisors.wait();
-        info!("Supervisor group has finished. Exiting the meta agent");
+        info!("Exit");
+        Ok(())
     }
-
-    // Ending the program
-    info!("Waiting for the signal manager to finish");
-    signal_manager.join().unwrap();
-    info!("Waiting for the output manager to finish");
-    output_manager.join().unwrap();
-
-    info!("Exit");
-    Ok(())
 }
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,9 +1,23 @@
-use meta_agent::agent;
+use meta_agent::agent::Agent;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    if let Err(e) = agent::work() {
-        eprintln!("{}", e);
-        std::process::exit(1);
-    }
-    Ok(())
+    Ok(Agent::work()?)
 }
+
+/*
+We can also make use of `main() -> ExitCode` if we want to return more specific exit codes
+instead of just success or failure states (What returning `Result` does):
+
+fn main() -> ExitCode {
+    // Assumi8ng Agent::work() returns an `i32`...
+    ExitCode::from(Agent::work())
+    // We could also check the actual errors here and decide what specific exit code to return
+    // based on the error type. Something like:
+    match Agent::work() {
+        Ok(_) => ExitCode::SUCCESS,
+        Err(AgentError::ConfigNotFound) => ExitCode::from(101),
+        Err(AgentError::ConfigParseError) => ExitCode::from(102),
+        // ...
+    }
+}
+ */
