acde012c0aa7dad8fbebd25f8c802c51dc7faf2e
Roger Coll <rogercoll@protonmail.com>
2023-06-13 17:32:05 +0200

    feat: move to try_init with env vars
    See: https://github.com/tokio-rs/tracing/issues/2514

--- src/agent/logging/mod.rs
+++ src/agent/logging/mod.rs
@@ -1,10 +1,12 @@
+use tracing_subscriber::fmt::try_init;
+
 use super::error::AgentError;
 
 pub struct Logging;
 
 impl Logging {
     pub fn init() -> Result<(), AgentError> {
-        tracing_subscriber::fmt().try_init().map_err(|_| {
+        try_init().map_err(|_| {
             AgentError::LoggingError("unable to set agent global logging subscriber".to_string())
         })
     }
