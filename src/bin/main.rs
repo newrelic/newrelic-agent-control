eeee69f5b274e22e2212756534adcfbc1c8d2a37
alvarocabanas <acabanas@newrelic.com>
2023-06-14 12:46:41 +0200

    Add ctrlc crate

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,7 @@
-use meta_agent::{agent::Agent, cli::Cli, logging::Logging};
+use meta_agent::{agent::Agent, cli::Cli, context::Context, logging::Logging};
 use std::error::Error;
 use tracing::info;
+use std::{sync::mpsc, thread};
 
 fn main() -> Result<(), Box<dyn Error>> {
     // init logging singleton
@@ -15,8 +16,30 @@ fn main() -> Result<(), Box<dyn Error>> {
         return Ok(());
     }
 
-    info!("Starting the meta agent");
+    println!("Creating the global context");
+    let ctx = Context::new();
 
-    Agent::new(&cli.get_config_path())?.run()?;
+    let signal_manager = thread::spawn({
+        let ctx = ctx.clone();
+        move || {
+            let (tx, rx) = mpsc::channel();
+
+            ctrlc::set_handler(move || tx.send(()).expect("Could not send signal on channel"))
+                .expect("Error setting signal handler");
+
+            // Wait for shutdown signal
+            rx.recv().expect("Could not receive from signal channel.");
+            println!("Graceful shutdown");
+            ctx.cancel_all().unwrap();
+        }
+    });
+
+    info!("Starting the meta agent");
+    Agent::new(&cli.get_config_path())?.run(ctx.clone())?;
+
+    // Ending the program
+    println!("Waiting for the signal manager to finish");
+    signal_manager.join().unwrap();
+
     Ok(())
 }
