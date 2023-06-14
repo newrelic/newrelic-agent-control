c0111fb3a8a46229c2224e7f0fb79a4df85cceb0
alvarocabanas <acabanas@newrelic.com>
2023-06-14 19:44:46 +0200

    Fix fmt

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,7 @@
+use ctrlc;
 use meta_agent::{agent::Agent, cli::Cli, context::Context, logging::Logging};
 use std::error::Error;
 use tracing::{error, info};
-use ctrlc;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // init logging singleton
