fda7dd3d91b39b3e71b94d6a34d6eacc4213d97e
David Sánchez <davidsanchez@newrelic.com>
2023-06-15 12:19:25 +0100

    feat: disable custom agent config availability

--- src/config/agent_type.rs
+++ src/config/agent_type.rs
@@ -1,17 +1,14 @@
 use serde::Deserialize;
 
 type AgentName = Option<String>;
-type CustomAgentType = String;
 
 /// AgentType represents the type of an agent. We currently support the following agent types:
 /// - `nr_infra_agent`: New Relic Infrastructure agent
 /// - `nr_otel_collector`: New Relic Distribution for the OpenTelemetry Collector
-/// - `custom_agent_type`: Custom agent type (e.g. a binary with its arguments)
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub enum AgentType {
     InfraAgent(AgentName),
     Nrdot(AgentName),
-    Custom(CustomAgentType, AgentName),
 }
 
 impl<'de> Deserialize<'de> for AgentType {
@@ -22,8 +19,8 @@ impl<'de> Deserialize<'de> for AgentType {
         let s = String::deserialize(deserializer)?;
         let parts: Vec<&str> = s.split('/').collect();
         match parts.len() {
-            1 => Ok(agent_type(parts[0], None)),
-            2 => Ok(agent_type(parts[0], Some(parts[1].to_string()))),
+            1 => agent_type(parts[0], None),
+            2 => agent_type(parts[0], Some(parts[1].to_string())),
             _ => Err(serde::de::Error::custom(
                 "`agents` items must be of the form `agent_type` or `agent_type/name`, where `agent_type` is one of `nr_infra_agent`, `nr_otel_collector` or some other custom string, and `name` is a custom name for the agent. Examples: `nr_infra_agent`, `nr_otel_collector/my_col`, `my_agent/agent1`",
             )),
@@ -31,10 +28,16 @@ impl<'de> Deserialize<'de> for AgentType {
     }
 }
 
-fn agent_type(agent_type: &str, id: Option<String>) -> AgentType {
+fn agent_type<D>(agent_type: &str, id: Option<String>) -> Result<AgentType, D>
+where
+    D: serde::de::Error,
+{
     match agent_type {
-        "nr_infra_agent" => AgentType::InfraAgent(id),
-        "nr_otel_collector" => AgentType::Nrdot(id),
-        custom => AgentType::Custom(custom.to_string(), id),
+        "nr_infra_agent" => Ok(AgentType::InfraAgent(id)),
+        "nr_otel_collector" => Ok(AgentType::Nrdot(id)),
+        custom => Err(serde::de::Error::custom(format!(
+            "unknown agent type {}",
+            custom
+        ))),
     }
 }
