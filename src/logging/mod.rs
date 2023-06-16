2bc81c13ee3bba31879af956d92ce05e5d9a9527
Roger Coll <rogercoll@protonmail.com>
2023-06-16 15:26:42 +0200

    feat: set INFO as default log level

--- src/logging/mod.rs
+++ src/logging/mod.rs
@@ -1,5 +1,7 @@
 use std::fmt::Debug;
 use thiserror::Error;
+use tracing::metadata::LevelFilter;
+use tracing::Level;
 use tracing_subscriber::fmt::format::PrettyFields;
 use tracing_subscriber::EnvFilter;
 
@@ -14,7 +16,12 @@ pub struct Logging;
 impl Logging {
     pub fn try_init() -> Result<(), LoggingError> {
         tracing_subscriber::fmt()
-            .with_env_filter(EnvFilter::from_default_env())
+            .with_max_level(Level::INFO)
+            .with_env_filter(
+                EnvFilter::builder()
+                    .with_default_directive(LevelFilter::INFO.into())
+                    .from_env_lossy(),
+            )
             .fmt_fields(PrettyFields::new())
             .try_init()
             .map_err(|_| {
