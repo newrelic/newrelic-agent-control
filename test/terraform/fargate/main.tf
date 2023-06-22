bdff240b0736f0d32bb094f77d91313b63ad8a08
David Sánchez <davidsanchez@newrelic.com>
2023-06-22 10:17:04 +0100

    package/testing provision (#68)
    * ci: NR-127587 packaging test provision
    
    * remove .terraform.lock.hcl
    
    * add workflows and tf setup
    
    * add aws role arn
    
    * ci: remove fetch-depth
    
    * ci: use ubuntu-latest
    
    * ci: use separate job for checkout
    
    * ci: use separate git configuration step (auth)
    
    * ci: use token in checkout action
    
    * ci: fix github token info
    
    * ci: move job definitions to workflow file
    
    * test checkout action
    
    * ci: add hcl lockfile to gitignore
    
    * ci: set up provisioning job
    
    * wip: hardcode tag temporarily
    
    * ci: use full url for cloning
    
    * fix: ssh url
    
    * ci: add full url to provision infra
    
    * ci: use full ssh url
    
    * ci: rename TF vars
    
    * fix: provisioner name
    
    * ci: reinstall rpm before install packages (centos)
    
    * ci: set packaging test to on-demand (disable push)
    
    * test: remove packaging dir
    
    * test: unify test directory
    
    ---------
    
    Co-authored-by: Grigorii Merkushev <brushknight@gmail.com>

---
+++ test/terraform/fargate/main.tf
@@ -0,0 +1,104 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+#########################################
+# State Backend
+#########################################
+terraform {
+  backend "s3" {
+    bucket = "automation-pipeline-terraform-state"
+    key    = "meta_agent_releases"
+    region = "us-east-2"
+  }
+}
+
+
+module "meta_agent_infra" {
+    source = "github.com/newrelic/fargate-runner-action//terraform/modules/infra-ecs-fargate?ref=main"
+    region = var.region
+    vpc_id = var.vpc_id
+    vpc_subnet_id = var.vpc_subnet
+    account_id = var.accountId
+
+    s3_terraform_bucket_arn = var.s3_bucket
+
+
+
+    cluster_name           = var.cluster_name
+
+    cloudwatch_log_group = var.task_logs_group
+
+    task_container_image = var.task_container_image
+    task_container_name = var.task_container_name
+    task_name_prefix = var.task_name_prefix
+    task_secrets = [
+        {
+          "name" : "SSH_KEY",
+          "valueFrom" : "arn:aws:secretsmanager:${var.region}:${var.accountId}:secret:${var.secret_name_ssh}"
+        },
+        {
+          "name" : "NR_LICENSE_KEY",
+          "valueFrom" : "arn:aws:secretsmanager:${var.region}:${var.accountId}:secret:${var.secret_name_license}"
+        },
+        {
+          "name" : "NR_LICENSE_KEY_CANARIES",
+          "valueFrom" : "arn:aws:secretsmanager:${var.region}:${var.accountId}:secret:${var.secret_name_license_canaries}"
+        },
+        {
+          "name" : "NEW_RELIC_ACCOUNT_ID",
+          "valueFrom" : "arn:aws:secretsmanager:${var.region}:${var.accountId}:secret:${var.secret_name_account}"
+        },
+        {
+          "name" : "NEW_RELIC_API_KEY",
+          "valueFrom" : "arn:aws:secretsmanager:${var.region}:${var.accountId}:secret:${var.secret_name_api}"
+        },
+        {
+          "name" : "NR_API_KEY",
+          "valueFrom" : "arn:aws:secretsmanager:${var.region}:${var.accountId}:secret:${var.secret_name_nr_api_key}"
+        },
+        {
+          "name" : "DOCKER_USERNAME",
+          "valueFrom" : "arn:aws:secretsmanager:${var.region}:${var.accountId}:secret:${var.secret_name_docker_username}"
+        },
+        {
+          "name" : "DOCKER_PASSWORD",
+          "valueFrom" : "arn:aws:secretsmanager:${var.region}:${var.accountId}:secret:${var.secret_name_docker_password}"
+        }
+      ]
+    task_custom_policies = [
+        jsonencode(
+          {
+            "Version" : "2012-10-17",
+            "Statement" : [
+
+              {
+                "Effect" : "Allow",
+                "Action" : [
+                  "secretsmanager:GetSecretValue"
+                ],
+                "Resource" : [
+                  "arn:aws:secretsmanager:${var.region}:${var.accountId}:secret:${var.secret_name_ssh}",
+                  "arn:aws:secretsmanager:${var.region}:${var.accountId}:secret:${var.secret_name_license}",
+                  "arn:aws:secretsmanager:${var.region}:${var.accountId}:secret:${var.secret_name_license_canaries}",
+                  "arn:aws:secretsmanager:${var.region}:${var.accountId}:secret:${var.secret_name_account}",
+                  "arn:aws:secretsmanager:${var.region}:${var.accountId}:secret:${var.secret_name_api}",
+                  "arn:aws:secretsmanager:${var.region}:${var.accountId}:secret:${var.secret_name_nr_api_key}",
+                  "arn:aws:secretsmanager:${var.region}:${var.accountId}:secret:${var.secret_name_docker_username}",
+                  "arn:aws:secretsmanager:${var.region}:${var.accountId}:secret:${var.secret_name_docker_password}"
+                ]
+              }
+            ]
+          }
+        )
+      ]
+
+    efs_volume_mount_point = var.efs_volume_mount_point
+    efs_volume_name = var.efs_volume_name
+    additional_efs_security_group_rules = var.additional_efs_security_group_rules
+    canaries_security_group = var.canaries_security_group
+
+    oidc_repository = var.oidc_repository
+    oidc_role_name = var.oidc_role_name
+
+}
---
+++ test/terraform/fargate/vars.tf
@@ -0,0 +1,130 @@
+#######################################
+# Global vars
+#######################################
+
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "accountId" {
+  default = "018789649883"
+}
+
+variable "vpc_id" {
+  default = "vpc-0a3c00f5dc8645fe0"
+}
+
+variable "vpc_subnet" {
+  default = "subnet-09b64de757828cdd4"
+}
+
+variable "cluster_name" {
+  default = "caos_meta_agent_releases"
+}
+
+#######################################
+# Task definition
+#######################################
+
+variable "task_command" {
+  default = [
+    "test/automated-run"
+  ]
+}
+
+variable "secret_name_ssh" {
+  default = "caos/canaries/ssh_key-UBSKNA"
+}
+
+variable "secret_name_license" {
+  default = "caos/canaries/license-f9eYwe"
+}
+
+variable "secret_name_license_canaries" {
+  default = "caos/canaries/license_canaries-1DCE1L"
+}
+
+variable "secret_name_account" {
+  default = "caos/canaries/account-kKFMGP"
+}
+
+variable "secret_name_api" {
+  default = "caos/canaries/api-9q0NPb"
+}
+
+variable "secret_name_nr_api_key" {
+  default = "caos/canaries/nr_api_key-xadBYJ"
+}
+
+variable "secret_name_docker_username" {
+  default = "caos/canaries/docker-username-iksa0V"
+}
+
+variable "secret_name_docker_password" {
+  default = "caos/canaries/docker-password-jAtw3v"
+}
+
+variable "task_container_image" {
+  default = "ghcr.io/newrelic/fargate-runner-action:latest"
+}
+
+variable "task_logs_group" {
+  default = "/ecs/test-prerelease-meta_agent-releases"
+}
+
+variable "task_container_name" {
+  default = "test-meta_agent-releases"
+}
+
+variable "task_name_prefix" {
+  default = "meta_agent-releases"
+}
+
+variable "task_logs_prefix" {
+  default = "ecs-meta_agent-releases"
+}
+
+variable "s3_bucket" {
+  default = "arn:aws:s3:::automation-pipeline-terraform-state"
+}
+
+#######################################
+# EFS volume
+#######################################
+
+variable "efs_volume_mount_point" {
+  default = "/srv/runner/inventory"
+}
+
+variable "efs_volume_name" {
+  default = "shared-meta_agent-releases"
+}
+
+variable "canaries_security_group" {
+  default = "sg-044ef7bc34691164a"
+}
+
+variable "additional_efs_security_group_rules" {
+    default = [
+    {
+      type                     = "ingress"
+      from_port                = 0
+      to_port                  = 65535
+      protocol                 = "tcp"
+      cidr_blocks              = ["10.10.0.0/24"]
+      description              = "Allow ingress traffic to EFS from trusted subnet"
+    }
+  ]
+}
+
+#######################################
+# OIDC permissions
+#######################################
+
+variable "oidc_repository" {
+  default = "repo:newrelic/newrelic-supervisor:*"
+}
+
+variable "oidc_role_name" {
+  default = "caos-pipeline-oidc-meta_agent-releases"
+}
