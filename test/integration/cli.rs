4f418aa1f2c1639edca4eee4f681f23432e5ec5a
David Sánchez <davidsanchez@newrelic.com>
2023-06-23 11:18:24 +0100

    feat: disallow run as non-root (NR-128816) (#76)
    * feat: disallow run as non-root (NR-128816)
    
    * fix: only check for root in unix
    
    * fix: run non-root test on unix only

--- test/integration/cli.rs
+++ test/integration/cli.rs
@@ -1,17 +1,33 @@
-use std::{fs::File, io::Write};
+use std::{fs::File, io::Write, path::PathBuf};
 
 use assert_cmd::Command;
+use predicates::prelude::predicate;
 
-#[test]
-fn print_debug_info() -> Result<(), Box<dyn std::error::Error>> {
+fn create_simple_config() -> Result<PathBuf, Box<dyn std::error::Error>> {
     let dir = assert_fs::TempDir::new()?;
     let file_path = dir.path().join("static.yml");
     let mut file = File::create(&file_path)?;
     writeln!(file, "agents: {{}}")?;
+    Ok(file_path)
+}
 
+#[test]
+fn print_debug_info() -> Result<(), Box<dyn std::error::Error>> {
+    let file_path = create_simple_config()?;
     let mut cmd = Command::cargo_bin("main")?;
     cmd.arg("--config").arg(file_path).arg("--print-debug-info");
     cmd.assert().success();
+    Ok(())
+}
 
+#[cfg(unix)]
+#[test]
+fn does_not_run_if_no_root() -> Result<(), Box<dyn std::error::Error>> {
+    let file_path = create_simple_config()?;
+    let mut cmd = Command::cargo_bin("main")?;
+    cmd.arg("--config").arg(file_path);
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("Program must run as root"));
     Ok(())
 }
