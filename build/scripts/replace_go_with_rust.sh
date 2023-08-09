166703adc583d8c83261305929319eea7ad30307
David Sánchez <davidsanchez@newrelic.com>
2023-08-09 17:00:10 +0100

    refactor: rust build script

--- build/scripts/replace_go_with_rust.sh
+++ build/scripts/replace_go_with_rust.sh
@@ -11,23 +11,20 @@ rm -rf ./target/**
 if [ "$ARCH" = "arm64" ];then
   BINARY_PATH="./dist/newrelic-super-agent_linux_${ARCH}/newrelic-super-agent"
   ARCH_NAME="aarch64"
-  rm "${BINARY_PATH}"
-  docker build -t rust-cross-${ARCH_NAME} -f ./build/rust.Dockerfile --build-arg ARCH_NAME=${ARCH_NAME} .
-  docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/app rust-cross-${ARCH_NAME}
-  # move rust compiled files into goreleaser generated locations
-  cp ./target/${ARCH_NAME}-unknown-linux-gnu/release/main "${BINARY_PATH}"
 fi
 
 if [ "$ARCH" = "amd64" ];then
   BINARY_PATH="./dist/newrelic-super-agent_linux_${ARCH}_v1/newrelic-super-agent"
   ARCH_NAME="x86_64"
-  rm "${BINARY_PATH}"
-  docker build -t rust-cross-${ARCH_NAME} -f ./build/rust.Dockerfile --build-arg ARCH_NAME=${ARCH_NAME} .
-  docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/app rust-cross-${ARCH_NAME}
-  # move rust compiled files into goreleaser generated locations
-  cp ./target/${ARCH_NAME}-unknown-linux-gnu/release/main "${BINARY_PATH}"
 fi
 
+rm "${BINARY_PATH}"
+
+docker build -t "rust-cross-${ARCH_NAME}" -f ./build/rust.Dockerfile --build-arg ARCH_NAME="${ARCH_NAME}" .
+docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/app rust-cross-"${ARCH_NAME}"
+
+# move rust compiled files into goreleaser generated locations
+cp "./target/${ARCH_NAME}-unknown-linux-gnu/release/main" "${BINARY_PATH}"
 
 # validate
 docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app ubuntu /bin/bash -c "apt-get update && apt-get install tree -y && tree ./"
