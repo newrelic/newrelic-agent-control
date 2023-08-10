87510f5e5f83ba28f0831810c2d8713afc93b928
David Sánchez <davidsanchez@newrelic.com>
2023-08-10 10:55:45 +0100

    build: static binary

---
+++ build/rust.Dockerfile
@@ -0,0 +1,24 @@
+ARG RUST_VERSION=1.71.1
+FROM rust:${RUST_VERSION}
+
+RUN apt update && apt upgrade -y
+
+ARG ARCH_NAME
+RUN if [ "${ARCH_NAME}" = "aarch64" ]; then \
+      # We assume the docker image's arch is x86_64, so cross-compiling for aarch64
+      apt install -y g++-aarch64-linux-gnu libc6-dev-arm64-cross && \
+      rustup toolchain install stable-aarch64-unknown-linux-gnu --force-non-host; \
+    fi
+RUN rustup target add "${ARCH_NAME}-unknown-linux-gnu"
+
+WORKDIR /usr/src/app
+
+ENV ARCH_NAME=${ARCH_NAME} \
+    # Generate static builds
+    RUSTFLAGS="-C target-feature=+crt-static" \
+    # Use the correct linker for aarch64 target
+    CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc \
+    CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc \
+    CXX_aarch64_unknown_linux_gnu=aarch64-linux-gnu-g++
+
+CMD cargo build --release --target "${ARCH_NAME}-unknown-linux-gnu"
--- build/scripts/replace_go_with_rust.sh
+++ build/scripts/replace_go_with_rust.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-RUST_VERSION="1.70.0"
+RUST_VERSION="1.71.1"
 
 # remove go generated files
 rm -rf ./target/**
@@ -10,22 +10,21 @@ rm -rf ./target/**
 
 if [ "$ARCH" = "arm64" ];then
   BINARY_PATH="./dist/newrelic-super-agent_linux_${ARCH}/newrelic-super-agent"
-  rm "${BINARY_PATH}"
-  docker build -t rust-cross-aarch64 -f ./build/rust-aarch64.Dockerfile .
-  docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/app rust-cross-aarch64
-  # move rust compiled files into goreleaser generated locations
-  cp ./target/aarch64-unknown-linux-gnu/release/main "${BINARY_PATH}"
-
+  ARCH_NAME="aarch64"
 fi
 
 if [ "$ARCH" = "amd64" ];then
   BINARY_PATH="./dist/newrelic-super-agent_linux_${ARCH}_v1/newrelic-super-agent"
-  rm "${BINARY_PATH}"
-  docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/app -w /usr/src/app rust:${RUST_VERSION} cargo build --release
-  # move rust compiled files into goreleaser generated locations
-  cp ./target/release/main "${BINARY_PATH}"
+  ARCH_NAME="x86_64"
 fi
 
+rm "${BINARY_PATH}"
+
+docker build -t "rust-cross-${ARCH_NAME}" -f ./build/rust.Dockerfile --build-arg ARCH_NAME="${ARCH_NAME}" .
+docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/app rust-cross-"${ARCH_NAME}"
+
+# move rust compiled files into goreleaser generated locations
+cp "./target/${ARCH_NAME}-unknown-linux-gnu/release/main" "${BINARY_PATH}"
 
 # validate
 docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app ubuntu /bin/bash -c "apt-get update && apt-get install tree -y && tree ./"
