b4e3689a9fd8ce02f24c0161f10fa2bfb9bf887b
David Sánchez <davidsanchez@newrelic.com>
2023-08-24 13:22:08 +0100

    style: reorder const declarations for tests

--- src/config/supervisor_config.rs
+++ src/config/supervisor_config.rs
@@ -211,11 +211,49 @@ meta:
           args: "-c ${deployment.on_host.args}"
 "#;
 
+    #[test]
+    fn test_validate_with_agent_type() {
+        let input_structure =
+            serde_yaml::from_str::<SupervisorConfig>(EXAMPLE_CONFIG_REPLACE).unwrap();
+        let normalized = normalize_supervisor_config(input_structure);
+        let agent_type = serde_yaml::from_str::<AgentType>(EXAMPLE_AGENT_YAML_REPLACE).unwrap();
+
+        let expected = Map::from([
+            (
+                "deployment.on_host.args".to_string(),
+                TrivialValue::String("--verbose true".to_string()),
+            ),
+            (
+                "deployment.on_host.path".to_string(),
+                TrivialValue::String("/etc".to_string()),
+            ),
+        ]);
+        let actual = validate_with_agent_type(normalized, &agent_type).unwrap();
+
+        assert_eq!(expected, actual);
+    }
+
     const EXAMPLE_CONFIG_REPLACE_NOPATH: &str = r#"
 deployment:
   on_host:
     args: --verbose true
 "#;
+
+    #[test]
+    fn test_validate_with_agent_type_missing_required() {
+        let input_structure =
+            serde_yaml::from_str::<SupervisorConfig>(EXAMPLE_CONFIG_REPLACE_NOPATH).unwrap();
+        let normalized = normalize_supervisor_config(input_structure);
+        let agent_type = serde_yaml::from_str::<AgentType>(EXAMPLE_AGENT_YAML_REPLACE).unwrap();
+
+        let actual = validate_with_agent_type(normalized, &agent_type);
+
+        assert!(actual.is_err());
+        assert_eq!(
+            actual.unwrap_err(),
+            "Missing required key in config: deployment.on_host.path"
+        );
+    }
 
     const EXAMPLE_AGENT_YAML_REPLACE_WITH_DEFAULT: &str = r#"
 name: nrdot
@@ -240,46 +278,6 @@ meta:
         - path: ${deployment.on_host.args}/otelcol
           args: "-c ${deployment.on_host.args}"
 "#;
-
-    #[test]
-    fn test_validate_with_agent_type() {
-        let input_structure =
-            serde_yaml::from_str::<SupervisorConfig>(EXAMPLE_CONFIG_REPLACE).unwrap();
-        let normalized = normalize_supervisor_config(input_structure);
-        let agent_type = serde_yaml::from_str::<AgentType>(EXAMPLE_AGENT_YAML_REPLACE).unwrap();
-
-        let expected = Map::from([
-            (
-                "deployment.on_host.args".to_string(),
-                TrivialValue::String("--verbose true".to_string()),
-            ),
-            (
-                "deployment.on_host.path".to_string(),
-                TrivialValue::String("/etc".to_string()),
-            ),
-        ]);
-        let actual = validate_with_agent_type(normalized, &agent_type).unwrap();
-
-        assert_eq!(expected, actual);
-    }
-
-    #[test]
-    fn test_validate_with_agent_type_missing_required() {
-        let input_structure =
-            serde_yaml::from_str::<SupervisorConfig>(EXAMPLE_CONFIG_REPLACE_NOPATH).unwrap();
-        let normalized = normalize_supervisor_config(input_structure);
-        let agent_type = serde_yaml::from_str::<AgentType>(EXAMPLE_AGENT_YAML_REPLACE).unwrap();
-
-        let actual = validate_with_agent_type(normalized, &agent_type);
-
-        println!("{:#?}", actual);
-
-        assert!(actual.is_err());
-        assert_eq!(
-            actual.unwrap_err(),
-            "Missing required key in config: deployment.on_host.path"
-        );
-    }
 
     #[test]
     fn test_validate_with_default() {
