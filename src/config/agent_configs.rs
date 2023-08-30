a57d9f0b71003726c337985d9f805901ef2d70c1
David Sánchez <davidsanchez@newrelic.com>
2023-08-30 11:09:03 +0100

    chore: remove comment

--- src/config/agent_configs.rs
+++ src/config/agent_configs.rs
@@ -2,44 +2,6 @@ use std::{collections::HashMap, fmt::Display};
 
 use serde::Deserialize;
 
-/*
-The structures below assume a config similar to the following:
-
-```yaml
-agents:
-    nr_infra_agent:
-        restart_policy:
-            backoff_strategy:
-                type: fixed
-                backoff_delay_seconds: 3
-                max_retries: 3
-                last_retry_interval_seconds: hello
-        config: {} # Some arbitrary values passed to the agent itself.
-        # TODO: What should we do with `bin'/`args` for custom agents?
-
-agents:
-    nrdot_gw:
-        type: newrelic/nrdot:0.1.1
-        config: config-nrdot-gw.yaml
-    nrdot_collector:
-        type: newrelic/nrdot:0.1.1
-        config: config-nrdot-collector.yaml
-
-agent_type
-namespace, name, version
-->getFQN() -> namespace/name:verison
-
-agent_config (variables file) no reference to type
-
-supervisor_config
-namespace/name:verison
-
-Agent_type registry
-namespace/name:version -> AgentType
-
-```
- */
-
 #[derive(Debug, Deserialize, PartialEq, Clone, Hash, Eq)]
 pub struct AgentID(pub String);
 
