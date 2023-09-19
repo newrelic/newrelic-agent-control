d1b06af982781e80b39ea08208bb97b8c160518e
David Sánchez <davidsanchez@newrelic.com>
2023-09-19 10:34:57 +0100

    feat(NR-160806): pass identifying attributes (#166)
    * NR-160806 add identifying attributes
    
    * feat: populate agent settings
    
    * chore: update Cargo manifests
    
    * test: fix expected input to build()
    
    * chore: update 3p notices
    
    * feat: add hostname to superagent
    
    * test: minimal start_settings check
    
    * test: examples of agent type FQN input (to review)
    
    * fix: test expected output
    
    * refactor: indicate lack of Windows support
    
    * build(deps): update opamp-rs
    
    * chore: update 3p notices
    
    * fix: use "host.name"
    
    * feat: use current infra-agent name in exec
    
    ---------
    
    Co-authored-by: Ruben Ruiz de Gauna <rubenruizdegauna@proton.me>

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -80,8 +80,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
 const NEWRELIC_INFRA_TYPE: &str = r#"
 namespace: newrelic
-name: newrelic-infra
-version: 1.39.1
+name: com.newrelic.infrastructure_agent
+version: 1.47.1
 variables:
   config:
     description: "Newrelic infra configuration yaml"
