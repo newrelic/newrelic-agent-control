5cd3823789c78d30e9b50bbf2107f9f4e85a53e0
Guillermo Sanchez Gavier <gsanchez@newrelic.com>
2023-10-20 11:39:37 +0200

    feat(CI): container image build (NR-168645) (#208)

--- build/scripts/replace_go_with_rust.sh
+++ build/scripts/replace_go_with_rust.sh
@@ -1,36 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-RUST_VERSION="1.71.1"
-
-# remove go generated files
-# rm -rf ./target/**
-
-# compile production version of rust agent
-
 if [ "$ARCH" = "arm64" ];then
   BINARY_PATH="./dist/newrelic-super-agent_linux_${ARCH}/newrelic-super-agent"
-  ARCH_NAME="aarch64"
 fi
 
 if [ "$ARCH" = "amd64" ];then
   BINARY_PATH="./dist/newrelic-super-agent_linux_${ARCH}_v1/newrelic-super-agent"
-  ARCH_NAME="x86_64"
-fi
-
-#rm "${BINARY_PATH}"
-
-docker build -t "rust-cross-${ARCH_NAME}" -f ./build/rust.Dockerfile --build-arg ARCH_NAME="${ARCH_NAME}" .
-
-if [ "${CI}" = "true" ]; then
-    CARGO_HOME=/tmp/.cargo cargo fetch
-    docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/app -v /tmp/.cargo:/usr/src/app/.cargo rust-cross-"${ARCH_NAME}"
-else
-    docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/app rust-cross-"${ARCH_NAME}"
 fi
 
 # move rust compiled files into goreleaser generated locations
-cp "./target/${ARCH_NAME}-unknown-linux-gnu/release/newrelic-super-agent" "${BINARY_PATH}"
+cp "./bin/newrelic-super-agent-${ARCH}" "${BINARY_PATH}"
 
 # validate
 docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app ubuntu /bin/bash -c "apt-get update && apt-get install tree -y && tree ./"
+
