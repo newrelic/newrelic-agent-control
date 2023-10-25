5a14b8fbff792f654528652e083633e2d6f9d319
David Sánchez <davidslt+git@pm.me>
2023-10-25 11:41:18 +0100

    style(NR-170461): detailed logging for OpAMP errors (#205)
    * style(NR-170461): detailed opamp error logging
    
    * fix: error message formatting
    
    * style: formatting
    
    * chore: update opamp-rs dependency

--- src/super_agent/callbacks.rs
+++ src/super_agent/callbacks.rs
@@ -1,10 +1,13 @@
 use opamp_client::{
+    error::ConnectionError,
+    http::HttpClientError,
     opamp::proto::{
         EffectiveConfig, OpAmpConnectionSettings, ServerErrorResponse, ServerToAgentCommand,
     },
     operation::callbacks::{Callbacks, MessageData},
 };
 use thiserror::Error;
+use tracing::error;
 
 pub struct AgentCallbacks;
 
@@ -24,7 +27,9 @@ impl Callbacks for AgentCallbacks {
         Ok(())
     }
 
-    fn on_connect_failed(&self, _err: Self::Error) {}
+    fn on_connect_failed(&self, err: ConnectionError) {
+        log_on_http_status_code(&err);
+    }
 
     fn get_effective_config(&self) -> Result<EffectiveConfig, Self::Error> {
         Ok(EffectiveConfig::default())
@@ -38,4 +43,23 @@ impl Callbacks for AgentCallbacks {
     }
 
     fn on_opamp_connection_settings_accepted(&self, _settings: &OpAmpConnectionSettings) {}
+}
+
+fn log_on_http_status_code(err: &ConnectionError) {
+    // Check if the error comes from receiving an undesired HTTP status code
+    if let ConnectionError::HTTPClientError(HttpClientError::UnsuccessfulResponse(code, reason)) =
+        &err
+    {
+        const STATUS_CODE_MSG: &str = "Received HTTP status code";
+        match code {
+            400 => error!("{STATUS_CODE_MSG} {code} ({reason}). The request was malformed. Possible reason: invalid ULID."),
+            401 => error!("{STATUS_CODE_MSG} {code} ({reason}). Check for missing or invalid license key."
+            ),
+            403 => error!("{STATUS_CODE_MSG} {code} ({reason}). The account provided is not allowed to use this resource."),
+            404 => error!("{STATUS_CODE_MSG} {code} ({reason}). The requested resource was not found."),
+            415 => error!("{STATUS_CODE_MSG} {code} ({reason}). Content-Type or Content-Encoding for the HTTP request was wrong."),
+            500 => error!("{STATUS_CODE_MSG} {code} ({reason}). Server-side problem."),
+            _ => error!("{STATUS_CODE_MSG} {code} ({reason}). Reasons unknown"),
+        }
+    }
 }
