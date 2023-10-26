2cf9d17a283ec06a3e8ab636ab2107e6d19312aa
alvarocabanas <acabanas@newrelic.com>
2023-10-26 16:17:59 +0200

    Return error on wrong_path and add test

--- src/config/remote_config_hash.rs
+++ src/config/remote_config_hash.rs
@@ -28,6 +28,9 @@ pub enum HashRepositoryError {
 
     #[error("file reader error: `{0}`")]
     FileRead(#[from] FileReaderError),
+
+    #[error("no path found")]
+    WrongPath,
 }
 
 impl Hash {
@@ -99,12 +102,14 @@ where
     }
 
     fn get(&self, agent_id: AgentID) -> Result<Hash, HashRepositoryError> {
-        let mut hash_file_path = self.conf_path.clone();
-        let hash_file = format!("{}.{}", agent_id.0.as_str(), HASH_FILE_EXTENSION);
-        hash_file_path.push(hash_file);
-        let contents = self.file_reader.read(hash_file_path.to_str().unwrap())?;
-        let result = serde_yaml::from_str(&contents);
-        Ok(result?)
+        let mut conf_path = self.conf_path.clone();
+        let hash_path = self.hash_file_path(&agent_id, &mut conf_path).to_str();
+        if let Some(path) = hash_path {
+            let contents = self.file_reader.read(path)?;
+            let result = serde_yaml::from_str(&contents);
+            return Ok(result?);
+        }
+        Err(HashRepositoryError::WrongPath)
     }
 }
 
@@ -113,6 +118,12 @@ where
     R: FileReader,
     W: Writer,
 {
+    fn hash_file_path<'a>(&'a self, agent_id: &AgentID, path: &'a mut PathBuf) -> &Path {
+        let hash_file = format!("{}.{}", agent_id.0.as_str(), HASH_FILE_EXTENSION);
+        path.push(hash_file);
+        path
+    }
+
     // Wrapper for linux with unix specific permissions
     #[cfg(target_family = "unix")]
     fn write(&self, path: &Path, content: String) -> Result<(), WriteError> {
@@ -126,9 +137,19 @@ where
 ////////////////////////////////////////////////////////////////////////////////////
 #[cfg(test)]
 pub mod test {
-    use crate::config::remote_config_hash::{Hash, HashRepository, HashRepositoryError};
-    use crate::config::super_agent_configs::AgentID;
+    #[cfg(target_family = "unix")]
+    use std::os::unix::fs::PermissionsExt;
+
     use mockall::mock;
+    use std::fs::Permissions;
+    use std::path::PathBuf;
+    use crate::config::super_agent_configs::AgentID;
+    use crate::config::persister::config_persister_file::FILE_PERMISSIONS;
+    use crate::config::persister::config_writer_file::test::MockFileWriterMock;
+    use crate::config::persister::config_writer_file::Writer;
+    use crate::file_reader::FileReader;
+    use crate::file_reader::test::MockFileReaderMock;
+    use super::{Hash, HASH_FILE_EXTENSION, HashRepository, HashRepositoryError, HashRepositoryFile};
     ////////////////////////////////////////////////////////////////////////////////////
     // Mock
     ////////////////////////////////////////////////////////////////////////////////////
@@ -141,5 +162,67 @@ pub mod test {
 
             fn get(&self, agent_id: AgentID) -> Result<Hash, HashRepositoryError>;
         }
+    }
+
+    impl<R, W> HashRepositoryFile<R, W>
+        where
+            R: FileReader,
+            W: Writer,
+    {
+        pub fn with_mocks(
+            file_reader: R,
+            file_writer: W,
+            conf_path: PathBuf,
+        ) -> Self {
+            HashRepositoryFile {
+                file_reader,
+                file_writer,
+                conf_path,
+            }
+        }
+    }
+
+    #[test]
+    fn test_save_and_get_hash() {
+        let some_path = PathBuf::from("some/path");
+        let mut file_writer_mock = MockFileWriterMock::new();
+        let mut file_reader_mock = MockFileReaderMock::new();
+        let file_permissions = Permissions::from_mode(FILE_PERMISSIONS);
+        let agent_id = AgentID::new("SomeAgentID");
+        let mut hash = Hash::new( "123456789".to_string());
+        hash.apply();
+
+        // This indentation and the single quotes is to match serde yaml
+        let content= r#"hash: '123456789'
+applied: true
+"#;
+
+        let mut expected_path = some_path.clone();
+        expected_path.push(format!("{}.{}", agent_id.0.as_str(), HASH_FILE_EXTENSION));
+
+        file_reader_mock.should_read(
+            expected_path.to_str().unwrap().to_string(),
+            content.to_string(),
+        );
+        file_writer_mock.should_write(
+            expected_path.as_path(),
+            content.to_string(),
+            file_permissions,
+        );
+
+        let hash_repository = HashRepositoryFile::with_mocks(
+            file_reader_mock,
+            file_writer_mock,
+            some_path,
+        );
+
+        let result = hash_repository.save(
+            agent_id.clone(),
+            hash.clone(),
+        );
+        assert_eq!((), result.unwrap());
+
+        let result = hash_repository.get(agent_id.clone());
+        assert_eq!(hash, result.unwrap());
     }
 }
