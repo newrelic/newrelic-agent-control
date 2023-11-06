03c14b586eafe9a383fba357d0c5dff3e8f29bcd
Christian Felipe Álvarez <calvarez@newrelic.com>
2023-11-06 16:50:02 +0100

    chore: simplify reflector and include unit tests

--- src/k8s/reader.rs
+++ src/k8s/reader.rs
@@ -2,15 +2,15 @@ use futures::StreamExt;
 use std::future;
 
 use kube::{
-    core::{DynamicObject, GroupVersionKind},
-    discovery,
+    core::DynamicObject,
+    discovery::ApiResource,
     runtime::{
         reflector::{self, store::Writer},
         watcher, WatchStreamExt,
     },
-    Api, Client, Resource,
+    Api, Client,
 };
-use tokio::task::JoinHandle;
+use tokio::task::{AbortHandle, JoinHandle};
 use tracing::warn;
 
 use super::error::K8sError;
@@ -33,32 +33,25 @@ impl ReflectorBuilder {
         self
     }
 
+    /// Builds the DynamicObject reflector using the builder.
     pub async fn dynamic_object_reflector(
         self,
-        gvk: GroupVersionKind,
-    ) -> Result<Reflector<DynamicObject>, K8sError> {
-        let (api_resource, _) = discovery::pinned_kind(&self.client, &gvk).await?;
+        api_resource: &ApiResource,
+    ) -> Result<DynamicObjectReflector, K8sError> {
         let api: Api<DynamicObject> =
-            Api::namespaced_with(self.client, &self.namespace, &api_resource);
+            Api::namespaced_with(self.client, &self.namespace, api_resource);
 
-        let writer: Writer<DynamicObject> = reflector::store::Writer::new(api_resource);
-        let reader = writer.as_reader();
+        let writer: Writer<DynamicObject> = reflector::store::Writer::new(api_resource.to_owned());
 
         let mut wc = watcher::Config::default();
         wc.label_selector = self.label_selector;
         wc.field_selector = self.field_selector;
 
-        let writer_task = Reflector::<DynamicObject>::start_reflector(writer, api, wc);
-
-        reader.wait_until_ready().await?;
-
-        Ok(Reflector {
-            reader,
-            writer_task,
-        })
+        DynamicObjectReflector::new(api, writer, wc).await
     }
 }
 
+/// Returns reflector builder, it consumes the provided client.
 pub fn reflector_builder(client: Client, namespace: String) -> ReflectorBuilder {
     ReflectorBuilder {
         client,
@@ -68,20 +61,28 @@ pub fn reflector_builder(client: Client, namespace: String) -> ReflectorBuilder 
     }
 }
 
-pub struct Reflector<K>
-where
-    K: 'static + Resource + Clone,
-    K::DynamicType: Eq + std::hash::Hash,
-{
-    reader: reflector::Store<K>,
-    writer_task: JoinHandle<()>,
+pub struct DynamicObjectReflector {
+    reader: reflector::Store<DynamicObject>,
+    writer_task: AbortHandle,
 }
 
-impl<K> Reflector<K>
-where
-    K: 'static + Resource + Clone,
-    K::DynamicType: Eq + std::hash::Hash,
-{
+impl DynamicObjectReflector {
+    async fn new(
+        api: Api<DynamicObject>,
+        writer: Writer<DynamicObject>,
+        wc: watcher::Config,
+    ) -> Result<Self, K8sError> {
+        let reader = writer.as_reader();
+        let writer_task = Self::start_reflector(writer, api, wc).abort_handle();
+
+        reader.wait_until_ready().await?;
+
+        Ok(DynamicObjectReflector {
+            reader,
+            writer_task,
+        })
+    }
+
     fn start_reflector(
         writer: Writer<DynamicObject>,
         api: Api<DynamicObject>,
@@ -103,18 +104,67 @@ where
     }
 
     // A copy of the reader
-    // TODO: we are cloning it for now, but we need to check that's the best approach considering its usage.
-    pub fn reader(&self) -> reflector::Store<K> {
+    // TODO: we are cloning it for now, but we need to check what's the best approach considering its usage.
+    pub fn reader(&self) -> reflector::Store<DynamicObject> {
         self.reader.clone()
     }
 }
 
-impl<K> Drop for Reflector<K>
-where
-    K: 'static + Resource + Clone,
-    K::DynamicType: Eq + std::hash::Hash,
-{
+impl Drop for DynamicObjectReflector {
     fn drop(&mut self) {
         self.writer_task.abort();
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use http::{Request, Response};
+    use hyper::Body;
+    use kube::core::GroupVersionKind;
+    use tokio::sync::oneshot::{channel, Sender};
+
+    async fn mocked_writer_task(_send: Sender<()>) {
+        // _send will be dropped when the task is finished
+        loop {
+            tokio::time::sleep(tokio::time::Duration::from_micros(10)).await;
+        }
+    }
+
+    #[tokio::test]
+    async fn test_reflector_abort_writer_on_drop() {
+        let gvk = GroupVersionKind::gvk("test.group", "v1", "TestKind");
+        let writer = reflector::store::Writer::new(ApiResource::from_gvk(&gvk));
+        let reader = writer.as_reader();
+        let (send, recv) = channel();
+        let reflector = DynamicObjectReflector {
+            reader,
+            writer_task: tokio::spawn(mocked_writer_task(send)).abort_handle(),
+        };
+        drop(reflector);
+        assert!(recv.await.is_err()); // must get err because the sender is dropped.
+    }
+
+    #[tokio::test]
+    // The client's requires async
+    async fn test_reflector_builder_options() {
+        let (mock_service, _) = tower_test::mock::pair::<Request<Body>, Response<Body>>();
+        let client = kube::Client::new(mock_service, "default");
+
+        let builder = reflector_builder(client, "builder-ns".into())
+            .with_fields("field1==v1,field2!=value2".into())
+            .with_labels("prometheus.io/scrape=true".into());
+
+        assert_eq!(builder.namespace, "builder-ns".to_string());
+
+        assert_eq!(
+            builder.field_selector,
+            Some("field1==v1,field2!=value2".to_string())
+        );
+
+        assert_eq!(
+            builder.label_selector,
+            Some("prometheus.io/scrape=true".to_string())
+        );
     }
 }
