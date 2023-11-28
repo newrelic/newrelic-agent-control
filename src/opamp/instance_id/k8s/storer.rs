5e929a65c292a40c17760b3102d9a043e372acf2
PaoloGallina <paologallina1992@gmail.com>
2023-11-28 08:39:57 +0100

    feat(instanceID):improve tests (#284)
    * feat(instanceID):improve tests

--- src/opamp/instance_id/k8s/storer.rs
+++ src/opamp/instance_id/k8s/storer.rs
@@ -1,14 +1,17 @@
 use crate::k8s;
-use crate::k8s::executor::K8sExecutor;
 use crate::opamp::instance_id::getter::DataStored;
 use crate::opamp::instance_id::storer::InstanceIDStorer;
+
+#[cfg_attr(test, mockall_double::double)]
+use crate::k8s::executor::K8sExecutor;
 
 pub struct Storer {
     k8s_executor: K8sExecutor,
     configmap_prefix: String,
 }
 
-const CM_KEY: &str = "ulid-data";
+pub const CM_KEY: &str = "ulid";
+const CM_PREFIX: &str = "ulid-data";
 
 #[derive(thiserror::Error, Debug)]
 pub enum StorerError {
@@ -39,7 +42,7 @@ impl Storer {
     pub fn new(k8s_executor: K8sExecutor) -> Self {
         Self {
             k8s_executor,
-            configmap_prefix: CM_KEY.to_string(),
+            configmap_prefix: CM_PREFIX.to_string(),
         }
     }
 
@@ -79,4 +82,131 @@ fn build_cm_name(prefix: &String, agent_id: &str) -> String {
     cm_name.push_str(agent_id);
 
     cm_name.to_owned()
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::{Storer, CM_KEY};
+    use crate::k8s::error::K8sError;
+    use crate::k8s::executor::MockK8sExecutor;
+    use crate::opamp::instance_id::getter::DataStored;
+    use crate::opamp::instance_id::storer::InstanceIDStorer;
+    use crate::opamp::instance_id::InstanceID;
+    use mockall::predicate;
+
+    const AGENT_NAME: &str = "agent1";
+    const DATA_STORED: &str = "ulid: 01HFW1YZKYWHTGC0WMWPNR4P4K
+identifiers:
+  cluster_name: ''
+";
+    const ULID: &str = "01HFW1YZKYWHTGC0WMWPNR4P4K";
+    const EXPECTED_CM_NAME: &str = "ulid-data-agent1";
+
+    #[test]
+    fn test_input_parameters_dependencies() {
+        // In this tests we are checking that the parameters are passed as expected and that cm names are built in the proper way
+        // The output of the commands are checked in following tests.
+        let mut m = MockK8sExecutor::default();
+        m.expect_get_configmap_key()
+            .once()
+            .with(
+                predicate::function(|name| name == EXPECTED_CM_NAME),
+                predicate::function(|key| key == CM_KEY),
+            )
+            .returning(move |_, _| Err(K8sError::CMMalformed()));
+        m.expect_set_configmap_key()
+            .once()
+            .with(
+                predicate::function(|name| name == EXPECTED_CM_NAME),
+                predicate::function(|key| key == CM_KEY),
+                predicate::function(|ds| ds == DATA_STORED),
+            )
+            .returning(move |_, _, _| Err(K8sError::CMMalformed()));
+        let s = Storer::new(m);
+        let _ = s.get(AGENT_NAME);
+        let _ = s.set(
+            AGENT_NAME,
+            &DataStored {
+                ulid: InstanceID::new(ULID.to_string()),
+                identifiers: Default::default(),
+            },
+        );
+    }
+
+    #[test]
+    fn test_get_error() {
+        let mut m = MockK8sExecutor::default();
+        m.expect_get_configmap_key()
+            .once()
+            .returning(move |_, _| Err(K8sError::CMMalformed()));
+        let s = Storer::new(m);
+
+        let id = s.get(AGENT_NAME);
+        assert!(id.is_err())
+    }
+
+    #[test]
+    fn test_get_not_found() {
+        let mut m = MockK8sExecutor::default();
+        m.expect_get_configmap_key()
+            .once()
+            .returning(move |_, _| Ok(None));
+        let s = Storer::new(m);
+
+        let id = s.get(AGENT_NAME);
+        assert!(id.is_ok());
+        assert!(id.unwrap().is_none());
+    }
+
+    #[test]
+    fn test_get_found_data() {
+        let mut m = MockK8sExecutor::default();
+        m.expect_get_configmap_key()
+            .once()
+            .returning(move |_, _| Ok(Some(DATA_STORED.to_string())));
+        let s = Storer::new(m);
+
+        let id = s.get(AGENT_NAME);
+        assert!(id.is_ok());
+        let id_un = id.unwrap();
+        assert!(id_un.is_some());
+        let td = id_un.unwrap();
+        assert_eq!(td.ulid, InstanceID::new(ULID.to_string()))
+    }
+
+    #[test]
+    fn test_set_error() {
+        let mut m = MockK8sExecutor::default();
+        m.expect_set_configmap_key()
+            .once()
+            .returning(move |_, _, _| Err(K8sError::CMMalformed()));
+        let s = Storer::new(m);
+
+        let id = s.set(
+            AGENT_NAME,
+            &DataStored {
+                ulid: Default::default(),
+                identifiers: Default::default(),
+            },
+        );
+        assert!(id.is_err())
+    }
+
+    #[test]
+    fn test_set_succeeded() {
+        let mut m = MockK8sExecutor::default();
+        m.expect_set_configmap_key()
+            .once()
+            .returning(move |_, _, _| Ok(()));
+        let s = Storer::new(m);
+
+        let id = s.set(
+            AGENT_NAME,
+            &DataStored {
+                ulid: Default::default(),
+                identifiers: Default::default(),
+            },
+        );
+        assert!(id.is_ok())
+    }
 }
--- test/k8s/ulid_storer.rs
+++ test/k8s/ulid_storer.rs
@@ -1,8 +1,9 @@
 use crate::common::K8sEnv;
+use k8s_openapi::api::core::v1::ConfigMap;
+use kube::Api;
 use newrelic_super_agent::opamp::instance_id::{
-    self,
     getter::{InstanceIDGetter, ULIDInstanceIDGetter},
-    Identifiers,
+    GetterError, Identifiers, CM_KEY,
 };
 
 const AGENT_ID_TEST: &str = "agent-id-test";
@@ -20,7 +21,7 @@ async fn k8s_ulid_persister() {
     let test_ns = test.test_namespace().await;
 
     let instance_id_getter =
-        ULIDInstanceIDGetter::try_with_identifiers(test_ns, Identifiers::default())
+        ULIDInstanceIDGetter::try_with_identifiers(test_ns.clone(), Identifiers::default())
             .await
             .unwrap();
 
@@ -39,4 +40,42 @@ async fn k8s_ulid_persister() {
 
     let invalid = instance_id_getter.get(AGENT_INVALID_ID_TEST);
     assert!(invalid.is_err());
+
+    // Verify also that the status of the cluster is the expected one
+    let cm_client: Api<ConfigMap> =
+        Api::<ConfigMap>::namespaced(test.client.clone(), test_ns.clone().as_str());
+
+    let cm = cm_client.get("ulid-data-agent-id-test").await;
+    assert!(cm.is_ok());
+    let cm_un = cm.unwrap();
+    assert!(cm_un.data.is_some());
+    assert!(cm_un.data.unwrap().contains_key(CM_KEY));
+
+    let cm = cm_client.get("ulid-data-agent-different-id-test").await;
+    assert!(cm.is_ok());
+    let cm_un = cm.unwrap();
+    assert!(cm_un.data.is_some());
+    assert!(cm_un.data.unwrap().contains_key(CM_KEY));
+}
+
+// tokio test runs with 1 thread by default causing deadlock when executing `block_on` code during test helper drop.
+#[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+#[ignore = "needs k8s cluster"]
+async fn k8s_ulid_persister_fail() {
+    // This tests cover the error path for the storer. The namespace does not exist, every call is going to fail.
+
+    let test_ns = "test-not-existing-namespace";
+
+    let instance_id_getter =
+        ULIDInstanceIDGetter::try_with_identifiers(test_ns.to_string(), Identifiers::default())
+            .await
+            .unwrap();
+
+    let e = instance_id_getter.get(AGENT_ID_TEST);
+    match e.unwrap_err() {
+        GetterError::K8sClientInitialization(_) => {
+            panic!("this is unexpected, the test should fail contacting the k8s API")
+        }
+        GetterError::Persisting(_) => return,
+    }
 }
