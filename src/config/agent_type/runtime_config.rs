87856c767aa97b77fe5860a4d7e5cb5dd1b89a9b
Christian <calvarez@newrelic.com>
2023-12-12 09:30:27 +0100

    NR-182330: agent's type k8s runtime config (#304)

--- src/config/agent_type/runtime_config.rs
+++ src/config/agent_type/runtime_config.rs
@@ -13,6 +13,7 @@ pub struct RuntimeConfig {
 #[derive(Debug, Deserialize, Default, Clone, PartialEq)]
 pub struct Deployment {
     pub on_host: Option<OnHost>,
+    pub k8s: Option<K8s>,
 }
 
 /// The definition for an on-host supervisor.
@@ -63,5 +64,76 @@ impl Env {
                     .unwrap()
             })
             .collect()
+    }
+}
+
+/// The definition for an K8s supervisor.
+///
+/// It contains the instructions of what are the agent resources to be managed by the super-agent.
+#[derive(Debug, Deserialize, Default, Clone, PartialEq)]
+pub struct K8s {
+    pub objects: HashMap<String, K8sObject>,
+}
+
+/// A K8s object, usually a CR, to be managed by the super-agent.
+// TODO: at lest, the spec should be templatable.
+#[derive(Debug, Deserialize, Default, Clone, PartialEq)]
+pub struct K8sObject {
+    #[serde(rename = "apiVersion")]
+    pub api_version: String,
+    pub kind: String,
+    // TODO: include metadata
+    #[serde(default, flatten)]
+    pub fields: serde_yaml::Mapping,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const RUNTIME_WITH_K8S_DEPLOYMENT: &str = r#"
+deployment:
+  k8s:
+    objects:
+      cr1:
+        apiVersion: super_agent.version/v0beta1
+        kind: Foo
+        spec:
+          anyKey: any-value
+      cr2:
+        apiVersion: super_agent.version/v0beta1
+        kind: Foo2
+        # no additional fields
+      cr3:
+        apiVersion: super_agent.version/v0beta1
+        kind: Foo
+        key: value # no spec field
+"#;
+
+    #[test]
+    fn test_k8s_object() {
+        let rtc: RuntimeConfig = serde_yaml::from_str(RUNTIME_WITH_K8S_DEPLOYMENT).unwrap();
+        assert!(rtc.deployment.on_host.is_none());
+        let k8s = rtc.deployment.k8s.unwrap();
+        assert_eq!("Foo".to_string(), k8s.objects["cr1"].kind);
+        assert_eq!(
+            "super_agent.version/v0beta1".to_string(),
+            k8s.objects["cr1"].api_version
+        );
+        assert_eq!(
+            &serde_yaml::Value::String("any-value".into()),
+            k8s.objects["cr1"]
+                .fields
+                .get("spec")
+                .unwrap()
+                .get("anyKey")
+                .unwrap()
+        );
+        assert_eq!("Foo2".to_string(), k8s.objects["cr2"].kind);
+        assert_eq!(serde_yaml::Mapping::default(), k8s.objects["cr2"].fields);
+        assert_eq!(
+            &serde_yaml::Value::String("value".into()),
+            k8s.objects["cr3"].fields.get("key").unwrap()
+        );
     }
 }
