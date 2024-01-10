076f7adcc4fc44474868f9af32590457f419ebb0
David Sánchez <davidslt+git@pm.me>
2024-01-10 20:03:22 +0000

    style: fmt
    Signed-off-by: David Sánchez <davidslt+git@pm.me>

--- super-agent/src/sub_agent/values/values_repository.rs
+++ super-agent/src/sub_agent/values/values_repository.rs
@@ -555,7 +555,8 @@ pub mod test {
         let mut final_agent = FinalAgent::default();
         final_agent.set_capabilities(default_capabilities());
 
-        file_reader.should_not_read_io_error(Path::new("some/local/path/some_agent_id/values.yaml"));
+        file_reader
+            .should_not_read_io_error(Path::new("some/local/path/some_agent_id/values.yaml"));
 
         let repo = ValuesRepositoryFile::with_mocks(
             file_writer,
