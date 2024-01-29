a1c2028727c1e92e33d5a2d5c1eb8f925dd81eff
Grigorii Merkushev <brushknight@gmail.com>
2024-01-29 17:40:04 +0100

    Debug

--- build/scripts/build_binary.sh
+++ build/scripts/build_binary.sh
@@ -26,6 +26,8 @@ if [ -z "${BUILD_FEATURE}" ]; then
     echo "BUILD_FEATURE not provided; defaulting to 'onhost'."
 fi
 
+echo "Arch: ${ARCH_NAME}"
+
 docker build --platform linux/amd64 -t "rust-cross-${ARCH_NAME}-${BIN}" -f ./build/rust.Dockerfile --build-arg ARCH_NAME="${ARCH_NAME}" --build-arg BUILD_MODE="${BUILD_MODE}" --build-arg BUILD_FEATURE="${BUILD_FEATURE}" --build-arg BUILD_BIN="${BIN}" .
 
 CARGO_HOME=/tmp/.cargo cargo fetch
