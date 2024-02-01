7581cf7ea03cc322c6b15394f07e782f54b75de7
Grigorii Merkushev <brushknight@gmail.com>
2024-02-01 12:05:58 +0100

    add validate download embedded

--- build/scripts/download_embedded.sh
+++ build/scripts/download_embedded.sh
@@ -7,3 +7,5 @@ GOCACHE=/tmp/.gocache
 # download assets
 docker run --rm --user "$(id -u)":"$(id -g)" -e GOCACHE="$GOCACHE" -e ARCH="$ARCH" -e STAGING="$STAGING" -v "$PWD":/usr/src/app -w /usr/src/app golang:1.20 make build/embedded
 
+# validate
+docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app ubuntu /bin/bash -c "apt-get update && apt-get install tree -y && tree ./build/embedded"
