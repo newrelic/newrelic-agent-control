f8c3b4606cc4249d934d165d699d9b78026dd229
David Sánchez <davidslt+git@pm.me>
2024-02-19 20:07:09 +0000

    test(wip): file logging

---
+++ super-agent/test/on_host/logging/level.rs
@@ -0,0 +1,7 @@
+const DEBUG_LOG_LEVEL_CONFIG: &str = r#"
+log:
+  level: info
+"#;
+
+#[test]
+fn default_log_level() {}
--- super-agent/test/on_host/mod.rs
+++ super-agent/test/on_host/mod.rs
@@ -1,5 +1,6 @@
 mod cli;
 mod command;
 mod config_persister;
+mod logging;
 mod supervisor;
 mod values_repository;
