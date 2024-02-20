99c6bd29ad27f731994a1c0ba33b6604a99a394b
Roger Coll <rogercoll@protonmail.com>
2024-02-20 17:50:02 +0100

    NR-221680: add tests Makefile and fix ci

--- super-agent/test/on_host/id.rs
+++ super-agent/test/on_host/id.rs
@@ -4,118 +4,44 @@ use newrelic_super_agent::opamp::instance_id::IdentifiersProvider;
 use wiremock::matchers::{method, path};
 use wiremock::{Mock, MockServer, ResponseTemplate};
 
-// TODO: run only on Linux
+// test_cloud_id tests that the default IdentifiersProvider is able to retrieve the cloud instance
+// id for any cloud provider. It spawns a mock server and provides the mocked cloud metadata in the
+// endpoint specified by the TEST_IPV4_METADATA_ENDPOINT environment variable. Required format of
+// the TEST_IPV4_METADATA_ENDPOINT: "http://address/endpoint". For example: "http://127.0.0.1:4343/testing_metadata_endpoint"
+// Note that if the environment variable is not defined, the test will fail.
 #[tokio::test]
 async fn test_cloud_id() {
+    let (mut address, endpoint) =
+        konst::option::unwrap!(option_env!("TEST_IPV4_METADATA_ENDPOINT"))
+            .rsplit_once("/")
+            .expect("Not a valid endpoint, expected format: http://address/endpoint");
+    address = address
+        .strip_prefix("http://")
+        .expect("Not a valid endpoint, expected format: http://address/endpoint");
+
     let mock_server = MockServer::builder()
-        .listener(TcpListener::bind("127.0.0.1:4343").unwrap())
+        .listener(TcpListener::bind(address).unwrap())
         .start()
         .await;
 
-    // AWS metadata endpoint mock
-    let aws_mock = Mock::given(method("GET"))
-        .and(path("/aws_testing_metadata_endpoint"))
-        .respond_with(ResponseTemplate::new(200).set_body_raw(
-            r#"
-{
-  "accountId": "012345678901",
-  "architecture": "x86_64",
-  "availabilityZone": "eu-central-1c",
-  "billingProducts": null,
-  "devpayProductCodes": null,
-  "marketplaceProductCodes": null,
-  "imageId": "ami-01ff76477b9b30d59",
-  "instanceId": "i-123456787d725bbe7",
-  "instanceType": "t3a.nano",
-  "kernelId": null,
-  "pendingTime": "2022-06-20T09:51:52Z",
-  "privateIp": "172.29.40.136",
-  "ramdiskId": null,
-  "region": "eu-central-1",
-  "version": "2017-09-30"
-}
-"#,
-            "application/json",
-        ));
-    let aws_mock_guard = mock_server.register_as_scoped(aws_mock).await;
+    let cloud_data = [
+        (super::consts::AWS_VM_RESPONSE, "i-123456787d725bbe7"),
+        (super::consts::GCP_VM_RESPONSE, "6331980990053453154"),
+        (
+            super::consts::AZURE_VM_RESPONSE,
+            "02aab8a4-74ef-476e-8182-f6d2ba4166a7",
+        ),
+    ];
 
-    // let aws_detector = AWSDetector::default();
-    //
-    // let id = aws_detector.detect().unwrap();
-    // Create a mock on the server
-    let id = IdentifiersProvider::default().provide().unwrap();
+    for (metadata_endpoint, expected_cloud_id) in cloud_data {
+        let endpoint_mock = Mock::given(method("GET")).and(path(endpoint)).respond_with(
+            ResponseTemplate::new(200).set_body_raw(metadata_endpoint, "application/json"),
+        );
+        let mock_guard = mock_server.register_as_scoped(endpoint_mock).await;
 
-    assert!(id.cloud_instance_id == "i-123456787d725bbe7".to_string());
-    drop(aws_mock_guard);
+        let id = IdentifiersProvider::default().provide().unwrap();
 
-    // GCP metadata endpoint mock
-    let endpoint_mock = Mock::given(method("GET"))
-        .and(path("/aws_testing_metadata_endpoint"))
-        .respond_with(ResponseTemplate::new(200).set_body_raw(
-            r#"
-{
-		"attributes": {},
-		"cpuPlatform": "Intel Haswell",
-		"description": "",
-		"disks": [
-			{
-				"deviceName": "mmacias-micro",
-				"index": 0,
-				"mode": "READ_WRITE",
-				"type": "PERSISTENT"
-			}
-		],
-		"hostname": "mmacias-micro.c.beyond-181918.internal",
-		"id": 6331980990053453154,
-		"image": "projects/debian-cloud/global/images/debian-9-stretch-v20171025",
-		"licenses": [
-			{
-				"id": "1000205"
-			}
-		],
-		"machineType": "projects/260890654058/machineTypes/f1-micro",
-		"maintenanceEvent": "NONE",
-		"name": "mmacias-micro",
-		"networkInterfaces": [
-			{
-				"accessConfigs": [
-					{
-						"externalIp": "104.154.137.202",
-						"type": "ONE_TO_ONE_NAT"
-					}
-				],
-				"forwardedIps": [],
-				"ip": "10.128.0.5",
-				"ipAliases": [],
-				"mac": "42:01:0a:80:00:05",
-				"network": "projects/260890654058/networks/default",
-				"targetInstanceIps": []
-			}
-		],
-		"preempted": "FALSE",
-		"scheduling": {
-			"automaticRestart": "TRUE",
-			"onHostMaintenance": "MIGRATE",
-			"preemptible": "FALSE"
-		},
-		"serviceAccounts": {},
-		"tags": [],
-		"virtualClock": {
-			"driftToken": "0"
-		},
-		"zone": "projects/260890654058/zones/us-central1-c"
-	}
-"#,
-            "application/json",
-        ));
-    let mock_guard = mock_server.register_as_scoped(endpoint_mock).await;
-
-    // let aws_detector = AWSDetector::default();
-    //
-    // let id = aws_detector.detect().unwrap();
-    // Create a mock on the server
-    let id = IdentifiersProvider::default().provide().unwrap();
-
-    assert!(id.cloud_instance_id == "6331980990053453154".to_string());
-    drop(mock_guard);
+        assert!(id.cloud_instance_id == expected_cloud_id.to_string());
+        drop(mock_guard);
+    }
 }
