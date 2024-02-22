ca9079321f4ae71d935487eca308af1cd8cdee75
Ruben Ruiz de Gauna <rubenruizdegauna@proton.me>
2024-02-22 15:51:49 +0100

    rename agent types constants

--- super-agent/src/agent_type/agent_type_registry.rs
+++ super-agent/src/agent_type/agent_type_registry.rs
@@ -1,8 +1,8 @@
 use crate::{
     agent_type::definition::AgentType,
     super_agent::defaults::{
-        NEWRELIC_INFRA_TYPE_1, NEWRELIC_INFRA_TYPE_2, NEWRELIC_INFRA_TYPE_3, NRDOT_TYPE_1,
-        NRDOT_TYPE_2,
+        NEWRELIC_INFRA_TYPE_0_0_1, NEWRELIC_INFRA_TYPE_0_0_2, NEWRELIC_INFRA_TYPE_0_1_0,
+        NRDOT_TYPE_0_0_1, NRDOT_TYPE_0_1_0,
     },
 };
 use log::debug;
@@ -33,19 +33,19 @@ impl Default for LocalRegistry {
         let mut local_agent_type_repository = LocalRegistry(HashMap::new());
         // save to unwrap(), default agent cannot be changed inline
         local_agent_type_repository
-            .store_from_yaml(NEWRELIC_INFRA_TYPE_1.as_bytes())
+            .store_from_yaml(NEWRELIC_INFRA_TYPE_0_0_1.as_bytes())
             .unwrap();
         local_agent_type_repository
-            .store_from_yaml(NEWRELIC_INFRA_TYPE_2.as_bytes())
+            .store_from_yaml(NEWRELIC_INFRA_TYPE_0_0_2.as_bytes())
             .unwrap();
         local_agent_type_repository
-            .store_from_yaml(NEWRELIC_INFRA_TYPE_3.as_bytes())
+            .store_from_yaml(NEWRELIC_INFRA_TYPE_0_1_0.as_bytes())
             .unwrap();
         local_agent_type_repository
-            .store_from_yaml(NRDOT_TYPE_1.as_bytes())
+            .store_from_yaml(NRDOT_TYPE_0_0_1.as_bytes())
             .unwrap();
         local_agent_type_repository
-            .store_from_yaml(NRDOT_TYPE_2.as_bytes())
+            .store_from_yaml(NRDOT_TYPE_0_1_0.as_bytes())
             .unwrap();
 
         if let Ok(file) =
