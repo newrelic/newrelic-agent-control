956c8c1c08d5cc8434195439711aa5eceac1b3a3
David Sánchez <davidslt+git@pm.me>
2024-03-01 11:14:54 +0000

    ci: do not edit example templates in place
    goreleaser complains about dirty state when doing this

--- build/scripts/download_collector_config.sh
+++ build/scripts/download_collector_config.sh
@@ -8,9 +8,17 @@ URL="https://raw.githubusercontent.com/newrelic/opentelemetry-collector-releases
 DEFAULT_CONFIG_FILENAME="nr-otel-collector-agent-linux.yaml"
 GATEWAY_CONFIG_FILENAME="nr-otel-collector-gateway.yaml"
 CONFIG_PLACEHOLDER="OTEL_COLLECTOR_CONFIG"
-TEMPLATE_DEFAULT="build/examples/values-nr-otel-collector-agent-linux-${AGENT_TYPE_VERSION}.yaml"
-TEMPLATE_GATEWAY="build/examples/values-nr-otel-collector-gateway-${AGENT_TYPE_VERSION}.yaml"
+TEMPLATE_DIR="build/example_templates"
+OUT_DIR="build/examples"
+TEMPLATE_DEFAULT="${TEMPLATE_DIR}/values-nr-otel-collector-agent-linux-${AGENT_TYPE_VERSION}.yaml"
+TEMPLATE_GATEWAY="${TEMPLATE_DIR}/values-nr-otel-collector-gateway-${AGENT_TYPE_VERSION}.yaml"
+FINAL_DEFAULT="${OUT_DIR}/values-nr-otel-collector-agent-linux-${AGENT_TYPE_VERSION}.yaml"
+FINAL_GATEWAY="${OUT_DIR}/values-nr-otel-collector-gateway-${AGENT_TYPE_VERSION}.yaml"
 OUTPUT_FILE="${BUILD_TMP_FOLDER}/examples/values-nr-otel-collector-${AGENT_TYPE_VERSION}.yaml"
+
+# Copy templates into their final locations (ignored by git)
+cp ${TEMPLATE_DEFAULT} ${FINAL_DEFAULT}
+cp ${TEMPLATE_GATEWAY} ${FINAL_GATEWAY}
 
 mkdir -p ${BUILD_TMP_FOLDER}
 
@@ -22,8 +30,8 @@ curl -s -o "${BUILD_TMP_FOLDER}/${GATEWAY_CONFIG_FILENAME}" "${URL}/${GATEWAY_CO
 docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app ubuntu /bin/bash -c "sed -i 's/^/  /g' ${BUILD_TMP_FOLDER}/${DEFAULT_CONFIG_FILENAME}"
 docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app ubuntu /bin/bash -c "sed -i 's/^/  /g' ${BUILD_TMP_FOLDER}/${GATEWAY_CONFIG_FILENAME}"
 # Replace nr-otel-collector sample file placeholder with nr-otel-config
-docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app ubuntu /bin/bash -c "sed -i -e '/${CONFIG_PLACEHOLDER}/{r ${BUILD_TMP_FOLDER}/${DEFAULT_CONFIG_FILENAME}' -e 'd}' ${TEMPLATE_DEFAULT}"
-docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app ubuntu /bin/bash -c "sed -i -e '/${CONFIG_PLACEHOLDER}/{r ${BUILD_TMP_FOLDER}/${GATEWAY_CONFIG_FILENAME}' -e 'd}' ${TEMPLATE_GATEWAY}"
+docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app ubuntu /bin/bash -c "sed -i -e '/${CONFIG_PLACEHOLDER}/{r ${BUILD_TMP_FOLDER}/${DEFAULT_CONFIG_FILENAME}' -e 'd}' ${FINAL_DEFAULT}"
+docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app ubuntu /bin/bash -c "sed -i -e '/${CONFIG_PLACEHOLDER}/{r ${BUILD_TMP_FOLDER}/${GATEWAY_CONFIG_FILENAME}' -e 'd}' ${FINAL_GATEWAY}"
 
 
 
