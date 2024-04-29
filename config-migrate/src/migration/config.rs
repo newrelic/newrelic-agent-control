aa297084b4a02c21ba5214f8b1e626d6887acfe4
Alvaro Cabanas <acabanas@newrelic.com>
2024-04-29 16:11:08 +0200

    Allow new agent_type versions to use latest lower agent_type to not have to repeat for each entry. (#600)

--- config-migrate/src/migration/config.rs
+++ config-migrate/src/migration/config.rs
@@ -1,8 +1,11 @@
 use newrelic_super_agent::super_agent::config::AgentTypeFQN;
 use serde::Deserialize;
+use serde_yaml::Error;
 use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
 use std::hash::{Hash, Hasher};
+use thiserror::Error;
+use tracing::error;
 
 pub const FILE_SEPARATOR: &str = ".";
 // Used to replace temporarily the . separator on files to not treat them as leafs on the hashmap
@@ -10,6 +13,15 @@ pub const FILE_SEPARATOR_REPLACE: &str = "#";
 
 pub type FilePath = String;
 pub type DirPath = String;
+
+#[derive(Error, Debug)]
+pub enum MigrationConfigError {
+    #[error("error parsing yaml: `{0}`")]
+    SerdeYaml(#[from] Error),
+
+    #[error("config mapping should not be empty`")]
+    EmptyConfigMapping,
+}
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct AgentTypeFieldFQN(String);
@@ -91,11 +103,41 @@ pub struct MigrationConfig {
     pub configs: Vec<MigrationAgentConfig>,
 }
 
+impl MigrationConfig {
+    pub fn parse(config_content: &str) -> Result<Self, MigrationConfigError> {
+        let mut config: MigrationConfig = serde_yaml::from_str(config_content)?;
+        config.configs.sort_by_key(|c| c.agent_type_fqn.to_string());
+        let last = config
+            .configs
+            .last()
+            .ok_or(MigrationConfigError::EmptyConfigMapping)?
+            .clone();
+        config.configs = config
+            .configs
+            .iter_mut()
+            .as_slice()
+            .windows(2)
+            .map(|c| {
+                let mut current = c[0].clone();
+                if c[0].agent_type_fqn.name() == c[1].agent_type_fqn.name()
+                    && c[0].agent_type_fqn.namespace() == c[1].agent_type_fqn.namespace()
+                {
+                    current.next = Some(c[1].agent_type_fqn.clone());
+                }
+                current
+            })
+            .chain([last])
+            .collect();
+        Ok(config)
+    }
+}
+
 #[derive(Debug, PartialEq, Clone, Deserialize)]
 pub struct MigrationAgentConfig {
     pub agent_type_fqn: AgentTypeFQN,
     pub files_map: FilesMap,
     pub dirs_map: DirsMap,
+    pub next: Option<AgentTypeFQN>,
 }
 
 impl MigrationAgentConfig {
@@ -122,5 +164,85 @@ impl MigrationAgentConfig {
             }
         }
         None
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::migration::config::MigrationConfig;
+    use newrelic_super_agent::super_agent::config::AgentTypeFQN;
+
+    #[test]
+    fn config_parse() {
+        pub const DISORDERED_AGENT_TYPES: &str = r#"
+configs:
+  -
+    agent_type_fqn: newrelic/com.newrelic.infrastructure_agent:0.0.2
+    files_map:
+      config_agent: /etc/newrelic-infra.yml
+    dirs_map:
+      config_ohis: /etc/newrelic-infra/integrations.d
+      logging: /etc/newrelic-infra/logging.d
+  -
+    agent_type_fqn: newrelic/com.newrelic.another:1.0.0
+    files_map:
+      config_another: /etc/another.yml
+    dirs_map:
+  -
+    agent_type_fqn: newrelic/com.newrelic.infrastructure_agent:1.0.1
+    files_map:
+      config_agent: /etc/newrelic-infra.yml
+    dirs_map:
+      config_integrations: /etc/newrelic-infra/integrations.d
+      config_logging: /etc/newrelic-infra/logging.d
+  -
+    agent_type_fqn: francisco-partners/com.newrelic.another:0.0.2
+    files_map:
+      config_another: /etc/another.yml
+    dirs_map:
+  -
+    agent_type_fqn: newrelic/com.newrelic.infrastructure_agent:0.1.2
+    files_map:
+      config_agent: /etc/newrelic-infra.yml
+    dirs_map:
+      config_integrations: /etc/newrelic-infra/integrations.d
+      config_logging: /etc/newrelic-infra/logging.d
+  -
+    agent_type_fqn: newrelic/com.newrelic.another:0.0.1
+    files_map:
+      config_another: /etc/another.yml
+    dirs_map:
+"#;
+
+        let expected_fqns_in_order = vec![
+            "francisco-partners/com.newrelic.another:0.0.2".into(),
+            "newrelic/com.newrelic.another:0.0.1".into(),
+            "newrelic/com.newrelic.another:1.0.0".into(),
+            "newrelic/com.newrelic.infrastructure_agent:0.0.2".into(),
+            "newrelic/com.newrelic.infrastructure_agent:0.1.2".into(),
+            "newrelic/com.newrelic.infrastructure_agent:1.0.1".into(),
+        ];
+        let expected_next_fqns_in_order: Vec<Option<AgentTypeFQN>> = vec![
+            None,
+            Some("newrelic/com.newrelic.another:1.0.0".into()),
+            None,
+            Some("newrelic/com.newrelic.infrastructure_agent:0.1.2".into()),
+            Some("newrelic/com.newrelic.infrastructure_agent:1.0.1".into()),
+            None,
+        ];
+
+        let config = MigrationConfig::parse(DISORDERED_AGENT_TYPES).unwrap();
+        for (key, cfg) in config.configs.iter().enumerate() {
+            assert_eq!(cfg.agent_type_fqn, expected_fqns_in_order[key]);
+            assert_eq!(cfg.next, expected_next_fqns_in_order[key]);
+        }
+    }
+
+    #[test]
+    fn config_parse_error_empty_mapping() {
+        pub const EMPTY_AGENT_TYPES: &str = r#"
+configs: []
+"#;
+        assert!(MigrationConfig::parse(EMPTY_AGENT_TYPES).is_err())
     }
 }
