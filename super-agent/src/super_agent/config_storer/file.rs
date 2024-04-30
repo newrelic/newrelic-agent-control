c0e6141ab202f904eafb74c1912032a40715f85b
Paolo Gallina <paologallina1992@gmail.com>
2024-04-30 10:10:25 +0200

    chore(clippy): warning has been fixed

--- super-agent/src/super_agent/config_storer/file.rs
+++ super-agent/src/super_agent/config_storer/file.rs
@@ -42,9 +42,6 @@ impl SuperAgentDynamicConfigDeleter for SuperAgentConfigStoreFile {
         let Some(remote_path_file) = &self.remote_path else {
             unreachable!("we should not write into local paths");
         };
-        // clippy complains because we are not changing the lock's content
-        // TODO: check RwLock is being used efficiently for this use-case.
-        #[allow(clippy::readonly_write_lock)]
         let _write_guard = self.rw_lock.write().unwrap();
         if remote_path_file.exists() {
             std::fs::remove_file(remote_path_file)?;
@@ -56,9 +53,6 @@ impl SuperAgentDynamicConfigDeleter for SuperAgentConfigStoreFile {
 impl SuperAgentDynamicConfigStorer for SuperAgentConfigStoreFile {
     fn store(&self, sub_agents: &SuperAgentDynamicConfig) -> Result<(), SuperAgentConfigError> {
         //TODO we should inject DirectoryManager and ensure the directory exists
-        // clippy complains because we are not changing the lock's content
-        // TODO: check RwLock is being used efficiently for this use-case.
-        #[allow(clippy::readonly_write_lock)]
         let _write_guard = self.rw_lock.write().unwrap();
         let Some(remote_path_file) = &self.remote_path else {
             unreachable!("we should not write into local paths");
