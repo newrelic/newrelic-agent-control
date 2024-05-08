aa297b743ebedb4589a43a3d77b7bd7d8cdab06d
Paolo Gallina <paologallina1992@gmail.com>
2024-05-08 13:17:49 +0200

    feat(review): address review comments

--- super-agent/src/sub_agent/health/k8s/health_checker.rs
+++ super-agent/src/sub_agent/health/k8s/health_checker.rs
@@ -8,28 +8,34 @@ use crate::super_agent::config::helm_release_type_meta;
 use kube::api::DynamicObject;
 use std::sync::Arc;
 
-/// K8sHealthChecker contains a collection of healthChecks that are queried to provide a unified health value
-pub struct K8sHealthChecker {
-    // Send is needed since K8sHealthChecker is passed to a different thread
-    health_checkers: Vec<Box<dyn HealthChecker + Send>>,
+// GenericHealthCheck exist to wrap all the k8s health checks to have a unique array and a single loop
+pub enum GenericHealthCheck {
+    Flux(K8sHealthFluxHelmRelease),
 }
 
-impl K8sHealthChecker {
+impl HealthChecker for GenericHealthCheck {
+    fn check_health(&self) -> Result<Health, HealthCheckerError> {
+        match self {
+            GenericHealthCheck::Flux(flux) => flux.check_health(),
+        }
+    }
+}
+
+/// K8sHealthChecker contains a collection of healthChecks that are queried to provide a unified health value
+pub struct K8sHealthChecker<HC = GenericHealthCheck>
+where
+    HC: HealthChecker,
+{
+    // Send is needed since K8sHealthChecker is passed to a different thread
+    health_checkers: Vec<HC>,
+}
+
+impl K8sHealthChecker<GenericHealthCheck> {
     pub fn try_new(
         k8s_client: Arc<SyncK8sClient>,
         resources: Vec<DynamicObject>,
     ) -> Result<Self, HealthCheckerError> {
-        Self {
-            health_checkers: vec![],
-        }
-        .set_releases_helm_checkers(k8s_client.clone(), resources)
-    }
-
-    fn set_releases_helm_checkers(
-        mut self,
-        k8s_client: Arc<SyncK8sClient>,
-        resources: Vec<DynamicObject>,
-    ) -> Result<Self, HealthCheckerError> {
+        let mut health_checkers = vec![];
         for resource in resources.iter() {
             let type_meta = resource.types.clone().ok_or(HealthCheckerError::new(
                 "not able to build flux health checker: type not found".to_string(),
@@ -46,17 +52,19 @@ impl K8sHealthChecker {
                     "not able to build flux health checker: name not found".to_string(),
                 ))?;
 
-            self.health_checkers
-                .push(Box::new(K8sHealthFluxHelmRelease::new(
-                    k8s_client.clone(),
-                    name,
-                )));
+            health_checkers.push(GenericHealthCheck::Flux(K8sHealthFluxHelmRelease::new(
+                k8s_client.clone(),
+                name,
+            )));
         }
-        Ok(self)
+        Ok(Self { health_checkers })
     }
 }
 
-impl HealthChecker for K8sHealthChecker {
+impl<HC> HealthChecker for K8sHealthChecker<HC>
+where
+    HC: HealthChecker,
+{
     fn check_health(&self) -> Result<Health, HealthCheckerError> {
         for rhc in self.health_checkers.iter() {
             let health = rhc.check_health()?;
@@ -72,13 +80,12 @@ impl HealthChecker for K8sHealthChecker {
 #[cfg(test)]
 pub mod test {
     use crate::k8s::client::MockSyncK8sClient;
-    use crate::sub_agent::health::health_checker::HealthChecker;
+    use crate::sub_agent::health::health_checker::{Health, Healthy, Unhealthy};
+    use crate::sub_agent::health::health_checker::{HealthChecker, HealthCheckerError};
     use crate::sub_agent::health::k8s::health_checker::K8sHealthChecker;
-    use crate::sub_agent::health::k8s::helm_release::test::setup_mock_client_with_conditions;
     use crate::super_agent::config::helm_release_type_meta;
-    use k8s_openapi::apimachinery::pkg::apis::meta::v1::ObjectMeta;
     use kube::api::DynamicObject;
-    use serde_json::json;
+    use mockall::mock;
     use std::sync::Arc;
 
     #[test]
@@ -96,42 +103,74 @@ pub mod test {
     fn failing_build_health_check() {
         let mock_client = MockSyncK8sClient::default();
 
-        assert!(K8sHealthChecker::try_new(
-            Arc::new(mock_client),
-            vec![DynamicObject {
-                types: Some(helm_release_type_meta()),
-                metadata: Default::default(),
-                data: Default::default(),
-            }]
-        )
-        .is_err());
+        assert_eq!(
+            K8sHealthChecker::try_new(
+                Arc::new(mock_client),
+                vec![DynamicObject {
+                    types: Some(helm_release_type_meta()),
+                    metadata: Default::default(),
+                    data: Default::default(),
+                }]
+            )
+            .err()
+            .unwrap(),
+            HealthCheckerError::new(
+                "not able to build flux health checker: name not found".to_string(),
+            )
+        );
+    }
+
+    mock! {
+        pub HealthCheckMock{}
+        impl HealthChecker for HealthCheckMock{
+            fn check_health(&self) -> Result<Health, HealthCheckerError>;
+        }
+    }
+
+    fn get_healthy_mock() -> MockHealthCheckMock {
+        let mut healthy = MockHealthCheckMock::new();
+        healthy
+            .expect_check_health()
+            .returning(|| Ok(Healthy::default().into()));
+        healthy
+    }
+
+    fn get_unhealthy_mock() -> MockHealthCheckMock {
+        let mut unhealthy = MockHealthCheckMock::new();
+        unhealthy
+            .expect_check_health()
+            .returning(|| Ok(Unhealthy::default().into()));
+        unhealthy
+    }
+
+    fn get_error_mock() -> MockHealthCheckMock {
+        let mut unhealthy = MockHealthCheckMock::new();
+        unhealthy
+            .expect_check_health()
+            .returning(|| Err(HealthCheckerError::new("test".to_string())));
+        unhealthy
     }
 
     #[test]
-    fn failing_health_check() {
-        let mut mock_client = MockSyncK8sClient::default();
-        let status_conditions = json!({
-            "conditions": [
-                {"type": "Ready", "status": "False", "lastTransitionTime": "2021-01-01T12:00:00Z"},
-            ]
-        });
-        setup_mock_client_with_conditions(&mut mock_client, status_conditions);
+    fn logic_health_check() {
+        assert!(K8sHealthChecker {
+            health_checkers: vec![get_healthy_mock(), get_healthy_mock()],
+        }
+        .check_health()
+        .unwrap()
+        .is_healthy());
 
-        let monitored_obj = DynamicObject {
-            types: Some(helm_release_type_meta()),
-            data: Default::default(),
-            metadata: ObjectMeta {
-                name: Some("example-release".to_string()),
-                ..Default::default()
-            },
-        };
+        assert!(!K8sHealthChecker {
+            health_checkers: vec![get_healthy_mock(), get_unhealthy_mock(), get_healthy_mock()],
+        }
+        .check_health()
+        .unwrap()
+        .is_healthy());
 
-        assert!(
-            !K8sHealthChecker::try_new(Arc::new(mock_client), vec![monitored_obj])
-                .unwrap()
-                .check_health()
-                .unwrap()
-                .is_healthy()
-        )
+        assert!(K8sHealthChecker {
+            health_checkers: vec![get_healthy_mock(), get_error_mock(), get_healthy_mock()],
+        }
+        .check_health()
+        .is_err());
     }
 }
