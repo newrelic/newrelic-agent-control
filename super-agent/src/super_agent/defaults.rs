ae87dbce092742c784692e16c456fb6be59c0156
David Sánchez <davidslt+git@pm.me>
2024-05-10 14:22:03 +0100

    feat: macro for generating statics

--- super-agent/src/super_agent/defaults.rs
+++ super-agent/src/super_agent/defaults.rs
@@ -1,56 +1,99 @@
 use opamp_client::capabilities;
 use opamp_client::opamp::proto::AgentCapabilities;
 use opamp_client::operation::capabilities::Capabilities;
+use paste::paste;
+use std::sync::OnceLock;
 
-pub const SUPER_AGENT_ID: &str = "super-agent";
-pub const SUPER_AGENT_TYPE: &str = "com.newrelic.super_agent";
-pub const SUPER_AGENT_NAMESPACE: &str = "newrelic";
-pub const SUPER_AGENT_VERSION: &str = env!("CARGO_PKG_VERSION");
+// What does this do?
+// This macro generates a static variable and a function that returns a reference to that variable.
+// Given an identifier `SUPER_AGENT_ID` and a value `"super-agent"` of type `&str`, the macro call
+// `generate_const_getter!(SUPER_AGENT_ID, "super-agent")` generates the following:
+//
+// ```
+// static SUPER_AGENT_ID_STATIC: OnceLock<&str> = OnceLock::new();
+//
+// pub(crate) fn super_agent_id() -> &'static str {
+//     SUPER_AGENT_ID_STATIC.get_or_init(|| "super-agent")
+// }
+// ```
+//
+// The `OnceLock` type is a wrapper around `std::sync::Once` that allows for the initialization of
+// a static variable with a closure that returns the value to be stored. The `get_or_init` method
+// ensures that the closure is only called once, and the value is stored and returned for subsequent
+// calls.
+macro_rules! generate_const_getter {
+    ($name:ident, $value:expr) => {
+        paste! {
+            static [<$name:upper _STATIC>]: OnceLock<&str> = OnceLock::new();
+
+            // I want this function usage to be analogous to referencing a constant, hence uppercase
+            #[allow(non_snake_case)]
+            pub fn [<$name:upper>]() -> &'static str {
+                [<$name:upper _STATIC>].get_or_init(|| $value)
+            }
+        }
+    };
+}
+
+generate_const_getter!(SUPER_AGENT_ID, "super-agent");
+generate_const_getter!(SUPER_AGENT_TYPE, "com.newrelic.super_agent");
+generate_const_getter!(SUPER_AGENT_NAMESPACE, "newrelic");
+generate_const_getter!(SUPER_AGENT_VERSION, env!("CARGO_PKG_VERSION"));
 
 // Keys non-identifying attributes
-pub const PARENT_AGENT_ID_ATTRIBUTE_KEY: &str = "parent.agent.id";
-pub const HOST_NAME_ATTRIBUTE_KEY: &str = opentelemetry_semantic_conventions::resource::HOST_NAME;
-pub const CLUSTER_NAME_ATTRIBUTE_KEY: &str = "cluster.name";
-pub const HOST_ID_ATTRIBUTE_KEY: &str = opentelemetry_semantic_conventions::resource::HOST_ID;
-pub const FLEET_ID_ATTRIBUTE_KEY: &str = "fleet.guid";
+generate_const_getter!(PARENT_AGENT_ID_ATTRIBUTE_KEY, "parent.agent.id");
+generate_const_getter!(
+    HOST_NAME_ATTRIBUTE_KEY,
+    opentelemetry_semantic_conventions::resource::HOST_NAME
+);
+generate_const_getter!(CLUSTER_NAME_ATTRIBUTE_KEY, "cluster.name");
+generate_const_getter!(
+    HOST_ID_ATTRIBUTE_KEY,
+    opentelemetry_semantic_conventions::resource::HOST_ID
+);
+generate_const_getter!(FLEET_ID_ATTRIBUTE_KEY, "fleet.guid");
 
 // Paths
 cfg_if::cfg_if! {
     if #[cfg(target_os = "macos")] {
-        pub const SUPER_AGENT_LOCAL_DATA_DIR: &str = "/opt/homebrew/etc/newrelic-super-agent";
-        pub const SUPER_AGENT_IDENTIFIERS_PATH: &str = "/opt/homebrew/var/lib/newrelic-super-agent/identifiers.yaml";
-        pub const REMOTE_AGENT_DATA_DIR: &str = "/opt/homebrew/var/lib/newrelic-super-agent/fleet/agents.d";
-        pub const LOCAL_AGENT_DATA_DIR: &str = "/opt/homebrew/etc/newrelic-super-agent/fleet/agents.d";
+        generate_const_getter!(SUPER_AGENT_LOCAL_DATA_DIR, "/opt/homebrew/etc/newrelic-super-agent");
+        generate_const_getter!(SUPER_AGENT_IDENTIFIERS_PATH, "/opt/homebrew/var/lib/newrelic-super-agent/identifiers.yaml");
+        generate_const_getter!(REMOTE_AGENT_DATA_DIR, "/opt/homebrew/var/lib/newrelic-super-agent/fleet/agents.d");
+        generate_const_getter!(LOCAL_AGENT_DATA_DIR, "/opt/homebrew/etc/newrelic-super-agent/fleet/agents.d");
 
-        pub const SUPER_AGENT_DATA_DIR: &str = "/opt/homebrew/var/lib/newrelic-super-agent";
-        pub const DYNAMIC_AGENT_TYPE :&str = "/opt/homebrew/etc/newrelic-super-agent/dynamic-agent-type.yaml";
+        generate_const_getter!(SUPER_AGENT_DATA_DIR, "/opt/homebrew/var/lib/newrelic-super-agent");
+        generate_const_getter!(DYNAMIC_AGENT_TYPE, "/opt/homebrew/etc/newrelic-super-agent/dynamic-agent-type.yaml");
 
         // Logging constants
-        pub const SUPER_AGENT_LOG_DIR: &str = "/opt/homebrew/var/log/newrelic-super-agent";
-        pub const SUB_AGENT_LOG_DIR: &str = "/opt/homebrew/var/log/newrelic-super-agent/fleet/agents.d";
+        generate_const_getter!(SUPER_AGENT_LOG_DIR, "/opt/homebrew/var/log/newrelic-super-agent");
+        generate_const_getter!(SUB_AGENT_LOG_DIR, "/opt/homebrew/var/log/newrelic-super-agent/fleet/agents.d");
 
     }else{
-        pub const SUPER_AGENT_LOCAL_DATA_DIR: &str = "/etc/newrelic-super-agent";
-        pub const SUPER_AGENT_IDENTIFIERS_PATH: &str = "/var/lib/newrelic-super-agent/identifiers.yaml";
-        pub const REMOTE_AGENT_DATA_DIR: &str = "/var/lib/newrelic-super-agent/fleet/agents.d";
-        pub const LOCAL_AGENT_DATA_DIR: &str = "/etc/newrelic-super-agent/fleet/agents.d";
+        generate_const_getter!(SUPER_AGENT_LOCAL_DATA_DIR, "/etc/newrelic-super-agent");
+        generate_const_getter!(SUPER_AGENT_IDENTIFIERS_PATH, "/var/lib/newrelic-super-agent/identifiers.yaml");
+        generate_const_getter!(REMOTE_AGENT_DATA_DIR, "/var/lib/newrelic-super-agent/fleet/agents.d");
+        generate_const_getter!(LOCAL_AGENT_DATA_DIR, "/etc/newrelic-super-agent/fleet/agents.d");
 
-        pub const SUPER_AGENT_DATA_DIR: &str = "/var/lib/newrelic-super-agent";
-        pub const DYNAMIC_AGENT_TYPE :&str = "/etc/newrelic-super-agent/dynamic-agent-type.yaml";
+        generate_const_getter!(SUPER_AGENT_DATA_DIR, "/var/lib/newrelic-super-agent");
+        generate_const_getter!(DYNAMIC_AGENT_TYPE, "/etc/newrelic-super-agent/dynamic-agent-type.yaml");
 
         // Logging constants
-        pub const SUPER_AGENT_LOG_DIR: &str = "/var/log/newrelic-super-agent";
-        pub const SUB_AGENT_LOG_DIR: &str = "/var/log/newrelic-super-agent/fleet/agents.d";
+        generate_const_getter!(SUPER_AGENT_LOG_DIR, "/var/log/newrelic-super-agent");
+        generate_const_getter!(SUB_AGENT_LOG_DIR, "/var/log/newrelic-super-agent/fleet/agents.d");
     }
 }
 
-pub const SUB_AGENT_DIRECTORY: &str = "agents.d";
-pub const VALUES_DIR: &str = "values";
-pub const VALUES_FILE: &str = "values.yaml";
-pub const GENERATED_FOLDER_NAME: &str = "auto-generated";
-pub const SUPER_AGENT_LOG_FILENAME: &str = "newrelic-super-agent.log";
-pub const STDOUT_LOG_PREFIX: &str = "stdout.log";
-pub const STDERR_LOG_PREFIX: &str = "stderr.log";
+generate_const_getter!(SUB_AGENT_DIRECTORY, "agents.d");
+generate_const_getter!(VALUES_DIR, "values");
+generate_const_getter!(VALUES_FILE, "values.yaml");
+generate_const_getter!(GENERATED_FOLDER_NAME, "auto-generated");
+generate_const_getter!(SUPER_AGENT_LOG_FILENAME, "newrelic-super-agent.log");
+generate_const_getter!(STDOUT_LOG_PREFIX, "stdout.log");
+generate_const_getter!(STDERR_LOG_PREFIX, "stderr.log");
+
+pub fn debug_mode() {
+    // Sets some of the variables to a "debug" value for easy testing!
+}
 
 pub fn default_capabilities() -> Capabilities {
     capabilities!(
