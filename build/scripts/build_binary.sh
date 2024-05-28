5a595d0a06fcca166474f45b51f3619bf195ec3a
Ruben Ruiz de Gauna <rubenruizdegauna@proton.me>
2024-05-28 10:29:53 +0200

    separate build per feature

--- build/scripts/build_binary.sh
+++ build/scripts/build_binary.sh
@@ -25,14 +25,24 @@ fi
 
 echo "arch: ${ARCH}, arch_name: ${ARCH_NAME}"
 
-docker build --platform linux/amd64 -t "rust-cross-${ARCH_NAME}-${BIN}" \
-    -f ./build/rust.Dockerfile \
-    --build-arg ARCH_NAME="${ARCH_NAME}" \
-    --build-arg BUILD_MODE="${BUILD_MODE}" \
-    --build-arg BUILD_FEATURE="'${BUILD_FEATURE}'" \
-    --build-arg BUILD_PKG="${PKG}" \
-    --build-arg BUILD_BIN="${BIN}" \
-    .
+if [ -z "${BUILD_FEATURE}" ]; then
+  docker build --platform linux/amd64 -t "rust-cross-${ARCH_NAME}-${BIN}" \
+      -f ./build/rust.Dockerfile \
+      --build-arg ARCH_NAME="${ARCH_NAME}" \
+      --build-arg BUILD_MODE="${BUILD_MODE}" \
+      --build-arg BUILD_PKG="${PKG}" \
+      --build-arg BUILD_BIN="${BIN}" \
+      .
+else
+  docker build --platform linux/amd64 -t "rust-cross-${ARCH_NAME}-${BIN}" \
+      -f ./build/rust.Dockerfile \
+      --build-arg ARCH_NAME="${ARCH_NAME}" \
+      --build-arg BUILD_MODE="${BUILD_MODE}" \
+      --build-arg BUILD_FEATURE="${BUILD_FEATURE}" \
+      --build-arg BUILD_PKG="${PKG}" \
+      --build-arg BUILD_BIN="${BIN}" \
+      .
+fi
 
 # Binary metadata
 GIT_COMMIT=$( git rev-parse HEAD )
