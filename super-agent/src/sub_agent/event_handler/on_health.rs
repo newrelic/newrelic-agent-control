771a5e6056cdf612bc34a5b27700aa4ba946f032
David Sánchez <davidslt+git@pm.me>
2024-06-18 20:19:48 +0100

    fix: use times provided by the health checker

--- super-agent/src/sub_agent/event_handler/on_health.rs
+++ super-agent/src/sub_agent/event_handler/on_health.rs
@@ -5,7 +5,6 @@ use crate::sub_agent::event_processor::EventProcessor;
 use crate::sub_agent::health::health_checker::Health;
 use crate::sub_agent::values::values_repository::ValuesRepository;
 use crate::sub_agent::SubAgentCallbacks;
-use crate::utils::time::get_sys_time_nano;
 use opamp_client::StartedClient;
 
 impl<C, H, R> EventProcessor<C, H, R>
@@ -18,7 +17,8 @@ where
         if let Some(client) = self.maybe_opamp_client.as_ref() {
             let health = opamp_client::opamp::proto::ComponentHealth {
                 healthy: health.is_healthy(),
-                status_time_unix_nano: get_sys_time_nano()?,
+                start_time_unix_nano: health.start_time_unix_nano(),
+                status_time_unix_nano: health.status_time_unix_nano(),
                 last_error: health.last_error().unwrap_or("").to_string(),
                 status: health.status().to_string(),
                 ..Default::default()
