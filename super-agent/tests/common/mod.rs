1c342f931b3ab0d96f3755ef165036c66ea3c7be
Marc Sanmiquel <marcsanmiquel@gmail.com>
2024-06-25 09:30:42 +0200

    feat: add health check status to FakeServer and k8s tests (#709)

--- super-agent/tests/common/mod.rs
+++ super-agent/tests/common/mod.rs
@@ -1,4 +1,5 @@
 /// Includes a OpAMP mock server to test scenarios involving OpAMP.
+pub(super) mod health;
 pub(super) mod opamp;
 /// Includes helpers to handle the _async_ code execution in non-tokio-tests.
 pub(super) mod runtime;
