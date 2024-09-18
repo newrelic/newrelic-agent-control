f5b9bcb427ca76facaa9d3c7ce8848f7e69aa7da
Ruben Ruiz de Gauna <rubenruizdegauna@proton.me>
2024-09-18 11:48:59 +0200

    NR-290452 integration test for executable less agent type

--- super-agent/tests/on_host/tools/config.rs
+++ super-agent/tests/on_host/tools/config.rs
@@ -1,9 +1,16 @@
-use newrelic_super_agent::super_agent::defaults::{
-    SUB_AGENT_DIR, SUPER_AGENT_CONFIG_FILE, VALUES_DIR, VALUES_FILE,
-};
 use std::fs::{create_dir_all, File};
 use std::io::Write;
 use std::path::PathBuf;
+
+use newrelic_super_agent::super_agent::config::AgentID;
+use newrelic_super_agent::super_agent::defaults::{
+    default_capabilities, SUB_AGENT_DIR, SUPER_AGENT_CONFIG_FILE, VALUES_DIR, VALUES_FILE,
+};
+use newrelic_super_agent::super_agent::run::BasePaths;
+use newrelic_super_agent::values::file::YAMLConfigRepositoryFile;
+use newrelic_super_agent::values::yaml_config_repository::{
+    YAMLConfigRepository, YAMLConfigRepositoryError,
+};
 
 /// Creates the super-agent config given an opamp_server_endpoint
 /// and a list of agents on the specified local_dir.
@@ -39,4 +46,20 @@ pub fn create_sub_agent_values(agent_id: String, config: String, base_dir: PathB
     let values_file_path = agent_values_dir_path.join(VALUES_FILE);
 
     create_file(config, values_file_path.clone());
+}
+
+pub fn get_remote_config_content(
+    agent_id: &AgentID,
+    base_paths: BasePaths,
+) -> Result<String, YAMLConfigRepositoryError> {
+    let yaml_config_repo =
+        YAMLConfigRepositoryFile::new(base_paths.local_dir.clone(), base_paths.remote_dir.clone())
+            .with_remote();
+    let remote_config = yaml_config_repo.load_remote(agent_id, &default_capabilities())?;
+    match remote_config {
+        None => Err(YAMLConfigRepositoryError::LoadError(
+            "file not found...".to_string(),
+        )),
+        Some(remote_config) => Ok(serde_yaml::to_string(&remote_config).unwrap()),
+    }
 }
