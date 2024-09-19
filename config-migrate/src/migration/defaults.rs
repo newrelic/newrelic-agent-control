defb4873e695bdf3f38f78caae9ce0d89a724eaa
PaoloGallina <paologallina1992@gmail.com>
2024-09-19 09:44:50 +0200

    chore(agentTypes): remove agentTypes (#828)

--- config-migrate/src/migration/defaults.rs
+++ config-migrate/src/migration/defaults.rs
@@ -3,46 +3,7 @@
 pub const NEWRELIC_INFRA_AGENT_TYPE_CONFIG_MAPPING: &str = r#"
 configs:
   -
-    agent_type_fqn: newrelic/com.newrelic.infrastructure_agent:0.0.2
-    files_map:
-      config_agent: /etc/newrelic-infra.yml
-    dirs_map:
-      config_ohis:
-        path: /etc/newrelic-infra/integrations.d
-        filename_patterns:
-          - ".*\\.ya?ml$"
-      logging:
-        path: /etc/newrelic-infra/logging.d
-        filename_patterns:
-          - ".*\\.ya?ml$"
-  -
-    agent_type_fqn: newrelic/com.newrelic.infrastructure_agent:0.1.0
-    files_map:
-      config_agent: /etc/newrelic-infra.yml
-    dirs_map:
-      config_integrations:
-        path: /etc/newrelic-infra/integrations.d
-        filename_patterns:
-          - ".*\\.ya?ml$"
-      config_logging:
-        path: /etc/newrelic-infra/logging.d
-        filename_patterns:
-          - ".*\\.ya?ml$"
-  -
-    agent_type_fqn: newrelic/com.newrelic.infrastructure_agent:0.1.1
-    files_map:
-      config_agent: /etc/newrelic-infra.yml
-    dirs_map:
-      config_integrations:
-        path: /etc/newrelic-infra/integrations.d
-        filename_patterns:
-          - ".*\\.ya?ml$"
-      config_logging:
-        path: /etc/newrelic-infra/logging.d
-        filename_patterns:
-          - ".*\\.ya?ml$"
-  -
-    agent_type_fqn: newrelic/com.newrelic.infrastructure_agent:0.1.2
+    agent_type_fqn: newrelic/com.newrelic.infrastructure_agent:0.1.3
     files_map:
       config_agent: /etc/newrelic-infra.yml
     dirs_map:
