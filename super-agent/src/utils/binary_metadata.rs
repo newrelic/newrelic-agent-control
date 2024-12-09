ab6b193c4c548e35dabe49b8ebdd521c6949a835
David Sánchez <davidslt+git@pm.me>
2024-12-09 16:13:43 +0000

    chore: remove build date from binary (#934)
    * chore: remove build date from binary
    
    * test: fix assertions of build date
    
    * style: fmt

--- super-agent/src/utils/binary_metadata.rs
+++ super-agent/src/utils/binary_metadata.rs
@@ -7,11 +7,9 @@ pub(crate) const VERSION: &str =
     konst::option::unwrap_or!(option_env!("SUPER_AGENT_VERSION"), "development");
 pub(crate) const GIT_COMMIT: &str =
     konst::option::unwrap_or!(option_env!("GIT_COMMIT"), "development");
-pub(crate) const BUILD_DATE: &str =
-    konst::option::unwrap_or!(option_env!("BUILD_DATE"), "1970-01-01");
 
 pub fn binary_metadata() -> String {
-    format!("New Relic Super Agent Version: {VERSION}, Rust Version: {RUST_VERSION}, GitCommit: {GIT_COMMIT}, BuildDate: {BUILD_DATE}")
+    format!("New Relic Super Agent Version: {VERSION}, Rust Version: {RUST_VERSION}, GitCommit: {GIT_COMMIT}")
 }
 pub fn sub_agent_versions() -> String {
     format!(
