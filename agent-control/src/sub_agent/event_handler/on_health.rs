d6f063b51b72187889836eb3f21a0fcf3d5c9b6c
Christian Felipe Álvarez <calvarez@newrelic.com>
2025-01-28 10:40:46 +0100

    feat(logs): improve health related logs

--- agent-control/src/sub_agent/event_handler/on_health.rs
+++ agent-control/src/sub_agent/event_handler/on_health.rs
@@ -5,7 +5,6 @@ use crate::sub_agent::error::SubAgentError;
 use crate::sub_agent::health::with_start_time::HealthWithStartTime;
 use opamp_client::operation::callbacks::Callbacks;
 use opamp_client::StartedClient;
-use tracing::{debug, warn};
 
 pub fn on_health<C, CB>(
     health: HealthWithStartTime,
@@ -18,13 +17,6 @@ where
     C: StartedClient<CB>,
     CB: Callbacks,
 {
-    if health.is_healthy() {
-        debug!(select_arm = "sub_agent_internal_consumer", "HealthyAgent");
-    } else {
-        debug!(select_arm = "sub_agent_internal_consumer", "UnhealthyAgent");
-        warn!(%agent_id, "sub agent became unhealthy!");
-    }
-
     if let Some(client) = maybe_opamp_client.as_ref() {
         client.set_health(health.clone().into())?;
     }
