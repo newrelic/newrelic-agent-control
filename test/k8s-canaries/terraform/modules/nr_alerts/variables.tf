1fb541bf193c30a04957d51445989d9aa2ff7b87
David Sánchez <davidslt+git@pm.me>
2025-02-12 10:33:08 +0000

    ci(canaries): add alerting to provisioned k8s clusters (#1048)
    * ci(canaries): add alerting to provisioned k8s clusters
    
    This adds a new Terraform module to the canaries infra that, using the New Relic provider, creates alerts for different scenarios.
    
    Currently there are basic alerts in place for general resource usage, but more could be added (and their thresholds updated) over time.
    
    * style: use labels for selection
    
    * fix(alerts): use label for staging
    
    * ci: extract secret creation to another target, parameterize staging setting
    
    * docs: clarify comments
    
    * ci(alerts): cover AC not present
    
    ---------
    
    Co-authored-by: alvarocabanas <acabanas@newrelic.com>

---
+++ test/k8s-canaries/terraform/modules/nr_alerts/variables.tf
@@ -0,0 +1,54 @@
+# NR Account ID
+variable "account_id" {
+  default = ""
+}
+
+# NR User Api Key
+variable "api_key" {
+  default = ""
+}
+
+# US/EU/Staging
+variable "region" {
+  default = "US"
+
+  validation {
+    condition     = can(regex("^(US|EU|Staging)$", var.region))
+    error_message = "Unsupported region"
+  }
+}
+
+variable "cluster_name" {
+  default = "Agent_Control_Canaries_Staging-Cluster"
+}
+
+variable "policies_prefix" {
+  default = "[Staging] Agent Control canaries metric monitoring"
+}
+
+# conditions should follow next structure:
+#[
+# {
+#   name          = "System / Core Count"
+#   metric        = "coreCount"
+#   sample        = "SystemSample"
+#   threshold     = 0
+#   duration      = 600
+#   operator      = "above"
+#   template_name = "./generic_metrics_threshold.tfpl"
+# },
+# {
+#   name = "System / Cpu IOWait Percent"
+#   metric = "cpuIOWaitPercent"
+#   sample = "SystemSample"
+#   threshold = 0.5 # max 0.112 in last week
+#   duration = 600
+#   operator = "above"
+#   template_name = "./generic_metrics_threshold.tfpl"
+# },
+# ...
+# ]
+#
+variable "conditions" {
+  default = []
+}
