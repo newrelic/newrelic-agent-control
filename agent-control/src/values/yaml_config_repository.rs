1e99067e745a5c3533ced9a53c25228113611dc3
Ruben Ruiz de Gauna <rubenruizdegauna@proton.me>
2025-02-24 09:52:14 +0100

    remove unused method for testing

--- agent-control/src/values/yaml_config_repository.rs
+++ agent-control/src/values/yaml_config_repository.rs
@@ -117,16 +117,5 @@ pub mod tests {
                     ))
                 });
         }
-
-        #[allow(dead_code)]
-        pub fn should_store_remote(&mut self, agent_id: &AgentID, yaml_config: &YAMLConfig) {
-            self.expect_store_remote()
-                .once()
-                .with(
-                    predicate::eq(agent_id.clone()),
-                    predicate::eq(yaml_config.clone()),
-                )
-                .returning(|_, _| Ok(()));
-        }
     }
 }
