f0e3cb2894be8a95f7b40540c8b3984f2ff36ff7
Daniel Orihuela Rodriguez <dorihuela@newrelic.com>
2025-05-05 16:44:35 +0200

    fix: tests

--- agent-control/tests/k8s/cli_dynamic_objects/mod.rs
+++ agent-control/tests/k8s/cli_dynamic_objects/mod.rs
@@ -7,6 +7,7 @@ mod install_agent_control;
 fn cli_fails_when_no_kubernetes() {
     let mut cmd = Command::cargo_bin("newrelic-agent-control-cli").unwrap();
     cmd.arg("install").arg("agent-control");
+    cmd.arg("--release-name").arg("agent-control-release");
     cmd.arg("--chart-version").arg("1.0.0");
     cmd.assert().failure();
     cmd.assert().code(predicate::eq(69));
