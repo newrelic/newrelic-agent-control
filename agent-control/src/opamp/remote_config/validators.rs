7f8934c90b6556caec8faa0da805d8bf35df1808
David Sánchez <davidslt+git@pm.me>
2025-05-06 12:14:42 +0100

    refactor: remove values validator

--- agent-control/src/opamp/remote_config/validators.rs
+++ agent-control/src/opamp/remote_config/validators.rs
@@ -1,15 +1,12 @@
 pub mod regexes;
 pub mod signature;
-pub mod values;
 
 use super::RemoteConfig;
-use crate::sub_agent::effective_agents_assembler::EffectiveAgentsAssembler;
 use crate::sub_agent::identity::AgentIdentity;
 use regexes::RegexValidator;
 use signature::validator::SignatureValidator;
 use std::fmt::Display;
 use thiserror::Error;
-use values::ValuesValidator;
 
 /// Represents a validator for config remote
 pub trait RemoteConfigValidator {
@@ -27,16 +24,12 @@ pub trait RemoteConfigValidator {
 /// Represents an error for RemoteConfigValidatorImpl
 pub struct SupportedRemoteConfigValidatorError(String);
 /// Variants of Implementations of [RemoteConfigValidator] to facilitate Static Dispatch.
-pub enum SupportedRemoteConfigValidator<A> {
+pub enum SupportedRemoteConfigValidator {
     Signature(SignatureValidator),
     Regex(RegexValidator),
-    Values(ValuesValidator<A>),
 }
 
-impl<A> RemoteConfigValidator for SupportedRemoteConfigValidator<A>
-where
-    A: EffectiveAgentsAssembler + Send + Sync + 'static,
-{
+impl RemoteConfigValidator for SupportedRemoteConfigValidator {
     type Err = SupportedRemoteConfigValidatorError;
     fn validate(
         &self,
@@ -48,9 +41,6 @@ where
                 .validate(agent_identity, remote_config)
                 .map_err(|e| SupportedRemoteConfigValidatorError(e.to_string())),
             Self::Regex(r) => r
-                .validate(agent_identity, remote_config)
-                .map_err(|e| SupportedRemoteConfigValidatorError(e.to_string())),
-            Self::Values(v) => v
                 .validate(agent_identity, remote_config)
                 .map_err(|e| SupportedRemoteConfigValidatorError(e.to_string())),
         }
