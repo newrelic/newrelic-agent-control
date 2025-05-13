10276731401c2c52e36699e554340294a16bc611
Daniel Orihuela Rodriguez <dorihuela@newrelic.com>
2025-05-13 13:08:00 +0200

    refactor: move no kubernetes test

--- agent-control/tests/k8s/agent_control_cli/mod.rs
+++ agent-control/tests/k8s/agent_control_cli/mod.rs
@@ -1,3 +1,16 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+
 mod dynamic_objects;
-mod exit_codes;
 mod installation;
+
+#[test]
+fn cli_install_agent_control_fails_when_no_kubernetes() {
+    let mut cmd = Command::cargo_bin("newrelic-agent-control-cli").unwrap();
+    cmd.arg("install").arg("agent-control");
+    cmd.arg("--release-name").arg("agent-control-release");
+    cmd.arg("--chart-version").arg("1.0.0");
+
+    cmd.assert().failure();
+    cmd.assert().code(predicate::eq(69));
+}
