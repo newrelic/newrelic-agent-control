823ee0b904535c952e1d13508a98aa4865da8639
Christian Felipe Álvarez <calvarez@newrelic.com>
2025-05-21 16:35:07 +0200

    chore(test): simplify and improve test documentation

--- agent-control/tests/k8s/scenarios/ac_restarts.rs
+++ agent-control/tests/k8s/scenarios/ac_restarts.rs
@@ -14,12 +14,13 @@ use newrelic_agent_control::agent_control::agent_id::AgentID;
 use std::time::Duration;
 use tempfile::tempdir;
 
-/// The local configuration for the agent-control is empty and a remote configuration including
-/// a sub-agent is set (we check that the remote configuration is applied correctly).
-/// After that, the agent control is rebooted using the same empty configuration. We check that the latest
-/// valid configuration still applies (and the underlying objects exists).
-/// Finally, a new remote configuration is set, and we check that the corresponding changes are reflected
-/// in the k8s cluster.
+/// AC is started with a local configuration including a single 'hello world' such agent has an empty local
+/// configuration. Then:
+/// - The configuration for the sub-agent is set remotely and we check that the corresponding
+///   helm-release and effective config are updated accordingly.
+/// - The Agent control is restarted and we check that the helm-release and effective config keeps the values
+///   remotely set.
+/// - The Remote configuration is updated again and values are finally checked.
 #[test]
 #[ignore = "needs k8s cluster"]
 fn k8s_opamp_subagent_configuration_change_after_ac_restarts() {
@@ -88,46 +89,10 @@ valid: true
         check_latest_health_status_was_healthy(&server, &instance_id.clone())
     });
 
-    // Update the agent configuration via OpAMP
-    server.set_config_response(
-        instance_id.clone(),
-        ConfigResponse::from(
-            r#"
-chart_values:
-  valid: super-true
-            "#,
-        ),
-    );
-
-    // Check the expected HelmRelease is updated with the new configuration
-    let expected_spec_values = r#"
-valid: super-true
-    "#;
-
-    retry(60, Duration::from_secs(1), || {
-        block_on(check_helmrelease_spec_values(
-            k8s.client.clone(),
-            namespace.as_str(),
-            "hello-world",
-            expected_spec_values,
-        ))?;
-
-        let expected_config = r#"chart_values:
-  valid: super-true
-"#;
-
-        check_latest_effective_config_is_expected(
-            &server,
-            &instance_id.clone(),
-            expected_config.to_string(),
-        )?;
-        check_latest_health_status_was_healthy(&server, &instance_id.clone())
-    });
-
     // stop agent-control
     drop(_sa);
 
-    // start the agent-control
+    // start the agent-control with the same configuration
     let _sa = start_agent_control_with_testdata_config(
         test_name,
         CUSTOM_AGENT_TYPE_PATH,
@@ -141,7 +106,7 @@ valid: super-true
     );
     wait_until_agent_control_with_opamp_is_started(k8s.client.clone(), namespace.as_str());
 
-    // Check if the HelmRelease is still with the correct config
+    // Check that after restarting the sub-agent configuration remains as set remotely
     retry(60, Duration::from_secs(1), || {
         block_on(check_helmrelease_spec_values(
             k8s.client.clone(),
@@ -151,7 +116,7 @@ valid: super-true
         ))?;
 
         let expected_config = r#"chart_values:
-  valid: super-true
+  valid: true
 "#;
 
         check_latest_effective_config_is_expected(
