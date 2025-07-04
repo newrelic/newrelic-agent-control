773c2d7c2b3fdb01bfb5827dc69b5d731f494966
alvarocabanas <acabanas@newrelic.com>
2025-07-04 15:32:01 +0200

    feat: report effective config on reset to empty local

--- agent-control/tests/k8s/self_update.rs
+++ agent-control/tests/k8s/self_update.rs
@@ -29,8 +29,10 @@ use url::Url;
 const AC_DEV_IMAGE_REPO: &str = "tilt.local/ac-dev";
 const AC_DEV_IMAGE_TAG: &str = "dev";
 pub const LOCAL_CHART_REPOSITORY: &str = "http://chartmuseum.default.svc.cluster.local:8080";
-pub const LOCAL_CHART_PREVIOUS_VERSION: &str = "0.0.1";
-pub const LOCAL_CHART_NEW_VERSION: &str = "0.0.2";
+// This version contains the image from remote
+pub const LOCAL_CHART_REMOTE_VERSION: &str = "0.0.1";
+pub const LOCAL_CHART_PREVIOUS_VERSION: &str = "0.0.2";
+pub const LOCAL_CHART_NEW_VERSION: &str = "0.0.3";
 const MISSING_VERSION: &str = "9.9.9";
 
 const SECRET_NAME: &str = "ac-values";
@@ -42,6 +44,73 @@ const MINIKUBE_HOST_ACCESS: &str = "host.minikube.internal";
 
 #[test]
 #[ignore = "needs k8s cluster"]
+/// This test installs AC using the CLI from the last chart released to our public repository,
+/// then sends a RemoteConfig with an AC chart version update to the local chart
+/// also introducing several changes in the AC config which should be applied to the new AC version.
+fn k8s_self_update_bump_chart_version_from_last_release_to_local_new_config() {
+    let mut opamp_server = FakeServer::start_new();
+    let mut k8s = block_on(K8sEnv::new());
+    let namespace = block_on(k8s.test_namespace());
+
+    let ac_instance_id = bootstrap_ac(
+        k8s.client.clone(),
+        &opamp_server,
+        &namespace,
+        LOCAL_CHART_REMOTE_VERSION,
+    );
+
+    let ac_config = format!(
+        r#"
+agents: {{}}
+chart_version: {LOCAL_CHART_PREVIOUS_VERSION}
+"#
+    );
+
+    opamp_server.set_config_response(
+        ac_instance_id.clone(),
+        ConfigResponse::from(ac_config.as_str()),
+    );
+
+    // Assert that opamp server receives Agent description with updated version.
+    // Also the rest of the config with the new agent has been effectevely applied.
+    retry(60, Duration::from_secs(5), || {
+        let current_attributes = opamp_server
+            .get_attributes(&ac_instance_id)
+            .ok_or_else(|| "Identifying attributes not found".to_string())?;
+
+        if !current_attributes
+            .identifying_attributes
+            .contains(&KeyValue {
+                key: OPAMP_CHART_VERSION_ATTRIBUTE_KEY.to_string(),
+                value: Some(AnyValue {
+                    value: Some(Value::StringValue(LOCAL_CHART_PREVIOUS_VERSION.to_string())),
+                }),
+            })
+        {
+            return Err(format!(
+                "new version has not been reported: {:?}",
+                current_attributes
+            )
+            .into());
+        }
+
+        check_latest_effective_config_is_expected(
+            &opamp_server,
+            &ac_instance_id,
+            ac_config.clone(),
+        )?;
+        check_latest_remote_config_status_is_expected(
+            &opamp_server,
+            &ac_instance_id,
+            RemoteConfigStatuses::Applied as i32,
+        )?;
+        check_latest_health_status_was_healthy(&opamp_server, &ac_instance_id)?;
+        Ok(())
+    });
+}
+
+#[test]
+#[ignore = "needs k8s cluster"]
 /// This test installs AC using the CLI, then sends a RemoteConfig with an AC chart version update
 /// and asserts that the new AC version is sending OpAmp messages.
 fn k8s_self_update_bump_chart_version() {
@@ -49,7 +118,12 @@ fn k8s_self_update_bump_chart_version() {
     let mut k8s = block_on(K8sEnv::new());
     let namespace = block_on(k8s.test_namespace());
 
-    let ac_instance_id = bootstrap_ac(k8s.client.clone(), &opamp_server, &namespace);
+    let ac_instance_id = bootstrap_ac(
+        k8s.client.clone(),
+        &opamp_server,
+        &namespace,
+        LOCAL_CHART_PREVIOUS_VERSION,
+    );
 
     opamp_server.set_config_response(
         ac_instance_id.clone(),
@@ -98,7 +172,12 @@ fn k8s_self_update_bump_chart_version_with_new_config() {
     let mut k8s = block_on(K8sEnv::new());
     let namespace = block_on(k8s.test_namespace());
 
-    let ac_instance_id = bootstrap_ac(k8s.client.clone(), &opamp_server, &namespace);
+    let ac_instance_id = bootstrap_ac(
+        k8s.client.clone(),
+        &opamp_server,
+        &namespace,
+        LOCAL_CHART_PREVIOUS_VERSION,
+    );
 
     // This agent will not actually be deployed since misses the chart_version config.
     let agents_config = r#"agents:
@@ -139,15 +218,15 @@ chart_version: {LOCAL_CHART_NEW_VERSION}
             );
         }
 
-        check_latest_effective_config_is_expected(
-            &opamp_server,
-            &ac_instance_id,
-            ac_config.clone(),
-        )?;
         check_latest_remote_config_status_is_expected(
             &opamp_server,
             &ac_instance_id,
             RemoteConfigStatuses::Applied as i32,
+        )?;
+        check_latest_effective_config_is_expected(
+            &opamp_server,
+            &ac_instance_id,
+            ac_config.clone(),
         )?;
         check_latest_health_status_was_healthy(&opamp_server, &ac_instance_id)?;
         Ok(())
@@ -158,13 +237,18 @@ chart_version: {LOCAL_CHART_NEW_VERSION}
 #[ignore = "needs k8s cluster"]
 /// This test installs AC using the CLI, then sends a RemoteConfig with an AC chart version update
 /// pointing to a version that doesn't exists. It expects that current AC keeps working and reports
-/// unhealthy status.
-fn k8s_self_update_new_version_fails_to_start() {
+/// unhealthy status then a new correct remote config arrives, it's applied and the new ac reports healthy.
+fn k8s_self_update_new_version_fails_to_start_next_receives_correct_version() {
     let mut opamp_server = FakeServer::start_new();
     let mut k8s = block_on(K8sEnv::new());
     let namespace = block_on(k8s.test_namespace());
 
-    let ac_instance_id = bootstrap_ac(k8s.client.clone(), &opamp_server, &namespace);
+    let ac_instance_id = bootstrap_ac(
+        k8s.client.clone(),
+        &opamp_server,
+        &namespace,
+        LOCAL_CHART_PREVIOUS_VERSION,
+    );
 
     // AC should start correctly and finally report healthy
     retry(60, Duration::from_secs(5), || {
@@ -191,17 +275,50 @@ chart_version: {MISSING_VERSION}
             RemoteConfigStatuses::Applied as i32, // The configuration was Applied even if it led to an unhealthy AC.
         )?;
         check_latest_health_status(&opamp_server, &ac_instance_id, |status| {
-            (!status.healthy)
-                && (status
+            !status.healthy
+                && status
                     .last_error
-                    .contains("latest generation of object has not been reconciled")) // Expected error when chart version doesn't exist
+                    .contains("latest generation of object has not been reconciled") // Expected error when chart version doesn't exist
         })
+    });
+
+    let ac_config = format!(
+        r#"
+agents: {{}}
+chart_version: {LOCAL_CHART_NEW_VERSION}
+"#
+    );
+
+    opamp_server.set_config_response(
+        ac_instance_id.clone(),
+        ConfigResponse::from(ac_config.as_str()),
+    );
+
+    retry(60, Duration::from_secs(5), || {
+        check_latest_effective_config_is_expected(
+            &opamp_server,
+            &ac_instance_id,
+            ac_config.clone(),
+        )?;
+        check_latest_remote_config_status_is_expected(
+            &opamp_server,
+            &ac_instance_id,
+            RemoteConfigStatuses::Applied as i32,
+        )?;
+        check_latest_health_status_was_healthy(&opamp_server, &ac_instance_id)?;
+
+        Ok(())
     });
 }
 
 /// installs ac with cli using minimal values set, pointing it to fake opamp server and printing
 /// pod logs to stdout
-fn bootstrap_ac(client: Client, opamp_server: &FakeServer, namespace: &str) -> InstanceID {
+fn bootstrap_ac(
+    client: Client,
+    opamp_server: &FakeServer,
+    namespace: &str,
+    chart_version: &str,
+) -> InstanceID {
     let mut opamp_endpoint = Url::from_str(&opamp_server.endpoint()).unwrap();
     opamp_endpoint.set_host(Some(MINIKUBE_HOST_ACCESS)).unwrap();
 
@@ -215,18 +332,18 @@ fn bootstrap_ac(client: Client, opamp_server: &FakeServer, namespace: &str) -> I
         ac_chart_values(opamp_endpoint, namespace),
     );
 
-    install_ac_with_cli(namespace);
+    install_ac_with_cli(namespace, chart_version);
 
     instance_id::get_instance_id(client.clone(), namespace, &AgentID::new_agent_control_id())
 }
 
-fn install_ac_with_cli(namespace: &str) {
+fn install_ac_with_cli(namespace: &str, chart_version: &str) {
     let mut cmd = Command::cargo_bin("newrelic-agent-control-cli").unwrap();
 
     cmd.arg("install-agent-control");
     cmd.arg("--log-level").arg("debug");
     cmd.arg("--repository-url").arg(LOCAL_CHART_REPOSITORY);
-    cmd.arg("--chart-version").arg(LOCAL_CHART_PREVIOUS_VERSION);
+    cmd.arg("--chart-version").arg(chart_version);
     cmd.arg("--namespace").arg(namespace);
     cmd.arg("--secrets")
         .arg(format!("{SECRET_NAME}={VALUES_KEY}"));
@@ -234,9 +351,11 @@ fn install_ac_with_cli(namespace: &str) {
     cmd.assert().success();
 }
 
+// Default image and tag will be used, in the case of local the default is overwritten in Tilt
+// setting it to tilt.local/ac-dev:dev
 fn ac_chart_values(opamp_endpoint: Url, name_override: &str) -> String {
     serde_json::json!({
-        // give an unique name per test to the cluster role to avoid collisions
+        // give a unique name per test to the cluster role to avoid collisions
         "nameOverride": name_override,
         "config": {
           // Disable the SI creation
@@ -260,11 +379,6 @@ fn ac_chart_values(opamp_endpoint: Url, name_override: &str) -> String {
         "global": {
           "cluster": "test-cluster",
           "licenseKey": "***",
-        },
-        "image": {
-          "repository": AC_DEV_IMAGE_REPO,
-          "tag": AC_DEV_IMAGE_TAG,
-          "pullPolicy": "Never",
         },
     })
     .to_string()
