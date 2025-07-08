8eb1f6d855be6ff1364851aefc8f117eebfc416c
David Sánchez <davidslt+git@pm.me>
2025-07-08 19:32:46 +0100

    refactor: module location

--- agent-control/src/agent_control/config.rs
+++ agent-control/src/agent_control/config.rs
@@ -2,7 +2,7 @@ use super::agent_id::AgentID;
 use super::http_server::config::ServerConfig;
 use super::uptime_report::UptimeReportConfig;
 use crate::agent_control::health_checker::AgentControlHealthCheckerConfig;
-use crate::agent_type::variable_constraints::VariableConstraints;
+use crate::agent_type::variable::constraints::VariableConstraints;
 use crate::http::config::ProxyConfig;
 use crate::instrumentation::config::logs::config::LoggingConfig;
 use crate::k8s::client::ClientConfig;
--- agent-control/src/agent_type.rs
+++ agent-control/src/agent_type.rs
@@ -11,5 +11,4 @@ pub mod templates;
 pub mod templates_function;
 pub mod trivial_value;
 pub mod variable;
-pub mod variable_constraints;
 pub mod version_config;
