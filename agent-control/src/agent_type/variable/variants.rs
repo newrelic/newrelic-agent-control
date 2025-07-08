8fa83250b66074d8dd6d115569fb435e9e108869
Christian Felipe Álvarez <calvarez@newrelic.com>
2025-07-08 11:25:29 +0200

    chore: simplify types

--- agent-control/src/agent_type/variable/variants.rs
+++ agent-control/src/agent_type/variable/variants.rs
@@ -8,41 +8,15 @@ use serde::{Deserialize, Serialize};
 pub struct Variants<T: PartialEq>(pub(crate) Vec<T>); // TODO: we may not need it to be public
 
 /// Defines the configuration to be set when defining [Variants] from Agent Control configuration.
-#[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
+#[derive(Debug, Default, PartialEq, Clone, Serialize, Deserialize)]
 pub struct VariantsConfig<T>
 where
     T: PartialEq,
 {
-    pub(crate) ac_config_field: String,
-    pub(crate) default: Variants<T>,
-}
-
-/// Defines the supported variants definition.
-/// # Examples:
-///
-/// ```
-/// # use newrelic_agent_control::agent_type::variable::variants::VariantsDefinition;
-/// # use assert_matches::assert_matches;
-/// // Variants defined in Agent Type
-/// let s = r#"["value1", "value2"]"#;
-/// let v: VariantsDefinition<String> = serde_yaml::from_str(s).unwrap();
-/// assert_matches!(v, VariantsDefinition::<String>::FromAgentType(_));
-///
-/// // Variants defined as Agent Control configuration reference
-/// let s = r#"{"ac_config_field": "some_field_name", "default": ["value1"]}"#;
-/// let v: VariantsDefinition<String> = serde_yaml::from_str(s).unwrap();
-/// assert_matches!(v, VariantsDefinition::<String>::FromAgentControlConfig(_));
-/// ```
-#[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
-#[serde(untagged)]
-pub enum VariantsDefinition<T>
-where
-    T: PartialEq,
-{
-    /// Variants directly set in the Agent Type definition.
-    FromAgentType(Variants<T>),
-    /// Variants set in Agent Control static configuration
-    FromAgentControlConfig(VariantsConfig<T>),
+    #[serde(default)]
+    pub(crate) ac_config_field: Option<String>,
+    #[serde(default)]
+    pub(crate) values: Variants<T>,
 }
 
 impl<T> Variants<T>
@@ -69,5 +43,33 @@ where
 {
     fn default() -> Self {
         Self(Vec::new())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case::default("", Default::default())]
+    #[case::values_only(
+        r#"{"values": ["v"]}"#,
+        VariantsConfig::<String> { values: vec!["v".to_string()].into(), ..Default::default()})
+    ]
+    #[case::values_only(
+        r#"{"ac_config_field": "some_variants"}"#,
+        VariantsConfig::<String> { ac_config_field: Some("some_variants".to_string()), ..Default::default()})
+    ]
+    #[case::all(
+        r#"{"ac_config_field": "some_variants", "values": ["v1", "v2"]}"#,
+        VariantsConfig::<String> { ac_config_field: Some("some_variants".to_string()), values: vec!["v1".to_string(), "v2".to_string()].into()})
+    ]
+    fn test_variants_config_deserialization(
+        #[case] input: &str,
+        #[case] expected: VariantsConfig<String>,
+    ) {
+        let value: VariantsConfig<String> = serde_yaml::from_str(input).unwrap();
+        assert_eq!(value, expected);
     }
 }
