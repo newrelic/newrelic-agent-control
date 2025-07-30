e33acc41f03d493e13b927600bcbb9c2f745ce64
David Sánchez <davidslt+git@pm.me>
2025-07-30 12:58:52 +0100

    feat: wip

--- agent-control/src/cli/install/flux.rs
+++ agent-control/src/cli/install/flux.rs
@@ -18,6 +18,55 @@ impl DynamicObjectListBuilder for InstallFlux {
         maybe_existing_helm_release: Option<Arc<DynamicObject>>,
         data: &InstallData,
     ) -> Vec<kube::api::DynamicObject> {
+        /*
+        apiVersion: source.toolkit.fluxcd.io/v1
+        kind: HelmRepository
+        metadata:
+          name: flux-repo
+          namespace: default
+        spec:
+          interval: 1m
+          url: https://fluxcd-community.github.io/helm-charts
+        ---
+        apiVersion: helm.toolkit.fluxcd.io/v2
+        kind: HelmRelease
+        metadata:
+          name: flux2
+        spec:
+          interval: 1m
+          chart:
+            spec:
+              sourceRef:
+                kind: HelmRepository
+                name: flux-repo
+                namespace: default
+              chart: flux2
+              version: 2.15.0
+          values:
+            installCRDS: true
+            sourceController:
+              create: true
+            helmController:
+              create: true
+            kustomizeController:
+              create: false
+            imageAutomationController:
+              create: false
+            imageReflectionController:
+              create: false
+            notificationController:
+              create: false
+        */
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic_dynamic_object_list() {
+        
     }
 }
