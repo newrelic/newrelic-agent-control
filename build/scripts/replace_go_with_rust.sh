6cc7560ceb570a097eca5f279de93f197916f108
David Sánchez <davidslt+git@pm.me>
2025-08-11 15:13:03 +0100

    build: use GoReleaser v2 (cargo-zigbuild)

--- build/scripts/replace_go_with_rust.sh
+++ build/scripts/replace_go_with_rust.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
-if [ "$ARCH" = "arm64" ];then
-  ARCH_DIRNAME="./dist/${BIN}_linux_${ARCH}"
-fi
+tree ./bin
+tree ./dist
 
-if [ "$ARCH" = "amd64" ];then
-  ARCH_DIRNAME="./dist/${BIN}_linux_${ARCH}_v1"
-fi
+exit 1
 
-# move rust compiled files into goreleaser generated locations
-cp "./bin/${BIN}-${ARCH}" "${ARCH_DIRNAME}/${BIN}"
+# if [ "$ARCH" = "arm64" ];then
+#   ARCH_DIRNAME="./dist/${BIN}_linux_${ARCH}"
+# fi
 
-# validate files are in the correct location (if tree is installed)
-which tree && tree ./dist/
+# if [ "$ARCH" = "amd64" ];then
+#   ARCH_DIRNAME="./dist/${BIN}_linux_${ARCH}_v1"
+# fi
+
+# # move rust compiled files into goreleaser generated locations
+# cp "./bin/${BIN}-${ARCH}" "${ARCH_DIRNAME}/${BIN}"
+
+# # validate files are in the correct location (if tree is installed)
+# which tree && tree ./dist/
