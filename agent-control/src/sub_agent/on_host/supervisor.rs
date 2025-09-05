b9d68a52ebdc8c20e11ccbef671c128c4e03d6d6
Daniel Orihuela Rodriguez <dorihuela@newrelic.com>
2025-09-05 17:18:15 +0200

    feat: add agent type to logs

--- agent-control/src/sub_agent/on_host/supervisor.rs
+++ agent-control/src/sub_agent/on_host/supervisor.rs
@@ -160,7 +160,7 @@ impl NotStartedSupervisorOnHost {
             );
             return Ok(Some(started_thread_context));
         }
-        debug!("health checks are disabled for this agent");
+        info!(%self.agent_identity.agent_type_id, "health checks are disabled for this agent");
         Ok(None)
     }
 
@@ -169,7 +169,7 @@ impl NotStartedSupervisorOnHost {
         sub_agent_internal_publisher: EventPublisher<SubAgentInternalEvent>,
     ) {
         let Some(version_config) = &self.version_config else {
-            debug!("version checks are disabled for this agent");
+            info!(%self.agent_identity.agent_type_id, "version checks are disabled for this agent");
             return;
         };
 
