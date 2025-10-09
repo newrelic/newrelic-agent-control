0c36c4b594380035c6b0d7e35a29046e8777343d
David Sánchez <davidslt+git@pm.me>
2025-10-09 19:01:07 +0100

    docs: top-level rendered types

--- agent-control/src/agent_type/runtime_config/rendered.rs
+++ agent-control/src/agent_type/runtime_config/rendered.rs
@@ -1,10 +1,19 @@
 use crate::agent_type::runtime_config::{k8s::K8s, on_host::rendered::OnHost};
 
+/// The runtime definition of an agent type after it has been completely
+/// rendered by the templating process.
+///
+/// This is used by the supervisorss to know how to create and manage the workload resources for
+/// the agent.
 #[derive(Debug, Clone, PartialEq)]
 pub struct Runtime {
     pub deployment: Deployment,
 }
 
+/// Deployment definition for an agent type after it has been completely rendered by the templating
+/// process.
+///
+/// Specifies if there are `on_host` instructions, `k8s` instructions, both or none.
 #[derive(Debug, Default, Clone, PartialEq)]
 pub struct Deployment {
     pub on_host: Option<OnHost>,
