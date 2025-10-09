5fcf1d93aca889fdc839a9a1d2872524ca89f72c
David Sánchez <davidslt+git@pm.me>
2025-10-09 09:22:18 +0100

    build: get go version from `go env` or default

--- build/scripts/download_embedded.sh
+++ build/scripts/download_embedded.sh
@@ -3,6 +3,8 @@
 ARCH="${ARCH:-amd64}"
 GOCACHE=/tmp/.gocache
 GOVERSION="${GOVERSION:-1.25}"
+# Extract only the number part if GOVERSION starts with 'go', common in the CLI output
+GOVERSION="${GOVERSION#go}"
 
 # download assets
 docker run --rm --user "$(id -u)":"$(id -g)" -e GOCACHE="$GOCACHE" -e ARCH="$ARCH" -e ARTIFACTS_VERSIONS="${ARTIFACTS_VERSIONS}" -v "$PWD":/usr/src/app -w /usr/src/app golang:"$GOVERSION" make build/embedded
