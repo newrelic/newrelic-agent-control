b221bb950b135919a494be75adcd09a3dd5c8b08
David Sánchez <davidslt+git@pm.me>
2025-10-15 19:06:16 +0100

    build: nix flake
    features: git hooks, dev shell, cross-compilation

---
+++ flake.nix
@@ -0,0 +1,125 @@
+{
+  description = "New Relic Agent Control";
+
+  inputs = {
+    # Nix package repository
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Can change this to 25.11 for stable
+    # A "framework" of sorts that implements a module system
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    # Build Rust projects
+    crane.url = "github:ipetkov/crane";
+    # Manage Rust toolchains
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # Git hooks
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # Project-wide formatting
+    treefmt = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.git-hooks.flakeModule
+        inputs.treefmt.flakeModule
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        { system, ... }:
+        let
+          acLib = import ./nix/lib.nix;
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ (import inputs.rust-overlay) ];
+          };
+          # Retrieve the Rust version from the Cargo.toml file
+          rustVersion = (pkgs.lib.importTOML ./Cargo.toml).workspace.package.rust-version;
+          craneLib = (inputs.crane.mkLib pkgs).overrideToolchain (
+            # Set the toolchain to the pinned Rust version
+            p: p.rust-bin.stable.${rustVersion}.default
+          );
+          # The default AC package definition
+          defaultAgentControl = pkgs.callPackage (acLib.crateExpression craneLib) { };
+        in
+        {
+          checks = {
+            # Build the crate as part of `nix flake check` for convenience
+            inherit defaultAgentControl;
+          };
+          packages = {
+            # Let's start defining everything even though there'll be quite a bit of repetition.
+            # First, a default package which builds AC natively for the platform.
+            default = defaultAgentControl;
+            # Cross-compiled packages
+            cross-linux-aarch64-musl =
+              let
+                crossSystem = {
+                  config = "aarch64-unknown-linux-musl";
+                };
+                localSystem = system;
+                pkgs' = import inputs.nixpkgs {
+                  inherit crossSystem localSystem;
+                  overlays = [ (import inputs.rust-overlay) ];
+                };
+                craneLib = (inputs.crane.mkLib pkgs').overrideToolchain (
+                  p: p.rust-bin.stable.${rustVersion}.default
+                );
+              in
+              pkgs'.callPackage (acLib.crateExpression craneLib) { };
+
+            cross-linux-x86_64-musl =
+              let
+                crossSystem = {
+                  config = "x86_64-unknown-linux-musl";
+                };
+                localSystem = system;
+                pkgs' = import inputs.nixpkgs {
+                  inherit crossSystem localSystem;
+                  overlays = [ (import inputs.rust-overlay) ];
+                };
+                craneLib = (inputs.crane.mkLib pkgs').overrideToolchain (
+                  p: p.rust-bin.stable.${rustVersion}.default
+                );
+              in
+              pkgs'.callPackage (acLib.crateExpression craneLib) { };
+
+          }
+          // pkgs.lib.optionalAttrs pkgs.stdenv.isx86_64 {
+            # Windows requires `wine64` somewhere, which is not available for aarch64 hosts :(
+            # fortunately for us, aarch64-darwin hosts can run these outputs thanks to Rosetta 2!
+            cross-windows =
+              let
+                crossSystem = {
+                  config = "x86_64-w64-mingw32";
+                  libc = "msvcrt";
+                };
+                localSystem = system;
+                pkgs' = import inputs.nixpkgs {
+                  inherit crossSystem localSystem;
+                  overlays = [ (import inputs.rust-overlay) ];
+                };
+                craneLib = (inputs.crane.mkLib pkgs').overrideToolchain (
+                  p: p.rust-bin.stable.${rustVersion}.default
+                );
+              in
+              pkgs'.callPackage (acLib.crateExpressionWin craneLib) { };
+          };
+
+          devShells = { };
+        };
+    };
+}
---
+++ nix/lib.nix
@@ -0,0 +1,92 @@
+{
+  crateExpression =
+    craneLib:
+    {
+      libiconv,
+      lib,
+      stdenv,
+    }:
+    let
+      src = craneLib.cleanCargoSource ../.;
+      commonArgs = {
+        inherit src;
+        strictDeps = true;
+        # Dependencies which need to be build for the current platform
+        # on which we are doing the cross compilation. In this case,
+        # pkg-config needs to run on the build platform so that the build
+        # script can find the location of openssl. Note that we don't
+        # need to specify the rustToolchain here since it was already
+        # overridden above.
+        nativeBuildInputs = [
+          # pkg-config
+        ]
+        ++ lib.optionals stdenv.buildPlatform.isDarwin [
+          libiconv
+        ];
+        # Dependencies which need to be built for the platform on which
+        # the binary will run. In this case, we need to compile openssl
+        # so that it can be linked with our executable.
+        buildInputs = [
+          # Add additional build inputs here
+          # openssl
+        ];
+      }
+      # All this is only to build Agent Control, so we set name/version in the "common args"
+      // craneLib.crateNameFromCargoToml { cargoToml = ../agent-control/Cargo.toml; };
+      # Build *just* the cargo dependencies, so we can reuse
+      # all of that work (e.g. via cachix) when running in CI
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+    in
+    craneLib.buildPackage (
+      commonArgs
+      // {
+        inherit cargoArtifacts;
+        # NB: we disable tests since we'll run them all via our CI or from CLI,
+        # Besides, need to disable tests requiring network etc etc
+        doCheck = false;
+      }
+    );
+
+  crateExpressionWin =
+    craneLib:
+    { pkgs, ... }:
+    let
+      buildPlatformSuffix = pkgs.lib.strings.toLower pkgs.pkgsBuildHost.stdenv.hostPlatform.rust.cargoEnvVarTarget;
+      src = craneLib.cleanCargoSource ../.;
+
+      commonArgsWin = {
+        inherit src;
+        strictDeps = true;
+        doCheck = false;
+
+        CARGO_BUILD_TARGET = "x86_64-pc-windows-gnu";
+
+        # fixes issues with aws-lc-sys
+        AWS_LC_SYS_PREBUILT_NASM = 0; # just assemble it instead of using the prebuilt objects
+        CFLAGS = "-Wno-stringop-overflow -Wno-array-bounds -Wno-restrict"; # ignore some warnings that pop up when cross compiling
+        CFLAGS_x86_64-pc-windows-gnu = "-I${pkgs.windows.pthreads}/include"; # fix missing <pthread.h>
+        "CC_${buildPlatformSuffix}" = "cc"; # Make linux build use linux compiler (not mingw)
+        "CXX_${buildPlatformSuffix}" = "cc";
+
+        nativeBuildInputs =
+          with pkgs;
+          [
+            buildPackages.nasm
+            buildPackages.cmake
+          ]
+          ++ lib.optionals stdenv.buildPlatform.isDarwin [
+            buildPackages.libiconv
+          ];
+      }
+      # All this is only to build Agent Control, so we set name/version in the "common args"
+      // craneLib.crateNameFromCargoToml { cargoToml = ../agent-control/Cargo.toml; };
+
+      cargoArtifactsWin = craneLib.buildDepsOnly commonArgsWin;
+    in
+    craneLib.buildPackage (
+      commonArgsWin
+      // {
+        cargoArtifacts = cargoArtifactsWin;
+      }
+    );
+}
