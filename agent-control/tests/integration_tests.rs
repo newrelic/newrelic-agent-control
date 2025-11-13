e56a6278130211dda63079b2323742dc65b315c0
David Sánchez <davidsanchez@newrelic.com>
2025-11-13 18:31:57 +0000

    style: disable unused code warning
    to revert once all code paths are covered for windows

--- agent-control/tests/integration_tests.rs
+++ agent-control/tests/integration_tests.rs
@@ -1,6 +1,6 @@
 //! Entry point for the integration tests.
 #![warn(missing_docs)]
-
+#![cfg_attr(target_family = "windows", allow(unused_imports, dead_code))] // to remove once all code paths are covered for windows
 mod common;
 mod k8s;
 mod on_host;
