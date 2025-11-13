0df1590c7a4696ca4764d4497bcbb32910dff6d0
David Sánchez <davidslt+git@pm.me>
2025-11-13 19:07:23 +0000

    Revert "style: disable unused code warning"
    This reverts commit e56a6278130211dda63079b2323742dc65b315c0.

--- agent-control/src/config_migrate/migration.rs
+++ agent-control/src/config_migrate/migration.rs
@@ -3,7 +3,5 @@ pub mod agent_value_spec;
 pub mod config;
 pub mod converter;
 pub mod defaults;
-// to remove once all code paths are covered for windows
-#[cfg_attr(target_family = "windows", allow(unused_imports, dead_code))]
 pub mod migrator;
 pub mod persister;
