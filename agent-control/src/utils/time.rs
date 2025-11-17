722cdce3ea0a9df315e0e2f86f80e4d8e2f12f1f
David Sánchez <davidsanchez@newrelic.com>
2025-11-17 16:18:20 +0000

    docs: account for platform-dependent system time differences

--- agent-control/src/utils/time.rs
+++ agent-control/src/utils/time.rs
@@ -1,10 +1,16 @@
 use std::time::{Duration, SystemTime};
 
 /// Converts a unix epoch timestamp in nanoseconds to a `SystemTime`.
+///
+/// Mind the platform-specific differences on `SystemTime` precision.
+/// For example, on Windows the time is represented in 100 nanosecond intervals
 pub fn sys_time_from_unix_timestamp(nanos: u64) -> SystemTime {
     SystemTime::UNIX_EPOCH + Duration::from_nanos(nanos)
 }
 /// Converts a unix epoch timestamp in nanoseconds to a `SystemTime`.
+///
+/// Mind the platform-specific differences on `SystemTime` precision.
+/// For example, on Windows the time is represented in 100 nanosecond intervals
 pub fn unix_timestamp_from_sys_time(time: SystemTime) -> u64 {
     time.duration_since(SystemTime::UNIX_EPOCH)
         .unwrap_or_default()
