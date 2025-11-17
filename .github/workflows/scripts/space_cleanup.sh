152e943a226eca8f17889a74eb29094c8d683c82
David Sánchez <davidslt+git@pm.me>
2025-11-17 13:56:58 +0000

    docs: reference where we got the snippet from

--- .github/workflows/scripts/space_cleanup.sh
+++ .github/workflows/scripts/space_cleanup.sh
@@ -3,6 +3,7 @@
 set -e
 
 ## Freeing space since 14GB are not enough anymore
+# Got this from <https://github.com/newrelic/infrastructure-publish-action/blob/ecba9f25fc8c7badc3c4e7d2c2aed51c26d52f2b/action-run.sh#L22>
 df -ih
 df -h
 echo "Deleting android, dotnet, haskell, CodeQL, Python, swift to free up space"
