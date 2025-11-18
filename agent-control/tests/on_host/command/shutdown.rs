ae8e04c4a24a2662a58b6e03efc0cc5663ec81a2
David Sánchez <davidslt+git@pm.me>
2025-11-18 19:12:38 +0000

    test: rename script, enable test using it

--- agent-control/tests/on_host/command/shutdown.rs
+++ agent-control/tests/on_host/command/shutdown.rs
@@ -1,4 +1,3 @@
-#![cfg(target_family = "unix")]
 use newrelic_agent_control::sub_agent::on_host::command::command_os::CommandOSNotStarted;
 use newrelic_agent_control::sub_agent::on_host::command::executable_data::ExecutableData;
 use newrelic_agent_control::sub_agent::on_host::command::restart_policy::RestartPolicy;
@@ -15,10 +14,20 @@ fn non_blocking_runner() {
 
     let cmd = CommandOSNotStarted::new(
         agent_id,
+        #[cfg(target_family = "unix")]
         &ExecutableData {
             id: "sleep".to_string(),
             bin: "sleep".to_string(),
             args: vec!["5".to_string()],
+            env: HashMap::from([("TEST".to_string(), "TEST".to_string())]),
+            restart_policy: RestartPolicy::default(),
+            shutdown_timeout: Default::default(),
+        },
+        #[cfg(target_family = "windows")]
+        &ExecutableData {
+            id: "sleep".to_string(),
+            bin: "powershell".to_string(),
+            args: vec!["-Command".to_string(), "Start-Sleep -Seconds 5".to_string()],
             env: HashMap::from([("TEST".to_string(), "TEST".to_string())]),
             restart_policy: RestartPolicy::default(),
             shutdown_timeout: Default::default(),
@@ -39,10 +48,26 @@ fn command_shutdown_when_sigterm_is_ignored() {
     let agent_id = "test".to_string().try_into().unwrap();
     let mut cmd = CommandOSNotStarted::new(
         agent_id,
+        #[cfg(target_family = "unix")]
         &ExecutableData {
             id: "test".to_string(),
             bin: "tests/on_host/data/ignore_sigterm.sh".to_string(),
             args: Default::default(),
+            env: Default::default(),
+            restart_policy: Default::default(),
+            shutdown_timeout: Duration::from_millis(100),
+        },
+        #[cfg(target_family = "windows")]
+        &ExecutableData {
+            id: "test".to_string(),
+            bin: "powershell".to_string(),
+            args: vec![
+                "-NoProfile".to_string(),
+                "-ExecutionPolicy".to_string(),
+                "Bypass".to_string(),
+                "-File".to_string(),
+                "tests\\on_host\\data\\ignore_sigbreak.ps1".to_string(),
+            ],
             env: Default::default(),
             restart_policy: Default::default(),
             shutdown_timeout: Duration::from_millis(100),
