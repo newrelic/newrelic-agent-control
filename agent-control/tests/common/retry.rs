62c0ca283116d205a774fc2dbc099e76c7e8ae54
David Sánchez <davidslt+git@pm.me>
2025-11-19 09:40:04 +0000

    test: make retry generic
    this is an ideal usecase for `ControlFlow`, but let's go step by step

--- agent-control/tests/common/retry.rs
+++ agent-control/tests/common/retry.rs
@@ -3,19 +3,23 @@ use std::{error::Error, time::Duration};
 /// Retries the execution of `f` after the the `interval` has elapsed, until `max_attempts` is reached.
 /// # Panics
 /// When executing `f` keeps failing after reaching `max_attempts`.
-pub fn retry<F>(max_attempts: usize, interval: Duration, mut f: F)
+pub fn retry<F, T>(max_attempts: usize, interval: Duration, mut f: F) -> T
 where
-    F: FnMut() -> Result<(), Box<dyn Error>>,
+    F: FnMut() -> Result<T, Box<dyn Error>>,
 {
-    let mut last_err = Ok(());
+    let mut result = None;
     for _ in 0..max_attempts {
-        let Err(err) = f() else {
-            return;
-        };
-        last_err = Err(err);
+        result = Some(f());
+        if result.as_ref().is_some_and(|r| r.is_ok()) {
+            break;
+        }
         std::thread::sleep(interval);
     }
-    last_err.unwrap_or_else(|err| panic!("retry failed after {max_attempts} attempts: {err}"))
+
+    let Some(final_result) = result else {
+        panic!("provided closure never runs");
+    };
+    final_result.unwrap_or_else(|err| panic!("retry failed after {max_attempts} attempts: {err}"))
 }
 
 /// DeferredCommand is a struct that allows you to register a cleanup function that is executed on Drop.
