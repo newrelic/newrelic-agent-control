05281a32f25fadc65b3320da3080fa60c5ee96fc
Victor Ripoll <vripoll@newrelic.com>
2026-01-07 08:08:09 +0100

    feat: change nrdot distro from nrdot-collector-host to nrdot-collector (#1986)

--- build/scripts/download_collector_config.sh
+++ build/scripts/download_collector_config.sh
@@ -3,7 +3,7 @@
 # This script will download the nr-otel-collector config and use it to create the values.yaml sample
 BUILD_TMP_FOLDER=build-tmp
 DEFAULT_CONFIG_FILENAME="config.yaml"
-URL="https://raw.githubusercontent.com/newrelic/nrdot-collector-releases/refs/tags/${NR_OTEL_COLLECTOR_VERSION}/distributions/nrdot-collector-host/${DEFAULT_CONFIG_FILENAME}"
+URL="https://raw.githubusercontent.com/newrelic/nrdot-collector-releases/refs/tags/${NR_OTEL_COLLECTOR_VERSION}/distributions/nrdot-collector/${DEFAULT_CONFIG_FILENAME}"
 CONFIG_PLACEHOLDER="OTEL_COLLECTOR_CONFIG"
 TEMPLATE_DIR="build/example_templates"
 OUT_DIR="build/examples"
