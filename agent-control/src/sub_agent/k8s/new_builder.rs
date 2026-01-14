db50f9d4c4a182b39b4bff7bd43a8b8603030813
David Sánchez <davidslt+git@pm.me>
2026-01-14 14:40:51 +0000

    test: with mocks for new supervisor traits

--- agent-control/src/sub_agent/k8s/new_builder.rs
+++ agent-control/src/sub_agent/k8s/new_builder.rs
@@ -48,3 +48,105 @@ impl SupervisorBuilder for SupervisorBuilderK8s {
         ))
     }
 }
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+    use crate::agent_control::agent_id::AgentID;
+    use crate::agent_control::config::K8sConfig;
+    use crate::agent_type::agent_type_id::AgentTypeID;
+    use crate::agent_type::runtime_config::k8s::{K8s, K8sObject};
+    use crate::agent_type::runtime_config::rendered::{Deployment, Runtime};
+    use crate::k8s::client::MockSyncK8sClient;
+    use crate::sub_agent::error::SubAgentBuilderError;
+    use crate::sub_agent::identity::AgentIdentity;
+    use assert_matches::assert_matches;
+    use std::collections::HashMap;
+    use std::sync::Arc;
+
+    const TEST_CLUSTER_NAME: &str = "cluster_name";
+    const TEST_AGENT_ID: &str = "k8s-test";
+
+    #[test]
+    fn supervisor_build_ok() {
+        let agent_identity = AgentIdentity::from((
+            AgentID::try_from(TEST_AGENT_ID).unwrap(),
+            AgentTypeID::try_from("newrelic/com.newrelic.infrastructure:0.0.2").unwrap(),
+        ));
+
+        let effective_agent = EffectiveAgent::new(
+            agent_identity,
+            Runtime {
+                deployment: Deployment {
+                    linux: None,
+                    windows: None,
+                    k8s: Some(k8s_sample_runtime_config(true)),
+                },
+            },
+        );
+
+        let supervisor_builder = testing_supervisor_builder();
+
+        SupervisorBuilder::build_supervisor(&supervisor_builder, effective_agent).unwrap();
+    }
+
+    #[test]
+    fn supervisor_build_fails_for_invalid_k8s_object_kind() {
+        let agent_identity = AgentIdentity::from((
+            AgentID::try_from(TEST_AGENT_ID).unwrap(),
+            AgentTypeID::try_from("newrelic/com.newrelic.infrastructure:0.0.2").unwrap(),
+        ));
+
+        let effective_agent = EffectiveAgent::new(
+            agent_identity,
+            Runtime {
+                deployment: Deployment {
+                    linux: None,
+                    windows: None,
+                    k8s: Some(k8s_sample_runtime_config(false)),
+                },
+            },
+        );
+
+        let supervisor_builder = testing_supervisor_builder();
+
+        let result = SupervisorBuilder::build_supervisor(&supervisor_builder, effective_agent);
+        assert_matches!(
+            result.err().expect("Expected error"),
+            SubAgentBuilderError::UnsupportedK8sObject(_)
+        );
+    }
+
+    pub fn k8s_sample_runtime_config(valid_kind: bool) -> K8s {
+        let kind = if valid_kind {
+            "HelmRelease".to_string()
+        } else {
+            "UnsupportedKind".to_string()
+        };
+
+        let k8s_object = K8sObject {
+            api_version: "helm.toolkit.fluxcd.io/v2".to_string(),
+            kind,
+            ..Default::default()
+        };
+
+        let mut objects = HashMap::new();
+        objects.insert("sample_object".to_string(), k8s_object);
+        K8s {
+            objects,
+            health: None,
+            version: Default::default(),
+        }
+    }
+
+    fn testing_supervisor_builder() -> SupervisorBuilderK8s {
+        let mock_client = MockSyncK8sClient::default();
+
+        let k8s_config = K8sConfig {
+            cluster_name: TEST_CLUSTER_NAME.to_string(),
+            cr_type_meta: K8sConfig::default().cr_type_meta,
+            ..Default::default()
+        };
+        SupervisorBuilderK8s::new(Arc::new(mock_client), k8s_config)
+    }
+}
--- agent-control/src/sub_agent/k8s/new_supervisor.rs
+++ agent-control/src/sub_agent/k8s/new_supervisor.rs
@@ -88,3 +88,269 @@ impl Supervisor for StartedSupervisorK8s {
         self.stop_threads()
     }
 }
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+    use crate::agent_control::agent_id::AgentID;
+    use crate::agent_control::config::helmrelease_v2_type_meta;
+    use crate::agent_type::agent_type_id::AgentTypeID;
+    use crate::agent_type::runtime_config::k8s::K8sObjectMeta;
+    use crate::agent_type::runtime_config::k8s::{K8s, K8sObject};
+    use crate::agent_type::runtime_config::rendered::{Deployment, Runtime};
+    use crate::event::channel::pub_sub;
+    use crate::k8s::annotations::Annotations;
+    use crate::k8s::client::MockSyncK8sClient;
+    use crate::k8s::error::K8sError;
+    use crate::k8s::labels::AGENT_ID_LABEL_KEY;
+    use crate::k8s::labels::Labels;
+    use crate::sub_agent::identity::AgentIdentity;
+    use crate::sub_agent::supervisor::Supervisor;
+    use crate::sub_agent::supervisor::SupervisorStarter;
+    use assert_matches::assert_matches;
+    use k8s_openapi::apimachinery::pkg::apis::meta::v1::ObjectMeta;
+    use kube::api::DynamicObject;
+    use kube::core::TypeMeta;
+    use serde_json::json;
+    use std::collections::{BTreeMap, HashMap};
+    use std::sync::Arc;
+    use std::thread;
+    use std::time::Duration;
+
+    const TEST_API_VERSION: &str = "test/v1";
+    const TEST_KIND: &str = "test";
+    const TEST_NAMESPACE: &str = "default";
+    const TEST_NAME: &str = "test-name";
+    const TEST_AGENT_ID: &str = "k8s-test";
+    const TEST_GENT_FQN: &str = "ns/test:0.1.2";
+
+    #[test]
+    fn test_build_dynamic_objects() {
+        let agent_identity = AgentIdentity::from((
+            AgentID::try_from("test").unwrap(),
+            AgentTypeID::try_from("ns/test:0.1.2").unwrap(),
+        ));
+
+        let mock_k8s_client = MockSyncK8sClient::default();
+
+        let mut labels = Labels::new(&agent_identity.id);
+        labels.append_extra_labels(&k8s_object().metadata.labels);
+        let annotations = Annotations::new_agent_type_id_annotation(&agent_identity.agent_type_id);
+
+        let expected = DynamicObject {
+            types: Some(TypeMeta {
+                api_version: TEST_API_VERSION.to_string(),
+                kind: TEST_KIND.to_string(),
+            }),
+            metadata: ObjectMeta {
+                name: Some(TEST_NAME.to_string()),
+                namespace: Some(TEST_NAMESPACE.to_string()),
+                labels: Some(labels.get()),
+                annotations: Some(annotations.get()),
+                ..Default::default()
+            },
+            data: json!({}),
+        };
+
+        let supervisor = NotStartedSupervisorK8s::new(
+            agent_identity,
+            Arc::new(mock_k8s_client),
+            K8s {
+                objects: HashMap::from([
+                    ("mock_cr1".to_string(), k8s_object()),
+                    ("mock_cr2".to_string(), k8s_object()),
+                ]),
+                health: None,
+                version: Default::default(),
+            },
+        );
+
+        let resources = supervisor.build_dynamic_objects().unwrap();
+        assert_eq!(resources, vec![expected.clone(), expected]);
+    }
+
+    #[test]
+    fn test_k8s_objects_supervisor() {
+        let interval = Duration::from_millis(250);
+        let agent_identity = AgentIdentity::from((
+            AgentID::try_from("test").unwrap(),
+            AgentTypeID::try_from("ns/test:0.1.2").unwrap(),
+        ));
+        let apply_issue = "some issue";
+
+        // The first apply call is OK, the second fails
+        let mut seq = mockall::Sequence::new();
+        let mut mock_client = MockSyncK8sClient::default();
+        mock_client
+            .expect_apply_dynamic_object_if_changed()
+            .times(1)
+            .returning(|_| Ok(()))
+            .in_sequence(&mut seq);
+        mock_client
+            .expect_apply_dynamic_object_if_changed()
+            .times(1)
+            .returning(|_| Err(K8sError::GetDynamic(apply_issue.to_string())))
+            .in_sequence(&mut seq);
+
+        let supervisor = NotStartedSupervisorK8s {
+            interval,
+            agent_identity,
+            k8s_client: Arc::new(mock_client),
+            k8s_config: Default::default(),
+        };
+
+        let started_thread_context =
+            supervisor.start_k8s_objects_supervisor(Arc::new(vec![dynamic_object()]));
+        thread::sleep(Duration::from_millis(300)); // Sleep a bit more than one interval, two apply calls should be executed.
+        started_thread_context.stop_blocking().unwrap()
+    }
+
+    #[test]
+    fn test_start_health_check_fails() {
+        let (sub_agent_internal_publisher, _) = pub_sub();
+        let config = K8s {
+            objects: HashMap::from([("obj".to_string(), k8s_object())]),
+            health: Some(Default::default()),
+            version: Default::default(),
+        };
+
+        let supervisor = not_started_supervisor(config, None);
+        let err = supervisor
+            .start_health_check(
+                sub_agent_internal_publisher,
+                Arc::new(vec![DynamicObject {
+                    types: Some(helmrelease_v2_type_meta()),
+                    metadata: Default::default(), // missing name
+                    data: Default::default(),
+                }]),
+            )
+            .err()
+            .unwrap(); // cannot use unwrap_err because the  underlying EventPublisher doesn't implement Debug
+        assert_matches!(err, SupervisorStarterError::HealthError(_))
+    }
+
+    #[test]
+    fn test_supervisor_start_stop() {
+        let (sub_agent_internal_publisher, _) = pub_sub();
+
+        let config = K8s {
+            objects: HashMap::from([("obj".to_string(), k8s_object())]),
+            health: Some(Default::default()),
+            version: Default::default(),
+        };
+
+        let not_started = not_started_supervisor(config, None);
+        let started = SupervisorStarter::start(not_started, sub_agent_internal_publisher)
+            .expect("supervisor started");
+
+        Supervisor::stop(started).expect("supervisor thread joined");
+    }
+
+    #[test]
+    fn test_supervisor_start_without_health_check() {
+        let (sub_agent_internal_publisher, _) = pub_sub();
+
+        let config = K8s {
+            objects: HashMap::from([("obj".to_string(), k8s_object())]),
+            health: None,
+            version: Default::default(),
+        };
+
+        let not_started = not_started_supervisor(config, None);
+        let started = SupervisorStarter::start(not_started, sub_agent_internal_publisher)
+            .expect("supervisor started");
+
+        assert!(
+            !started
+                .thread_contexts
+                .iter()
+                .any(|thread_contexts| thread_contexts.thread_name() == "k8s health checker")
+        );
+    }
+
+    #[test]
+    fn test_supervisor_apply() {
+        let (sub_agent_internal_publisher, _) = pub_sub();
+
+        let config = K8s {
+            objects: HashMap::from([("obj".to_string(), k8s_object())]),
+            health: Some(Default::default()),
+            version: Default::default(),
+        };
+
+        let not_started = not_started_supervisor(config.clone(), None);
+        let started = SupervisorStarter::start(not_started, sub_agent_internal_publisher)
+            .expect("supervisor started");
+
+        // Apply new config
+        let effective_agent = EffectiveAgent::new(
+            AgentIdentity::from((
+                AgentID::try_from(TEST_AGENT_ID).unwrap(),
+                AgentTypeID::try_from(TEST_GENT_FQN).unwrap(),
+            )),
+            Runtime {
+                deployment: Deployment {
+                    k8s: Some(config),
+                    ..Deployment::default()
+                },
+            },
+        );
+
+        let started = started.apply(effective_agent).expect("applied");
+        Supervisor::stop(started).expect("stopped");
+    }
+
+    fn k8s_object() -> K8sObject {
+        K8sObject {
+            api_version: TEST_API_VERSION.to_string(),
+            kind: TEST_KIND.to_string(),
+            metadata: K8sObjectMeta {
+                labels: BTreeMap::from([
+                    ("custom-label".to_string(), "values".to_string()),
+                    (
+                        AGENT_ID_LABEL_KEY.to_string(),
+                        "to be overwritten".to_string(),
+                    ),
+                ]),
+                name: TEST_NAME.to_string(),
+                namespace: TEST_NAMESPACE.to_string(),
+            },
+            ..Default::default()
+        }
+    }
+
+    fn dynamic_object() -> DynamicObject {
+        DynamicObject {
+            types: Some(TypeMeta {
+                api_version: TEST_API_VERSION.to_string(),
+                kind: TEST_KIND.to_string(),
+            }),
+            metadata: ObjectMeta {
+                name: Some(TEST_NAME.to_string()),
+                namespace: Some(TEST_NAMESPACE.to_string()),
+                ..Default::default()
+            },
+            data: json!({}),
+        }
+    }
+
+    fn not_started_supervisor(
+        config: K8s,
+        additional_expectations_fn: Option<fn(&mut MockSyncK8sClient)>,
+    ) -> NotStartedSupervisorK8s {
+        let agent_identity = AgentIdentity::from((
+            AgentID::try_from(TEST_AGENT_ID).unwrap(),
+            AgentTypeID::try_from(TEST_GENT_FQN).unwrap(),
+        ));
+
+        let mut mock_client = MockSyncK8sClient::default();
+        mock_client
+            .expect_apply_dynamic_object_if_changed()
+            .returning(|_| Ok(()));
+        if let Some(f) = additional_expectations_fn {
+            f(&mut mock_client)
+        }
+
+        NotStartedSupervisorK8s::new(agent_identity, Arc::new(mock_client), config)
+    }
+}
