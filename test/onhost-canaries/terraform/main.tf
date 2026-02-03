62c0b0c323ab98f5f90e8279794cc0dde7ac2a76
alvarocabanas <acabanas@newrelic.com>
2026-02-03 15:24:04 +0100

    feat: Add windows fleet id

--- test/onhost-canaries/terraform/main.tf
+++ test/onhost-canaries/terraform/main.tf
@@ -29,7 +29,13 @@ variable "package_version" {
 }
 
 variable "fleet_id" {
-  description = "New Relic Fleet ID"
+  description = "New Relic Linux Fleet ID"
+  type        = string
+}
+
+
+variable "windows_fleet_id" {
+  description = "New Relic Windows Fleet ID"
   type        = string
 }
 
@@ -100,7 +106,7 @@ locals {
   // We decided to recompute the hostnames here as the "env-provisioner" module does.
   // If env-provisioner changes the way it computes the hostnames, we need to change
   // it here too. However, terraform plan will properly list all the resources that
-  // will be created and we can spot any problems with the hostnames..
+  // will be created and we can spot any problems with the hostnames.
   hostnames = [for k, v in local.ec2_instances : "${var.ec2_prefix}-${replace(k, "/[:.]/", "-")}"]
   infra_staging = var.nr_region == "Staging" ? "true" : "false"
 }
@@ -116,7 +122,7 @@ module "agent_control-canary-env-provisioner" {
   ssh_pub_key        = "AAAAB3NzaC1yc2EAAAADAQABAAABAQDH9C7BS2XrtXGXFFyL0pNku/Hfy84RliqvYKpuslJFeUivf5QY6Ipi8yXfXn6TsRDbdxfGPi6oOR60Fa+4cJmCo6N5g57hBS6f2IdzQBNrZr7i1I/a3cFeK6XOc1G1tQaurx7Pu+qvACfJjLXKG66tHlaVhAHd/1l2FocgFNUDFFuKS3mnzt9hKys7sB4aO3O0OdohN/0NJC4ldV8/OmeXqqfkiPWcgPx3C8bYyXCX7QJNBHKrzbX1jW51Px7SIDWFDV6kxGwpQGGBMJg/k79gjjM+jhn4fg1/VP/Fx37mAnfLqpcTfiOkzSE80ORGefQ1XfGK/Dpa3ITrzRYW8xlR caos-dev-arm"
   inventory_template = "../ansible/inventory-template.tmpl"
   inventory_output   = var.inventory_output
-  ansible_playbook   = "-e system_identity_client_id=${var.system_identity_client_id} -e nr_license_key=${var.license_key} -e repo_endpoint=${var.repository_endpoint} -e package_version=${var.package_version} -e fleet_id=${var.fleet_id} -e opamp_endpoint=${var.opamp_endpoint} -e token_endpoint=${var.token_endpoint} -e signature_validation_endpoint=${var.signature_validation_endpoint} -e otlp_endpoint=${var.otlp_endpoint} -e infra_staging=${local.infra_staging} ../ansible/install_ac_with_basic_config.yml"
+  ansible_playbook   = "-e system_identity_client_id=${var.system_identity_client_id} -e nr_license_key=${var.license_key} -e repo_endpoint=${var.repository_endpoint} -e package_version=${var.package_version} -e fleet_id=${var.fleet_id} -e windows_fleet_id=${var.windows_fleet_id} -e opamp_endpoint=${var.opamp_endpoint} -e token_endpoint=${var.token_endpoint} -e signature_validation_endpoint=${var.signature_validation_endpoint} -e otlp_endpoint=${var.otlp_endpoint} -e infra_staging=${local.infra_staging} ../ansible/install_ac_with_basic_config.yml"
   ec2_otels          = local.ec2_instances
 }
 
