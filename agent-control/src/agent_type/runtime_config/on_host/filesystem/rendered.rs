799085ad66d452bb8944bd7eec50e483fd4f78e9
PaoloGallina <paologallina1992@gmail.com>
2026-02-03 11:01:24 +0000

    feat(filesystem): support empty dirs (#2129)

--- agent-control/src/agent_type/runtime_config/on_host/filesystem/rendered.rs
+++ agent-control/src/agent_type/runtime_config/on_host/filesystem/rendered.rs
@@ -1,102 +1,54 @@
-use std::{
-    collections::HashMap,
-    io,
-    path::{Path, PathBuf},
-};
-
+use crate::agent_type::runtime_config::on_host::filesystem::{DirEntriesMap, SafePath};
 use ::fs::{directory_manager::DirectoryManager, file::writer::FileWriter};
+use std::collections::HashMap;
+use std::path::PathBuf;
 use thiserror::Error;
 use tracing::trace;
 
-use crate::agent_type::runtime_config::on_host::filesystem::{DirEntriesMap, SafePath};
-
 #[derive(Debug, Default, Clone, PartialEq)]
-pub struct FileSystem(pub(super) HashMap<SafePath, DirEntriesType>);
+pub struct FileSystem(pub(super) HashMap<SafePath, DirEntriesMap>);
 
 impl FileSystem {
-    /// Returns the internal file entries as a [`HashMap<PathBuf, String>`] so they can
-    /// be written into the actual host filesystem.
-    pub(super) fn expand_paths(self) -> HashMap<PathBuf, String> {
-        self.0
-            .into_iter()
-            .flat_map(|(dir_path, dir_entries)| dir_entries.expand_paths_with(&dir_path))
-            .collect()
-    }
-}
-
-#[derive(Debug, PartialEq, Clone)]
-pub enum DirEntriesType {
-    /// A directory with a fixed set of entries (i.e. files). Each entry's content can be templated.
-    /// E.g.
-    /// ```yaml
-    /// "my/dir":
-    ///   filepath1: "file1 content with ${nr-var:some_var}"
-    ///   filepath2: "file2 content"
-    /// ```
-    FixedWithTemplatedContent(HashMap<SafePath, String>),
-
-    /// A directory with a fully templated set of entries, where it's expected that a full template
-    /// is provided that renders to a valid YAML mapping of a safe [`PathBuf`] to [`String`].
-    /// E.g.
-    /// ```yaml
-    /// "my/templated/dir":
-    ///   ${nr-var:some_var_that_renders_to_a_yaml_mapping}
-    /// ```
-    FullyTemplated(DirEntriesMap),
-}
-
-impl DirEntriesType {
-    /// Returns the directory entries as an iterator of [`HashMap<PathBuf, String>`] so they can
-    /// be written into the actual host filesystem. Takes a base path to prepend to each entry.
-    fn expand_paths_with(self, path: impl AsRef<Path>) -> HashMap<PathBuf, String> {
-        let map = match self {
-            Self::FixedWithTemplatedContent(map) => map,
-            Self::FullyTemplated(tv) => tv.0,
-        };
-
-        map.into_iter()
-            .map(|(k, v)| (path.as_ref().join(k), v))
-            .collect()
-    }
-}
-
-#[derive(Debug, Error)]
-#[error("file system entries error: {0}")]
-pub struct FileSystemEntriesError(io::Error);
-
-impl From<io::Error> for FileSystemEntriesError {
-    fn from(value: io::Error) -> Self {
-        FileSystemEntriesError(value)
-    }
-}
-
-#[derive(Debug, Clone, Default, PartialEq)]
-pub struct FileSystemEntries(HashMap<PathBuf, String>);
-
-impl From<FileSystem> for FileSystemEntries {
-    fn from(value: FileSystem) -> Self {
-        Self(value.expand_paths())
-    }
-}
-
-impl FileSystemEntries {
     pub fn write(
         &self,
         file_writer: &impl FileWriter,
         dir_manager: &impl DirectoryManager,
     ) -> Result<(), FileSystemEntriesError> {
-        self.0.iter().try_for_each(|(path, content)| {
-            trace!("Writing filesystem entry to {}", path.display());
-            let parent_dir = path.parent().ok_or_else(|| {
-                io::Error::new(
-                    io::ErrorKind::InvalidFilename,
-                    format!("{} has no parent dir", path.display()),
-                )
+        self.0.iter().try_for_each(|(dir_path, dir_entries)| {
+            // Create the base directory so that we support empty directories
+            dir_manager.create(dir_path.as_ref()).map_err(|err| {
+                FileSystemEntriesError(format!("creating directory {dir_path:?}: {err}"))
             })?;
-            dir_manager.create(parent_dir)?;
-            // Will overwrite files if they already exist!
-            file_writer.write(path, content.to_owned())?;
-            Ok(())
+            dir_entries
+                .0
+                .iter()
+                .try_for_each(|(sub_path, file_content)| {
+                    let file_path = dir_path.as_ref().join(sub_path);
+                    create_file(file_writer, dir_manager, &file_path, file_content)
+                })
         })
     }
 }
+
+fn create_file(
+    file_writer: &impl FileWriter,
+    dir_manager: &impl DirectoryManager,
+    file_path: &PathBuf,
+    file_content: &String,
+) -> Result<(), FileSystemEntriesError> {
+    trace!("Writing filesystem entry to {}", file_path.display());
+    let parent_dir = file_path.parent().ok_or_else(|| {
+        FileSystemEntriesError(format!("{} has no parent dir", file_path.display()))
+    })?;
+    dir_manager.create(parent_dir).map_err(|err| {
+        FileSystemEntriesError(format!("creating directory {parent_dir:?}: {err}"))
+    })?;
+    // Will overwrite files if they already exist!
+    file_writer
+        .write(file_path.as_path(), file_content.to_owned())
+        .map_err(|err| FileSystemEntriesError(format!("creating file {file_path:?}: {err}")))
+}
+
+#[derive(Debug, Error)]
+#[error("file system entries error: {0}")]
+pub struct FileSystemEntriesError(String);
