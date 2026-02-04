9ef7b5c68dd7edac345fd841907babeca85b3c19
Paolo Gallina <paologallina1992@gmail.com>
2026-02-04 17:44:51 +0100

    chore(urls): update URLs to point to real registries

--- test/e2e-runner/src/windows/scenarios.rs
+++ test/e2e-runner/src/windows/scenarios.rs
@@ -2,4 +2,4 @@ pub mod installation_infra_agent;
 pub mod proxy;
 
 // TODO we should get the version dynamically from the recipe itself
-const INFRA_AGENT_VERSION: &str = "v1.72.1";
+const INFRA_AGENT_VERSION: &str = "1.72.1";
