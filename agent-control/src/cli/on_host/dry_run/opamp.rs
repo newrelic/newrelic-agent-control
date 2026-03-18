ec3f2089df8587851b3127f1c56cb8d60ff75a16
Daniel Orihuela Rodriguez <dorihuela@newrelic.com>
2026-03-18 17:09:34 +0100

    feat: simplify check

--- agent-control/src/cli/on_host/dry_run/opamp.rs
+++ agent-control/src/cli/on_host/dry_run/opamp.rs
@@ -5,12 +5,8 @@ use tracing::info;
 
 use crate::{
     agent_control::{
-        config_repository::repository::AgentControlConfigLoader,
         defaults::{AGENT_CONTROL_VERSION, OPAMP_AGENT_VERSION_ATTRIBUTE_KEY},
-        run::{
-            on_host::agent_control_opamp_non_identifying_attributes,
-            setup_config_repository_and_store,
-        },
+        run::on_host::agent_control_opamp_non_identifying_attributes,
     },
     command::Context,
     on_host::file_store::FileStore,
@@ -27,84 +23,69 @@ use crate::{
     secret_retriever::on_host::retrieve::OnHostSecretRetriever,
     secrets_provider::file::FileSecretProvider,
     sub_agent::identity::AgentIdentity,
+    values::ConfigRepo,
 };
 
 pub fn check_connectivity(context: Context) -> Result<(), Box<dyn std::error::Error>> {
-    let base_paths = context.ac_runner_context.base_paths;
-    let local_dir = base_paths.local_dir;
-    let remote_dir = base_paths.remote_dir;
-
-    let file_store = Arc::new(FileStore::new_local_fs(
-        local_dir.clone(),
-        remote_dir.clone(),
-    ));
-
     let maybe_opamp = context.ac_runner_context.bootstrap_config.fleet_control;
+    let Some(opamp) = maybe_opamp else {
+        info!("OpAMP configuration not found. Skipping OpAMP connectivity check.");
+        return Ok(());
+    };
 
+    let fleet_id = opamp.fleet_id.clone();
+    let poll_interval = opamp.poll_interval;
+
+    let base_paths = context.ac_runner_context.base_paths;
     let secret_retriever = OnHostSecretRetriever::new(
-        maybe_opamp.clone(),
-        local_dir.clone(),
+        Some(opamp.clone()),
+        base_paths.local_dir.clone(),
         FileSecretProvider::new(),
     );
+    let http_client_builder = OpAMPHttpClientBuilder::new(
+        opamp,
+        context.ac_runner_context.bootstrap_config.proxy.clone(),
+        secret_retriever,
+    );
 
-    let (yaml_config_repository, config_storer) =
-        setup_config_repository_and_store(file_store.clone(), maybe_opamp.is_some());
-    let agent_control_config = config_storer
-        .load()
-        .map_err(|err| format!("failed to load Agent Control config: {err}"))?;
+    let file_store = Arc::new(FileStore::new_local_fs(
+        base_paths.local_dir.clone(),
+        base_paths.remote_dir.clone(),
+    ));
+    let loader = EffectiveConfigLoaderBuilder::new(Arc::new(ConfigRepo::new(file_store.clone())));
 
-    let fleet_id = agent_control_config
-        .fleet_control
-        .as_ref()
-        .map(|c| c.fleet_id.to_string())
-        .unwrap_or_default();
+    let opamp_client_builder = OpAMPClientBuilder::new(poll_interval, http_client_builder, loader);
 
     let identifiers_provider = IdentifiersProvider::try_default()
         .map_err(|err| format!("failed to build the identifiers provider: {err}"))?
-        .with_host_id(agent_control_config.host_id.to_string())
-        .with_fleet_id(fleet_id);
-
+        .with_host_id(
+            context
+                .ac_runner_context
+                .bootstrap_config
+                .host_id
+                .to_string(),
+        )
+        .with_fleet_id(fleet_id.to_string());
     let identifiers = identifiers_provider
         .provide()
         .map_err(|err| format!("failure obtaining identifiers: {err}"))?;
     let non_identifying_attributes = agent_control_opamp_non_identifying_attributes(&identifiers);
     info!("Instance Identifiers: {:?}", identifiers);
-
-    let instance_id_storer = Storer::from(file_store);
+    let instance_id_storer = Storer::from(file_store.clone());
     let instance_id_getter = InstanceIDWithIdentifiersGetter::new(instance_id_storer, identifiers);
 
-    let opamp_client_builder = maybe_opamp.map(|config| {
-        OpAMPClientBuilder::new(
-            config.poll_interval,
-            OpAMPHttpClientBuilder::new(
-                config,
-                context.ac_runner_context.bootstrap_config.proxy.clone(),
-                secret_retriever,
-            ),
-            EffectiveConfigLoaderBuilder::new(yaml_config_repository.clone()),
-        )
-    });
-
     // Build and start AC OpAMP client
-    let (maybe_client, _maybe_sa_opamp_consumer) = opamp_client_builder
-        .as_ref()
-        .map(|builder| {
-            build_opamp_with_channel(
-                builder,
-                &instance_id_getter,
-                &AgentIdentity::new_agent_control_identity(),
-                HashMap::from([(
-                    OPAMP_AGENT_VERSION_ATTRIBUTE_KEY.to_string(),
-                    DescriptionValueType::String(AGENT_CONTROL_VERSION.to_string()),
-                )]),
-                non_identifying_attributes,
-            )
-        })
-        // Transpose changes Option<Result<T, E>> to Result<Option<T>, E>, enabling the use of `?` to handle errors in this function
-        .transpose()
-        .map_err(|err| format!("error initializing OpAMP client: {err}"))?
-        .map(|(client, consumer)| (Some(client), Some(consumer)))
-        .unwrap_or_default();
+    let (client, _consumer) = build_opamp_with_channel(
+        &opamp_client_builder,
+        &instance_id_getter,
+        &AgentIdentity::new_agent_control_identity(),
+        HashMap::from([(
+            OPAMP_AGENT_VERSION_ATTRIBUTE_KEY.to_string(),
+            DescriptionValueType::String(AGENT_CONTROL_VERSION.to_string()),
+        )]),
+        non_identifying_attributes,
+    )
+    .map_err(|err| format!("error initializing OpAMP client: {err}"))?;
 
     // We are starting and immediately stopping the client just to check connectivity.
     // The client performs a connectivity check as part of its startup process.
@@ -115,7 +96,7 @@ pub fn check_connectivity(context: Context) -> Result<(), Box<dyn std::error::Er
     // `process_message` (as part of the initial check), which is not needed.
     //
     // Long short story, the implementation leverages existing functionality at the cost of doing some unnecessary work.
-    maybe_client.map(|c| c.stop());
+    client.stop()?;
 
     info!("OpAMP connectivity check successful");
 
