b00e76e8a2a94c5e0d4b11d278b9438d2a12e274
Christian Felipe Álvarez <calvarez@newrelic.com>
2026-03-19 15:28:30 +0100

    fix(windows): race condition on service stop

--- agent-control/src/command/windows.rs
+++ agent-control/src/command/windows.rs
@@ -1,17 +1,19 @@
 //! This module contains functions to handle the Windows version of the main, which involves a Windows Service
 //! running mode.
 
-use crate::event::ApplicationEvent;
 use crate::event::channel::EventPublisher;
+use crate::{event::ApplicationEvent, utils::retry::retry};
 use std::error::Error;
 use std::sync::OnceLock;
+use std::time::Duration;
 use tracing::error;
 use windows_service::{
     service::{
-        ServiceControl, ServiceControlAccept, ServiceExitCode, ServiceState, ServiceStatus,
-        ServiceType,
+        ServiceAccess, ServiceControl, ServiceControlAccept, ServiceExitCode, ServiceState,
+        ServiceStatus, ServiceType,
     },
     service_control_handler::{self, ServiceControlHandlerResult, ServiceStatusHandle},
+    service_manager::{ServiceManager, ServiceManagerAccess},
 };
 
 /// Global handle used by the event handler to signal state changes (like StopPending) to Windows.
@@ -20,6 +22,9 @@ pub static WINDOWS_SERVICE_HANDLE: OnceLock<ServiceStatusHandle> = OnceLock::new
 
 /// Defines the name for the Windows Service.
 pub const WINDOWS_SERVICE_NAME: &str = "newrelic-agent-control";
+
+const SCM_CHECK_MAX_ATTEMPTS: usize = 10;
+const SCM_CHECK_INTERVALS: Duration = Duration::from_millis(200);
 
 /// Manages the Windows Service lifecycle, ensuring the service status is updated on exit.
 pub struct WindowsServiceStopHandler {
@@ -41,14 +46,15 @@ impl WindowsServiceStopHandler {
         run_result: &Result<(), Box<dyn Error>>,
     ) -> Result<(), Box<dyn Error>> {
         if let Some(handle) = self.handle.take() {
-            let mut status = ServiceStatus::from(WindowsServiceStatus::Stopped);
+            let exit_code = match run_result {
+                Err(err) => {
+                    error!("Service stopping due to error: {err}");
+                    ServiceExitCode::ServiceSpecific(1)
+                }
+                Ok(_) => ServiceExitCode::Win32(0),
+            };
 
-            if let Err(err) = run_result {
-                error!("Service stopping due to error: {err}");
-                status.exit_code = ServiceExitCode::ServiceSpecific(1);
-            }
-
-            handle.set_service_status(status)?;
+            set_service_as_stopped(handle, exit_code)?;
         }
         Ok(())
     }
@@ -58,15 +64,32 @@ impl Drop for WindowsServiceStopHandler {
     fn drop(&mut self) {
         // If the handle is still Some, teardown wasn't called (Panic or early return)
         if let Some(handle) = self.handle.take() {
-            let mut status = ServiceStatus::from(WindowsServiceStatus::Stopped);
             // Win32(1) indicates an abnormal process termination to Windows
-            status.exit_code = ServiceExitCode::Win32(1);
-
-            let _ = handle.set_service_status(status).inspect_err(|e| {
+            let _ = set_service_as_stopped(handle, ServiceExitCode::Win32(1)).inspect_err(|e| {
                 error!("Failed to report stopped status during abnormal exit: {e}");
             });
         }
     }
+}
+
+/// Helper to set the status server as stopped.
+/// Before setting up the Stopped status it queries the SCM to check that the service was effectively registered
+/// as running/stop-pending before. This delay avoids race-conditions if the service finishes right after starting.
+/// If the status is not correctly checked after [SCM_CHECK_MAX_ATTEMPTS], the service is still set as stopped.
+fn set_service_as_stopped(
+    handle: ServiceStatusHandle,
+    exit_code: ServiceExitCode,
+) -> windows_service::Result<()> {
+    let _ = retry(SCM_CHECK_MAX_ATTEMPTS, SCM_CHECK_INTERVALS, || {
+        scm_service_ready_to_stop(WINDOWS_SERVICE_NAME)
+    }).inspect_err(|err| {
+        error!("Could not assure that '{WINDOWS_SERVICE_NAME}' was in the expected state after {SCM_CHECK_MAX_ATTEMPTS} attempts: {err}");
+    });
+    let service_status = ServiceStatus {
+        exit_code,
+        ..ServiceStatus::from(WindowsServiceStatus::Stopped)
+    };
+    handle.set_service_status(service_status)
 }
 
 /// Sets up the Windows Service by creating the status handler and setting the service status as [WindowsServiceStatus::Running].
@@ -157,5 +180,37 @@ impl From<WindowsServiceStatus> for ServiceStatus {
             wait_hint,
             process_id: None,
         }
+    }
+}
+
+/// Queries the Windows Service Control Manager to check if the service with the given name exists and its current state
+/// is [ServiceState::Running] or [ServiceState::StopPending].
+pub fn scm_service_ready_to_stop(service_name: &str) -> Result<(), Box<dyn Error>> {
+    let manager = ServiceManager::local_computer(None::<&str>, ServiceManagerAccess::CONNECT)?;
+    let service = manager.open_service(service_name, ServiceAccess::QUERY_STATUS)?;
+    let status = service.query_status()?;
+
+    match status.current_state {
+        ServiceState::Running | ServiceState::StopPending => Ok(()),
+        _ => Err(format!(
+            "service '{}' is not in Running or StopPending state (current state: {:?})",
+            service_name, status.current_state
+        )
+        .into()),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// "eventlog" (Windows Event Log) is guaranteed to be running on Windows machines.
+    const ALWAYS_RUNNING_SERVICE: &str = "eventlog";
+    const NONEXISTING_SERVICE: &str = "no-service-with-this-name";
+
+    #[test]
+    fn test_check_scm_service_is_running() {
+        assert!(scm_service_ready_to_stop(ALWAYS_RUNNING_SERVICE).is_ok());
+        assert!(scm_service_ready_to_stop(NONEXISTING_SERVICE).is_err());
     }
 }
