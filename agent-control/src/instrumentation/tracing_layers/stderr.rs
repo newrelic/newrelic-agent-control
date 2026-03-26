b18f657231a00d9820d0bc10d0240779d5161d26
Daniel Orihuela Rodriguez <dorihuela@newrelic.com>
2026-03-26 12:31:26 +0100

    feat: send tracing to stderr

--- agent-control/src/instrumentation/tracing_layers/stderr.rs
+++ agent-control/src/instrumentation/tracing_layers/stderr.rs
@@ -5,13 +5,13 @@ use tracing_subscriber::Layer;
 use tracing_subscriber::fmt::format::PrettyFields;
 use tracing_subscriber::fmt::time::ChronoLocal;
 
-/// Returns the [LayerBox] corresponding to the standard output.
-pub fn stdout(config: &LoggingConfig) -> Result<LayerBox, LoggingConfigError> {
+/// Returns the [LayerBox] corresponding to the standard error.
+pub fn stderr(config: &LoggingConfig) -> Result<LayerBox, LoggingConfigError> {
     let target = config.format.target;
     let timestamp_fmt = config.format.timestamp.0.clone();
 
     let layer = tracing_subscriber::fmt::layer()
-        .with_writer(std::io::stdout)
+        .with_writer(std::io::stderr)
         .with_target(target)
         .with_span_events(config.fmt_span_events())
         .with_timer(ChronoLocal::new(timestamp_fmt));
