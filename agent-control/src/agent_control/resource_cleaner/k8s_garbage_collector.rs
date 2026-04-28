51b7f49e583b6526d4845bcfd89221655af21cb7
David Sánchez <davidslt+git@pm.me>
2026-04-28 10:44:50 +0100

    style: error reporting

--- agent-control/src/agent_control/resource_cleaner/k8s_garbage_collector.rs
+++ agent-control/src/agent_control/resource_cleaner/k8s_garbage_collector.rs
@@ -114,7 +114,7 @@ impl K8sGarbageCollector {
                         })
                 }
                 Err(K8sError::MissingAPIResource(e)) => {
-                    debug!(error = %e, "GC skipping for TypeMeta {}", tm.kind);
+                    debug!(error = %e, "skipping GC for TypeMeta {}", tm.kind);
                     Ok(())
                 }
                 Err(e) => Err(e.into()),
@@ -149,7 +149,11 @@ impl K8sGarbageCollector {
             Err(AgentIDError::Reserved(_)) => Ok(false),
             // We should also be conservative, so we do not delete an object if we cannot retrieve a valid AgentID from it
             Err(e) => {
-                warn!("invalid agent id: {e}");
+                warn!(
+                    namespace = self.namespace,
+                    error = %e,
+                    "invalid agent id with name {agent_id_from_labels}"
+                );
                 Ok(false)
             }
         }
