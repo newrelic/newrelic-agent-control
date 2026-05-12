60bf3389008e5ec6de87aae25d06e7d0d154796e
danielorihuela <danielorihuela@users.noreply.github.com>
2026-05-12 13:08:59 +0200

    feat: alert for consistent memory growth over time (#2508)

--- test/k8s-canaries/terraform/staging/main.tf
+++ test/k8s-canaries/terraform/staging/main.tf
@@ -15,13 +15,13 @@ variable "slack_webhook_url" {}
 module "alerts" {
   source = "../../../terraform/modules/nr_alerts"
 
-  api_key         = var.api_key
-  account_id      = var.account_id
+  api_key           = var.api_key
+  account_id        = var.account_id
   slack_webhook_url = var.slack_webhook_url
-  policies_prefix = "Agent Control canaries metric monitoring"
+  policies_prefix   = "Agent Control canaries metric monitoring"
 
-  region       = "Staging"
-  instance_id  = "Agent_Control_Canaries_Staging-Cluster"
+  region      = "Staging"
+  instance_id = "Agent_Control_Canaries_Staging-Cluster"
 
   conditions = [
     {
@@ -71,6 +71,18 @@ module "alerts" {
       template_name = "./alert_nrql_templates/generic_metric_threshold.tftpl"
     },
     {
+      name   = "Memory growth (bytes/hour)"
+      metric = "derivative(memoryResidentSizeBytes, 1 hour)"
+      sample = "ProcessSample"
+      # 3 hours in seconds
+      since = 10800
+      # 210KB/hour = ~5MB over 24 hours
+      threshold     = 210000
+      duration      = 3600
+      operator      = "above"
+      template_name = "./alert_nrql_templates/generic_metric_threshold.tftpl"
+    },
+    {
       name          = "Agent Control container"
       metric        = "*"
       sample        = "K8sContainerSample"
@@ -80,13 +92,13 @@ module "alerts" {
       template_name = "./alert_nrql_templates/generic_metric_count.tftpl"
     },
     {
-      name          = "Opamp traces per minute"
-      metric        = "*"
-      sample        = "Span"
-      threshold     = 1
-      duration      = 3600
-      operator      = "below_or_equals"
-      wheres        = {
+      name      = "Opamp traces per minute"
+      metric    = "*"
+      sample    = "Span"
+      threshold = 1
+      duration  = 3600
+      operator  = "below_or_equals"
+      wheres = {
         name = "opamp"
       }
       template_name = "./alert_nrql_templates/generic_metric_count.tftpl"
