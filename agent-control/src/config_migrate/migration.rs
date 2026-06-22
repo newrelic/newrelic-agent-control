0967c3de100ebcbfb473fcf77a593599bf4076fc
Paolo Gallina <paologallina1992@gmail.com>
2026-06-22 11:02:57 +0200

    chode(migrate): dead code

--- agent-control/src/config_migrate/migration.rs
+++
@@ -1,7 +0,0 @@
-pub mod agent_config_getter;
-pub mod agent_value_spec;
-pub mod config;
-pub mod converter;
-pub mod defaults;
-pub mod migrator;
-pub mod persister;
--- agent-control/src/config_migrate/migration/persister.rs
+++
@@ -1,2 +0,0 @@
-pub mod legacy_config_renamer;
-pub mod values_persister_file;
