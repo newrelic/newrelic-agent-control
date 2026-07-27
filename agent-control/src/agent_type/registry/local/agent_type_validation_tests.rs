ef1ed3f3702069ac16e2e0a9bdc5c6dc70ea4003
danielorihuela <danielorihuela@users.noreply.github.com>
2026-07-27 16:02:26 +0200

    feat: nri mysql agent type (#2719)

--- agent-control/src/agent_type/registry/local/agent_type_validation_tests.rs
+++ agent-control/src/agent_type/registry/local/agent_type_validation_tests.rs
@@ -863,6 +863,54 @@ static AGENT_TYPE_POSTGRESQL: LazyLock<AgentTypeValuesTestCase> =
         ..Default::default()
     });
 
+static AGENT_TYPE_MYSQL: LazyLock<AgentTypeValuesTestCase> =
+    LazyLock::new(|| AgentTypeValuesTestCase {
+        agent_type: "newrelic/com.newrelic.infrastructure.nri_mysql:0.1.0",
+        values_linux: AgentTypeValues {
+            cases: HashMap::from([
+                (
+                    "mandatory fields only",
+                    r#"
+                config: "integrations: []"
+                version: "v1.23.2"
+                "#,
+                ),
+                (
+                    "check all value types are correct",
+                    r#"
+                config: "integrations: []"
+                version: "v1.23.2"
+                oci.repository: "newrelic/nri-mysql"
+                "#,
+                ),
+            ]),
+            ..Default::default()
+        }
+        .into(),
+        values_windows: AgentTypeValues {
+            cases: HashMap::from([
+                (
+                    "mandatory fields only",
+                    r#"
+                config: "integrations: []"
+                version: "v1.23.2"
+                "#,
+                ),
+                (
+                    "check all value types are correct",
+                    r#"
+                config: "integrations: []"
+                version: "v1.23.2"
+                oci.repository: "newrelic/nri-mysql"
+                "#,
+                ),
+            ]),
+            ..Default::default()
+        }
+        .into(),
+        ..Default::default()
+    });
+
 fn get_agent_type_test_cases() -> impl Iterator<Item = &'static AgentTypeValuesTestCase> {
     [
         &AGENT_TYPE_APM_DOTNET,
@@ -884,6 +932,7 @@ fn get_agent_type_test_cases() -> impl Iterator<Item = &'static AgentTypeValuesT
         &AGENT_TYPE_NGINX,
         &AGENT_TYPE_APACHE,
         &AGENT_TYPE_POSTGRESQL,
+        &AGENT_TYPE_MYSQL,
     ]
     .into_iter()
     .map(Deref::deref)
