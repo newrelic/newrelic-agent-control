7362d3f75e544990a8ed7ff933f166b690491105
vjripoll <vripoll@newrelic.com>
2026-08-10 10:31:21 +0200

    feat: fix auto enbale systemctl

--- build/package/preremove.sh
+++ build/package/preremove.sh
@@ -4,6 +4,11 @@
 # Newrelic Agent Control
 ######################################################################################
 if command -v systemctl >/dev/null 2>&1; then
-    systemctl stop newrelic-agent-control.service
-    systemctl disable newrelic-agent-control.service
+    # Only on real removal, not on upgrade (rpm: "0", dpkg: "remove").
+    case "$1" in
+        0|remove)
+            systemctl stop newrelic-agent-control.service
+            systemctl disable newrelic-agent-control.service
+            ;;
+    esac
 fi
