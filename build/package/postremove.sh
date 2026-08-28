1e173c4b25181a9a9f5da4c2831c372db1f4cb5f
alvarocabanas <acabanas@newrelic.com>
2026-08-28 10:07:27 +0200

    fix: only execute remove of folders on deletion

--- build/package/postremove.sh
+++ build/package/postremove.sh
@@ -4,9 +4,14 @@
 # Delete config and running files
 ######################################################################################
 
-# Outside agent-control's filesystem; deleted on uninstall since a standalone newrelic-infra would recreate it.
-rm -rf /var/run/newrelic-infra
-rm -rf /etc/newrelic-agent-control
-rm -rf /var/lib/newrelic-agent-control
-rm -rf /var/log/newrelic-agent-control
-rm -rf /usr/share/doc/newrelic/newrelic-agent-control/
+# $1 = "remove"|"purge" on DEB, "0" on RPM. Skip deletion during upgrades ("upgrade" on DEB, "1" on RPM).
+case "$1" in
+  remove|purge|0)
+    # Outside agent-control's filesystem; deleted on uninstall since a standalone newrelic-infra would recreate it.
+    rm -rf /var/run/newrelic-infra
+    rm -rf /etc/newrelic-agent-control
+    rm -rf /var/lib/newrelic-agent-control
+    rm -rf /var/log/newrelic-agent-control
+    rm -rf /usr/share/doc/newrelic/newrelic-agent-control/
+  ;;
+esac
